0dfca8d63e84c604c290ab164e311a2b2cc515b9
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-05-01 18:01:46 +0200

    rework setup.sh

--- Linux/Build/setup.sh
+++ Linux/Build/setup.sh
@@ -5,8 +5,8 @@
 set -e
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-# install neofetch
-install_neofetch() {
+# install depends
+install_depends() {
     local package_manager
 
     # check packagemanager
@@ -18,10 +18,8 @@ install_neofetch() {
         package_manager="yum"
     elif command -v pacman &>/dev/null; then
         package_manager="pacman"
-    elif command -v zypper &>/dev/null; then
-        package_manager="zypper"
     else
-        echo "Unkown packagemanager. Please install neofetch" >&2
+        echo "Unkown packagemanager." >&2
         exit 1
     fi
 
@@ -30,115 +28,35 @@ install_neofetch() {
         apt-get)
             sudo apt-get update
             sudo apt-get install neofetch
-            ;;
-        dnf)
-            sudo dnf install neofetch
-            ;;
-        yum)
-            sudo yum install epel-release # only if EPEL-repo isn't installed
-			sudo yum install neofetch
-            ;;
-        pacman)
-            sudo pacman -S neofetch
-            ;;
-        zypper)
-        	sudo zypper install neofetch
-        	;;
-    esac
-}
-
-# install desktop file utils
-install_desktop_file_utils() {
-    local package_manager
-
-    # check packagemanager
-    if command -v apt-get &>/dev/null; then
-        package_manager="apt-get"
-    elif command -v dnf &>/dev/null; then
-        package_manager="dnf"
-    elif command -v yum &>/dev/null; then
-        package_manager="yum"
-    elif command -v pacman &>/dev/null; then
-        package_manager="pacman"
-    elif command -v zypper &>/dev/null; then
-        package_manager="zypper"
-    else
-        echo "Unkown packagemanager. Please install desktop-file-utils." >&2
-        exit 1
-    fi
-
-    # Install desktop-file-utils
-    case $package_manager in
-        apt-get)
-            sudo apt-get update
             sudo apt-get install -y desktop-file-utils
-            ;;
-        dnf)
-            sudo dnf install -y desktop-file-utils
-            ;;
-        yum)
-            sudo yum install -y desktop-file-utils
-            ;;
-        pacman)
-            sudo pacman -S --noconfirm desktop-file-utils
-            ;;
-        zypper)
-        	sudo zypper install desktop-file-utils
-        	;;
-    esac
-}
-
-# install adb/fastboot
-install_adb_fastboot() {
-    local package_manager
-
-    # check packagemanager
-    if command -v apt-get &>/dev/null; then
-        package_manager="apt-get"
-    elif command -v dnf &>/dev/null; then
-        package_manager="dnf"
-    elif command -v yum &>/dev/null; then
-        package_manager="yum"
-    elif command -v pacman &>/dev/null; then
-        package_manager="pacman"
-    elif command -v zypper &>/dev/null; then
-        package_manager="zypper"
-    else
-        echo "Unkown packagemanager. Please install adb and fastboot" >&2
-        exit 1
-    fi
-
-    # Install adb/fastboot
-    case $package_manager in
-        apt-get)
-            sudo apt-get update
             sudo apt-get install adb
             sudo apt-get install fastboot
             ;;
         dnf)
+            sudo dnf install neofetch
+            sudo dnf install -y desktop-file-utils
             sudo dnf install -y android-tools
             ;;
         yum)
-            sudo yum install android-tools
+            sudo yum install epel-release # only if EPEL-repo isn't installed
+			sudo yum install neofetch
+			sudo yum install -y desktop-file-utils
+			sudo yum install android-tools
             ;;
         pacman)
+            sudo pacman -S neofetch
+            sudo pacman -S --noconfirm desktop-file-utils
             sudo pacman -S --noconfirm android-tools
             ;;
-        zypper)
-        	sudo zypper install android-tools
-        	;;
     esac
 }
 
-install_neofetch
 # install depends
 echo "Get depends..."
 echo "projekt-122-l: desktop-file-utils adb fastboot"
 echo "Installing desktop-file-utils..."
-install_desktop_file_utils
-echo "Installation complete."
 echo "Installing ADB/Fastboot..."
-install_adb_fastboot
+install_depends
 echo "Installation complete."
 
 # add desktop entry
