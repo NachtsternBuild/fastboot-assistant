2ae0036bcc3ec8084b2b39507e55d4eb8aa803b9
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-07-19 13:16:26 +0200

    add file with function, that create directories

---
+++ Anwendung/config_projekt/make_dir.c
@@ -0,0 +1,31 @@
+/*
+ *-------------------------------------------*
+ *                Projekt 122                *
+ *-------------------------------------------*
+ *  	Apache License, Version 2.0		     *
+ *-------------------------------------------*
+ *                                           *
+ *  Programm um das installieren von 		 *
+ *	Custom-ROM und GSIs auf Android-Geräte 	 *
+ *	zu erleichtern  						 *
+ *                                           *
+ *-------------------------------------------*
+ *      (C) Copyright 2023 Elias Mörz 		 *
+ *-------------------------------------------*
+ *											 *
+ *              make_dir					 *
+ *											 *
+ *-------------------------------------------*
+ */
+ 
+ /* headers that used in the main programm */
+#include <stdio.h>
+#include <stdlib.h>
+
+// make dir function
+void make_dir()
+{
+	printf("Verzeichnise wird erstellt...\n");
+	system("mkdir ~/Downloads/ROM-Install");
+	system("mkdir ~/Downloads/ROM-Install/Backup");
+}
