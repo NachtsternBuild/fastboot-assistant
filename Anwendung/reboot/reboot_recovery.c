99c7091fb909fc257e26242a570d7d598df041c6
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-07-19 13:09:20 +0200

    add file with function that reboot in recovery

---
+++ Anwendung/reboot/reboot_recovery.c
@@ -0,0 +1,116 @@
+/*
+ *-------------------------------------------*
+ *                Projekt 122                *
+ *-------------------------------------------*
+ *  	Apache License, Version 2.0		     *
+ *-------------------------------------------*
+ *                                           *
+ *  Programm um das installieren von 		 *
+ *	Custom-ROM und GSIs auf Android-Geräte 	 *
+ *	zu erleichtern  						 *
+ *                                           *
+ *-------------------------------------------*
+ *      (C) Copyright 2023 Elias Mörz 		 *
+ *-------------------------------------------*
+ *											 *
+ *              reboot_recovery				 *
+ *											 *
+ *-------------------------------------------*
+ */
+ 
+/* headers that used in the main programm */
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include "program_functions.h"
+
+// Callback functions for each button
+// start reboot_recovery adb-function
+static void start_recovery_adb(GtkWidget *widget, gpointer data) 
+{
+    GtkWidget *dialog;
+    const char *title, *message;
+    
+    title = "Reboot Recovery";
+    message = "Beachten sie, dass USB-Debugging aktiviert ist in den Entwickleroptionen!\n";
+    show_message_with_title(title, message);
+    system("adb reboot recovery");
+    
+}
+
+// start reboot_recovery fastboot
+static void start_recovery_fastboot(GtkWidget *widget, gpointer data) 
+{
+    GtkWidget *dialog;
+    const char *message = "Beachten sie, dass sich ihr Gerät im Fastboot-Modus befindet!\n";
+    
+    show_message(message);
+    system("fastboot reboot recovery");
+}
+
+/* main function of reboot*/
+void reboot_fastboot(int argc, char *argv[]) 
+{
+    GtkWidget *window;
+    GtkWidget *grid;
+    GtkWidget *button;
+    char button_labels[2][20] = {"Neustart von ADB", "Neustart von Fastboot"};
+
+    gtk_init(&argc, &argv);
+
+    // create new windows
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Neustarten/Recovery");
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+    // create a grid
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    // add buttons and connect them with callback
+    for (int i = 0; i < 2; i++) {
+        button = gtk_button_new_with_label(button_labels[i]);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
+        switch (i) {
+            case 0:
+                g_signal_connect(button, "clicked", G_CALLBACK(start_recovery_adb), NULL);
+                break;
+            case 1:
+                g_signal_connect(button, "clicked", G_CALLBACK(start_recovery_fastboot), NULL);
+                break;
+        }
+    }
+
+    /* change the style of the buttons to quick settings buttons from android 12 - css is used for this*/
+    GtkCssProvider *provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_data(provider,
+                                    "button {\n"
+                                    "    background-color: #8B0000;\n" /* darkred */
+            						"	 border: none;\n"
+            						"	 border-radius: 12px;\n" /* round corners */
+            						"	 padding: 12px 24px;\n" /* inside distance */
+            						"	 color: #ffffff;\n" /* White text */
+            						"	 font-size: 16px;\n" /* Font size */
+            						"	 font-weight: 500;\n" /* Medium font */
+            						"	 text-align: center;\n" /* Centre text */
+            						"	 box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);\n" /* Light shade */
+            						"	 transition: background-color 0.3s ease, box-shadow 0.3s ease;\n" /* Animation */
+                                    "}\n"
+                                    "button:hover {\n"
+                                    "    background-color: #A52A2A;\n" /* Brighter red on hover */
+            						"	 box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);\n" /* Amplify shadows */"
+                                    "}\n",
+                                    -1,
+                                    NULL);
+    GtkStyleContext *context = gtk_widget_get_style_context(window);
+    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_object_unref(provider);
+
+    // show window
+    gtk_widget_show_all(window);
+
+    // run main-gtk-loop
+    gtk_main();
+}
+
