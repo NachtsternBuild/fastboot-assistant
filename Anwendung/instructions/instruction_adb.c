9c50697d192e34d21568ac6dcb6105951436d027
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-07-23 13:10:39 +0200

    add the instruction for the adb

---
+++ Anwendung/instructions/instruction_adb.c
@@ -0,0 +1,102 @@
+/*
+ *-------------------------------------------*
+ *                Projekt 122 - GUI          *
+ *-------------------------------------------*
+ *  	Apache License, Version 2.0		     *
+ *-------------------------------------------*
+ *                                           *
+ *  Programm um das installieren von 		 *
+ *	Custom-ROM und GSIs auf Android-Geräte 	 *
+ *	zu erleichtern - instruction_adb		 *
+ *                                           *
+ *-------------------------------------------*
+ *      (C) Copyright 2023 Elias Mörz 		 *
+ *-------------------------------------------*
+ *
+ */
+
+/* headers that used in the programm */
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include "program_functions.h"
+
+void instruction_adb(int argc, char *argv[]) 
+{
+    // Initiate GTK
+    gtk_init(&argc, &argv);
+
+    // Create main window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Anleitung ADB/Fastboot");
+    gtk_widget_set_size_request(window, 500, 400);
+
+    // Connect close function to 'destroy' signal
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_close), NULL);
+
+    // Frame for each information field
+    GtkWidget *frame1 = gtk_frame_new(NULL);
+    GtkWidget *frame2 = gtk_frame_new(NULL);
+    GtkWidget *frame3 = gtk_frame_new(NULL);
+    GtkWidget *frame4 = gtk_frame_new(NULL);
+    GtkWidget *frame5 = gtk_frame_new(NULL);
+    
+    // Vertical box layout for the frames
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+    
+    // show-text
+    GtkWidget *inst1_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst1_label), g_strdup_printf("<b><u> Android Debug Bridge (ADB)/ Fastboot </u></b>"));
+    gtk_container_add(GTK_CONTAINER(vbox), inst1_label);
+
+    // Creation of text labels with the text to be displayed
+    const char *adb_text = "\nADB (Android Debug Bridge) stellt \neine Kommunikationsschnittstelle zwischen einem Computer \nund einem Android-Gerät zur Verfügung, \ndie für Debugging und Dateiübertragung genutzt werden kann.\nFastboot ist ein Befehlszeilenwerkzeug \nfür den Bootloader-Modus von Android-Geräten, \nmit dem benutzerdefinierte Firmware und andere Systemabbilder \ngeflasht werden können. \nBeide Tools sind für Entwickler und fortgeschrittene Nutzer \nvon großem Nutzen, wenn es darum geht, Android-Geräte \nzu modifizieren und zu diagnostizieren.\n";
+    GtkWidget *label_adb = gtk_label_new(adb_text);
+    gtk_container_add(GTK_CONTAINER(frame1), label_adb);
+    
+    // show-text
+    GtkWidget *inst2_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst2_label), g_strdup_printf("<b><u> Verbinden mit ADB </u></b>"));
+    gtk_container_add(GTK_CONTAINER(vbox), inst2_label);
+
+    const char *connect1_text = "\nVerbinden sie ihr das Gerät mit ihrem Computer.\n";
+    GtkWidget *label_connect1 = gtk_label_new(connect1_text);
+    gtk_container_add(GTK_CONTAINER(frame2), label_connect1);
+
+    const char *connect2_text = "\nEntwickleroptionen aktivieren.\n1.Suchen sie in den Einstellungen nach 'Build-Nummer'. \n2. Tippen sie siebenmal auf die 'Build-Nummer'. \n3. Suchen sie in den Einstellungen 'Debug'. \n4. Aktivieren sie USB-Debbuging. \n5. Aktivieren sie in \nden USB-Einstellungen 'Dateiübertragung'. \n5. Prüfen sie ob eine Verbindung zur ADB besteht.\n";
+    GtkWidget *connect2_version = gtk_label_new(connect2_text);
+    gtk_container_add(GTK_CONTAINER(frame3), label_connect2);
+    
+    // show-text
+    GtkWidget *inst3_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst3_label), g_strdup_printf("<b><u> Neustart in Fastboot </u></b>"));
+    gtk_container_add(GTK_CONTAINER(vbox), inst3_label);
+
+    const char *connect3_text = "\n1. Navigieren sie in der Anwendung \ndurch 'Gerät neustarten' zu \n'Neustart in Fastboot (von der ADB)'. \n2. Warten sie bis sich \nihr Gerät im Fastboot-Modus befindet. \n3. Prüfen sie ob eine Verbindung \nzum Fastboot-Modus besteht.\n";
+    GtkWidget *label_connect3 = gtk_label_new(connect3_text);
+    gtk_container_add(GTK_CONTAINER(frame4), label_connect3);
+    
+    // show-text
+    GtkWidget *inst4_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst4_label), g_strdup_printf("<b><u> Weiteres </u></b>"));
+    gtk_container_add(GTK_CONTAINER(vbox), inst4_label);
+    
+    const char *more_text = "\nWollen sie mehr über die \nBedienung von Fastboot erfahren \nstarten sie die Fastboot-Hilfe.\n";
+    GtkWidget *label_more = gtk_label_new(more_text);
+    gtk_container_add(GTK_CONTAINER(frame5), label_more);
+    
+
+    // Add frames to the main window
+    gtk_box_pack_start(GTK_BOX(vbox), frame1, TRUE, TRUE, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), frame2, TRUE, TRUE, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), frame3, TRUE, TRUE, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), frame4, TRUE, TRUE, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), frame5, TRUE, TRUE, 5);
+
+    // Show all elements
+    gtk_widget_show_all(window);
+
+    // Run the GTK main loop
+    gtk_main();
+}
