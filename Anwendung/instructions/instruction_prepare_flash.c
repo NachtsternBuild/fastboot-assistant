54fa44cba57f02d8ba54c41c64e095d97ab60b49
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-07-25 11:41:13 +0200

    add instruction prepare flash

---
+++ Anwendung/instructions/instruction_prepare_flash.c
@@ -0,0 +1,89 @@
+/*
+ *-------------------------------------------*
+ *                Projekt 122 - GUI          *
+ *-------------------------------------------*
+ *  	Apache License, Version 2.0		     *
+ *-------------------------------------------*
+ *                                           *
+ *  Programm um das installieren von 		 *
+ *	Custom-ROM und GSIs auf Android-Geräte 	 *
+ *	zu erleichtern 							*
+ *											 * 
+ *			instruction_prepare_flash		 *
+ *                                           *
+ *-------------------------------------------*
+ *      (C) Copyright 2023 Elias Mörz 		 *
+ *-------------------------------------------*
+ *
+ */
+
+/* headers that used in the programm */
+#include <stdio.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include "program_functions.h"
+
+// button 1 - inst. for root
+static void inst_backup(GtkWidget *widget, gpointer data)
+{
+    instruction_backup();
+}
+
+// button 2 - inst. for vendor
+static void inst_preflash(GtkWidget *widget, gpointer data)
+{
+	instruction_preflash();
+}
+
+/* start main programm */
+void instruction_prepare_flash(int argc, char *argv[])
+{
+	// int gtk
+	gtk_init(&argc, &argv);
+	
+	// make main window
+	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Anleitungen Vorbereitungen Flash");
+    gtk_container_set_border_width(GTK_CONTAINER(window), 500);
+    gtk_widget_set_size_request(window, 800, 750);
+	gtk_widget_show(window);
+	
+	// make button for every function with label
+    GtkWidget *button_inst_backup = gtk_button_new_with_label("Backup");
+    GtkWidget *button_inst_preflash = gtk_button_new_with_label("Images vorbereiten");
+    
+    // Link the click callback function with the buttons 
+    g_signal_connect(button_inst_backup, "clicked", G_CALLBACK(inst_backup), NULL);
+    g_signal_connect(button_inst_preflash, "clicked", G_CALLBACK(inst_preflash), NULL);
+    
+    // Create a layout container (HBox) for the buttons
+    GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+    
+    // Create a layout container (VBox) for the left and right buttons
+    GtkWidget *left_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
+    GtkWidget *right_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
+    
+    // Add the first two buttons to the left VBox
+    gtk_box_pack_start(GTK_BOX(left_vbox), button_inst_backup, TRUE, TRUE, 0);
+    
+    // Add the other two buttons to the right VBox
+    gtk_box_pack_start(GTK_BOX(right_vbox), button_inst_preflash, TRUE, TRUE, 0);
+    
+     // Add the left and right VBoxes to the main HBox
+    gtk_box_pack_start(GTK_BOX(hbox), left_vbox, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox), right_vbox, TRUE, TRUE, 0);
+
+    // Add the main HBox to the main window
+    gtk_container_add(GTK_CONTAINER(window), hbox);
+
+    // show all button
+    gtk_widget_show(button_inst_backup);
+    gtk_widget_show(button_inst_preflash);
+    gtk_widget_show(left_vbox);
+    gtk_widget_show(right_vbox);
+    gtk_widget_show(hbox);
+	
+	// gtk main loop
+	gtk_main();
+}
+    
