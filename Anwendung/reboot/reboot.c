4122e24d7f8e7b019c498ab9228aed944ac19491
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-07-29 14:26:12 +0200

    fix the function name

--- Anwendung/reboot/reboot.c
+++ Anwendung/reboot/reboot.c
@@ -23,7 +23,7 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 
-void reboot_fastboot(GtkWidget *widget, gpointer data)
+void reboot_system(GtkWidget *widget, gpointer data)
 {
 	GtkWidget *dialog;
     const char *message = "Neustart wird durchgeführt.\n";
@@ -49,11 +49,11 @@ void reboot(int argc, char *argv[])
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
     // Create a button
-    GtkWidget *button_reboot_fastboot = gtk_button_new_with_label("Neustarten");
-    g_signal_connect(button_reboot_fastboot, "clicked", G_CALLBACK(reboot_fastboot), NULL);
+    GtkWidget *button_reboot_system = gtk_button_new_with_label("Neustarten");
+    g_signal_connect(button_reboot_system, "clicked", G_CALLBACK(reboot_system), NULL);
 
     // Pack the button into the box
-    gtk_box_pack_start(GTK_BOX(vbox), button_reboot_fastboot, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button_reboot_system, TRUE, TRUE, 0);
 
     // Show all the widgets
     gtk_widget_show_all(window);
