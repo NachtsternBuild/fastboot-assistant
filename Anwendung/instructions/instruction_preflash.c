0a6c1af723b9f7fccd0ce6263683733750e3e833
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-07-29 12:15:54 +0200

    fix some errors

--- Anwendung/instructions/instruction_preflash.c
+++ Anwendung/instructions/instruction_preflash.c
@@ -37,10 +37,6 @@ void instruction_preflash(int argc, char *argv[])
     // frame at the box
     GtkWidget *frame = gtk_frame_new(NULL);
     gtk_container_add(GTK_CONTAINER(window), frame);
-
-    // create vbox
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_container_add(GTK_CONTAINER(frame), vbox);
     
     // Frame for each information field
     GtkWidget *frame26 = gtk_frame_new(NULL);
