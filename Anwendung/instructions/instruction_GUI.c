c0ff0e2b7a8e1dc35b752aa15f1aee433c037bf5
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-07-29 11:42:22 +0200

    fix some errors

--- Anwendung/instructions/instruction_GUI.c
+++ Anwendung/instructions/instruction_GUI.c
@@ -131,7 +131,7 @@ void preflash_GUI(int argc, char *argv[])
                                     "}\n"
                                     "button:hover {\n"
                                     "    background-color: #A52A2A;\n" /* Brighter red on hover */
-            						"	 box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);\n" /* Amplify shadows */"
+            						"	 box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);\n" /* Amplify shadows */
                                     "}\n",
                                     -1,
                                     NULL);
