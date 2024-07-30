d58b025f06d07e1c6e9ad49af08290774c6d40b1
Elias Mörz <133899397+NachtsternBuild@users.noreply.github.com>
2024-07-30 12:13:46 +0200

    rework build.sh

--- Anwendung/Build/build.sh
+++ Anwendung/Build/build.sh
@@ -2,10 +2,11 @@
 # build.sh
 
 # Define directory
-
+# home directory
 home_dir="$HOME"
 
-source_dir="$home_dir/Anwendung"
+# the other directory
+source_dir="$home_dir/fastboot-assistant/Anwendung"
 header_dir="${source_dir}/header"
 config_dir="${source_dir}/config_projekt"
 reboot_dir="${source_dir}/reboot"
@@ -14,6 +15,15 @@ preflash_dir="${source_dir}/preflash"
 instructions_dir="${source_dir}/instructions"
 target_dir="${source_dir}/build_project"
 output_dir="${source_dir}/output"
+
+# set Authorisation
+chmod a+x "$source_dir"
+chmod a+x "$header_dir"
+chmod a+x "$config_dir"
+chmod a+x "$reboot_dir"
+chmod a+x "$flash_dir"
+chmod a+x "$preflash_dir"
+chmod a+x "$instructions_dir"
 
 # Check if target directory exists
 while true; do
@@ -41,10 +51,12 @@ find "$flash_dir" -maxdepth 1 -type f -exec cp {} "$target_dir" \;
 find "$preflash_dir" -maxdepth 1 -type f -exec cp {} "$target_dir" \;
 find "$instructions_dir" -maxdepth 1 -type f -exec cp {} "$target_dir" \;
 echo "Alle Dateien wurden nach $target_dir kopiert."
-
+# set Authorisation
+chmod a+x "$target_dir"
 cd "$target_dir" || { echo "Fehler beim Wechseln ins Verzeichnis $target_dir"; exit 1; }
 
 # Build files
+echo "Starte Build."
 if make; then
     echo "Build erfolgreich."
 else
@@ -52,9 +64,13 @@ else
     exit 1
 fi
 
+# copy output to output-dir
+echo "Kopieren des Paketes."
 rm -r "$output_dir"
 mkdir "$output_dir"
 cp Projekt-122-l "$output_dir"
+chmod a+x "$output_dir"
+echo "Fertig kopiert."
 
 # Cleanup prompt
 while true; do
