4620aed3363ba9cc797ad8d0a4f3fcf77efd7eee
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-07-31 15:38:33 +0200

    fix #12

--- Anwendung/instructions/instruction_preflash.c
+++ Anwendung/instructions/instruction_preflash.c
@@ -29,71 +29,59 @@ void instruction_preflash(int argc, char *argv[])
     // Create main window
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Anleitung Vorbereitungen flash");
-    gtk_widget_set_size_request(window, 500, 400);
+    gtk_widget_set_size_request(window, 1000, 950);
 
     // Connect close function to 'destroy' signal
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_close), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // frame at the box
-    GtkWidget *frame = gtk_frame_new(NULL);
-    gtk_container_add(GTK_CONTAINER(window), frame);
-    
-    // Frame for each information field
-    GtkWidget *frame26 = gtk_frame_new(NULL);
-    
     // Vertical box layout for the frames
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
     // Creation of text labels with the text to be displayed
-    const char *preflash_text = "\n1. Informieren sie genauer über \nihr Gerät und den Chipsatz ihres Gerätes. \n2. Suchen sie nach entsprechenden \nCustom-ROMs, Custom-Recoverys oder GSIs. \n3. Prüfen sie die Verfügbarkeit von Project Treble für ihr Gerät. \n4. Prüfen sie welche Partitionierungsschema \nauf ihrem Gerät vorliegt (a/b-devices oder only-a-devices).\n5.Sichern sie ihr Daten lokal und wenn sie wollen in einer Cloud. \n6. Laden sie alle Dateien herunter die für das Custom-ROM \noder GSI gebraucht werden und lesen sie die Anleitungen \nder Entwickler der Custom-ROMs vor dem flash. \n7. Benennen sie alle Dateien der folgenden Liste nach um. \nEs gibt ein Funktion, die das übernimmt, \naber es ist nicht garantiert, dass es funktioniert. \n8. Kopieren sie alle Dateien \ndie geflasht werden sollen in das Verzeichnis:\n \n'~/Downlaods/ROM-Install/'\n";
+    const char *preflash_text = "\n1. Informieren sie genauer über \nihr Gerät und den Chipsatz ihres Gerätes. \n2. Suchen sie nach entsprechenden \nCustom-ROMs, Custom-Recoverys oder GSIs. \n3. Prüfen sie die Verfügbarkeit von Project Treble für ihr Gerät. \n4. Prüfen sie welches Partitionierungsschema \nauf ihrem Gerät vorliegt (a/b-devices oder only-a-devices).\n5. Sichern sie ihre Daten lokal und wenn sie wollen in einer Cloud. \n6. Laden sie alle Dateien herunter die für das Custom-ROM \noder GSI gebraucht werden und lesen sie die Anleitungen \nder Entwickler der Custom-ROMs vor dem Flash. \n7. Benennen sie alle Dateien der folgenden Liste nach um. \nEs gibt eine Funktion, die das übernimmt, \naber es ist nicht garantiert, dass es funktioniert. \n8. Kopieren sie alle Dateien \ndie geflasht werden sollen in das Verzeichnis:\n \n'~/Downloads/ROM-Install/'\n";
     GtkWidget *label_preflash = gtk_label_new(preflash_text);
-    gtk_container_add(GTK_CONTAINER(frame26), label_preflash);
+    gtk_box_pack_start(GTK_BOX(vbox), label_preflash, TRUE, TRUE, 5);
     
-    // list of all file names that are flashed
-    // show-text
+    // show-text for list of all file names that are flashed
     GtkWidget *inst17_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst17_label), g_strdup_printf("<b><u> Liste aller Dateien </u></b>"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst17_label);
-    
+    gtk_label_set_markup(GTK_LABEL(inst17_label), "<b><u>Liste aller Dateien</u></b>");
+    gtk_box_pack_start(GTK_BOX(vbox), inst17_label, FALSE, FALSE, 5);
+
     // Creation of text labels with the text to be displayed
-    const char *file_list_text = "\n- preloader.bin \n- preloader.img \n-super.img \n- recovery.img \n- boot.img \n- init_boot.img \n- vendor_boot.img \n- vendor.img \n- system.img \n- vbmeta.img \n- dtbo.img \n- metadata.img \n- userdata.img \n- payload.zip (das ist die Zip-Datei eines jeden Custom-ROMs)\n";
+    const char *file_list_text = "\n- preloader.bin \n- preloader.img \n- super.img \n- recovery.img \n- boot.img \n- init_boot.img \n- vendor_boot.img \n- vendor.img \n- system.img \n- vbmeta.img \n- dtbo.img \n- metadata.img \n- userdata.img \n- payload.zip (das ist die Zip-Datei eines jeden Custom-ROMs)\n";
     GtkWidget *label_file_list = gtk_label_new(file_list_text);
-    gtk_container_add(GTK_CONTAINER(frame27), label_file_list);
-    // end of the list
-    // list with some interesting links
+    gtk_box_pack_start(GTK_BOX(vbox), label_file_list, TRUE, TRUE, 5);
+
+    // show-text for some interesting links
     GtkWidget *inst18_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst18_label), g_strdup_printf("<b><u> Einige Custom-ROMs und Recoverys </u></b>"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst18_label);
+    gtk_label_set_markup(GTK_LABEL(inst18_label), "<b><u>Einige Custom-ROMs und Recoverys</u></b>");
+    gtk_box_pack_start(GTK_BOX(vbox), inst18_label, FALSE, FALSE, 5);
     
     GtkWidget *inst19_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst19_label), g_strdup_printf("<b>TWRP: </b> \nhttps://www.twrp.me/\n"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst17_label);
-    
-    GtkWidget *inst20_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst20_label), g_strdup_printf("<b>/e/ OS: </b> \nhttps://e.foundation/e-os/\n"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst20_label);
-    
-    GtkWidget *inst21_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst21_label), g_strdup_printf("<b>LineageOS: </b> \nhttps://lineageos.org/\n"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst21_label);
-    
-     GtkWidget *inst22_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst22_label), g_strdup_printf("<b>Project Elixir: </b> \nhttps://projectelixiros.com/home\n"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst22_label);
-    
-     GtkWidget *inst23_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst23_label), g_strdup_printf("<b>crDroid: </b> \nhttps://crdroid.net/\n"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst23_label);
-    
-     GtkWidget *inst24_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst24_label), g_strdup_printf("<b>Evolution X: </b> \nhttps://sourceforge.net/projects/evolution-x/\n"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst24_label);
-    // end of the list
-    
-    // Add frames to the main window
-    gtk_box_pack_start(GTK_BOX(vbox), frame26, TRUE, TRUE, 5);
+    gtk_label_set_markup(GTK_LABEL(inst19_label), "<b>TWRP:</b> https://www.twrp.me/\n");
+    gtk_box_pack_start(GTK_BOX(vbox), inst19_label, FALSE, FALSE, 5);
 
+    GtkWidget *inst20_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst20_label), "<b>/e/ OS:</b> https://e.foundation/e-os/\n");
+    gtk_box_pack_start(GTK_BOX(vbox), inst20_label, FALSE, FALSE, 5);
+
+    GtkWidget *inst21_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst21_label), "<b>LineageOS:</b> https://lineageos.org/\n");
+    gtk_box_pack_start(GTK_BOX(vbox), inst21_label, FALSE, FALSE, 5);
+
+    GtkWidget *inst22_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst22_label), "<b>Project Elixir:</b> https://projectelixiros.com/home\n");
+    gtk_box_pack_start(GTK_BOX(vbox), inst22_label, FALSE, FALSE, 5);
+
+    GtkWidget *inst23_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst23_label), "<b>crDroid:</b> https://crdroid.net/\n");
+    gtk_box_pack_start(GTK_BOX(vbox), inst23_label, FALSE, FALSE, 5);
+
+    GtkWidget *inst24_label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(inst24_label), "<b>Evolution X:</b> https://sourceforge.net/projects/evolution-x/\n");
+    gtk_box_pack_start(GTK_BOX(vbox), inst24_label, FALSE, FALSE, 5);
+    
     // Show all elements
     gtk_widget_show_all(window);
 
