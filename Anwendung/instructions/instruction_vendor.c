3821d87766a2a2e8d6a9f7244bd8cee3793e8177
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-07-31 15:33:04 +0200

    fix #12

--- Anwendung/instructions/instruction_vendor.c
+++ Anwendung/instructions/instruction_vendor.c
@@ -15,8 +15,6 @@
  *
  */
 
-// add unlock bootloader
-
 /* headers that used in the programm */
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,34 +29,24 @@ void instruction_vendor(int argc, char *argv[])
     // Create main window
     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Anleitung und Info Vendor");
-    gtk_widget_set_size_request(window, 500, 400);
+    gtk_widget_set_size_request(window, 1000, 950);
 
     // Connect close function to 'destroy' signal
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_close), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // frame at the box
-    GtkWidget *frame = gtk_frame_new(NULL);
-    gtk_container_add(GTK_CONTAINER(window), frame);
-    
-    // Frame for each information field
-    GtkWidget *frame17 = gtk_frame_new(NULL);
-    
-    // Vertical box layout for the frames
+    // Create vertical box layout for the frames
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_container_add(GTK_CONTAINER(window), vbox);
     
-    // show-text
+    // Vendor Native Development Kit (VNDK) header
     GtkWidget *inst9_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst9_label), g_strdup_printf("<b><u> Vendor Native Development Kit (VNDK) </u></b>"));
-    gtk_container_add(GTK_CONTAINER(vbox), inst9_label);
+    gtk_label_set_markup(GTK_LABEL(inst9_label), "<b><u>Vendor Native Development Kit (VNDK)</u></b>");
+    gtk_box_pack_start(GTK_BOX(vbox), inst9_label, FALSE, FALSE, 5);
 
-    // Creation of text labels with the text to be displayed
-    const char *vndk_text = "\nAlles was mit 'Vendor' bezeichnet wird, \nstellen hersteller- oder \ngerätspezifische Komponenten des Betriebssystems da. \nÄnderungen daran sind mit äußerster Vorsicht zu betrachten.\n \nDie VNDK-Version (Vendor Native Development Kit) \nstellt eine Sammlung von Bibliotheken und Richtlinien dar, \ndie es ermöglichen, den hardware-spezifischen Teil \neines Android-Systems von \ngenerischen Systemimplementierung zu trennen. \nDadurch lassen sich Updates und Wartung erleichtern, \nindem sichergestellt wird, dass herstellerspezifische \nAnpassungen nicht die Kompatibilität und Stabilität \ndes Android-Betriebssystems beeinträchtigen.\n";
+    // Description about VNDK
+    const char *vndk_text = "\nAlles was mit 'Vendor' bezeichnet wird, \nstellen hersteller- oder \ngerätspezifische Komponenten des Betriebssystems da. \nÄnderungen daran sind mit äußerster Vorsicht zu betrachten.\n \nDie VNDK-Version (Vendor Native Development Kit) \nstellt eine Sammlung von Bibliotheken und Richtlinien dar, \ndie es ermöglichen, den hardware-spezifischen Teil \neines Android-Systems von \ngenerischen Systemimplementierungen zu trennen. \nDadurch lassen sich Updates und Wartung erleichtern, \nindem sichergestellt wird, dass herstellerspezifische \nAnpassungen nicht die Kompatibilität und Stabilität \ndes Android-Betriebssystems beeinträchtigen.\n";
     GtkWidget *label_vndk = gtk_label_new(vndk_text);
-    gtk_container_add(GTK_CONTAINER(frame17), label_vndk);
-    
-    // Add frames to the main window
-    gtk_box_pack_start(GTK_BOX(vbox), frame17, TRUE, TRUE, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), label_vndk, TRUE, TRUE, 5);
 
     // Show all elements
     gtk_widget_show_all(window);
@@ -66,3 +54,4 @@ void instruction_vendor(int argc, char *argv[])
     // Run the GTK main loop
     gtk_main();
 }
+
