3567c934676660e986d61db5cd014ffb19b1223a
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-09 17:31:57 +0200

    rework the UI and fix #6

--- Anwendung/preflash/backup_noroot.c
+++ Anwendung/preflash/backup_noroot.c
@@ -22,92 +22,107 @@
 #include "program_functions.h"
 #include "function_header.h"
 
-// button 1 - install dependencies for the backup.sh
-static void install_dependencies(GtkWidget *widget, gpointer data)
+#define MAX_BUFFER_SIZE 256
+// Callback functions for each button
+// install depends for open android backup
+static void install_depends(GtkWidget *widget, gpointer data) 
 {
     GtkWidget *dialog;
-    const char *message = "Die Installation der benötigen Abhängigkeiten kann nur mit Root-Rechten ausgeführt werden.";
+    const char *message = "\nDie Installation der benötigen Abhängigkeiten kann nur mit Root-Rechten ausgeführt werden.\n";
     // show message
     show_message(message);
     
 	char function_command[255];
-	open_terminal_by_desktop("sudo apt update && sudo apt install p7zip-full adb curl whiptail pv bc secure-delete zenity && exit");
+	open_terminal_by_desktop("sudo apt update && sudo apt-get install p7zip-full adb curl whiptail pv bc secure-delete zenity && exit");
 }
 
-// button 2 - download the open_android_backup.zip and unpack it
-static void download_backup_sh(GtkWidget *widget, gpointer data)
+// download open android backup
+static void download_backup(GtkWidget *widget, gpointer data) 
 {
-	system("fastboot devices");
-	char function_command[255];
+    GtkWidget *dialog;
+    const char *message = "\nDas Tool 'Open Android Backup' wird heruntergeladen und entpackt.\n";
+    char function_command[255];
 	open_terminal_by_desktop("wget -O ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip https://github.com/mrrfv/open-android-backup/releases/download/v1.0.18/Open_Android_Backup_v1.0.18_Bundle.zip && exit");
 
 	system("unzip ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip -d ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle");
-	GtkWidget *dialog;
-    const char *message = "Open Android Backup ist nun bereit.";
+    const char *message = "\nOpen Android Backup ist nun bereit.\n";
     // show message
     show_message(message);
 }
 
-// button 3 - start the backup.sh
-static void start_backup_sh(GtkWidget *widget, gpointer data)
+// open open android backup
+static void open_backup(GtkWidget *widget, gpointer data) 
 {
-	char function_command[255];
+    char function_command[255];
 	open_terminal_by_desktop("bash ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle/backup.sh");
 }
 
-/* start main programm */
-void backup_noroot(int argc, char *argv[])
+/* main function of backup_noroot*/
+void backup_noroot(int argc, char *argv[]) 
 {
-	// int gtk
-	gtk_init(&argc, &argv);
-	
-	// make main window
-	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Backup ohne Root");
-    gtk_container_set_border_width(GTK_CONTAINER(window), 500);
-    gtk_widget_set_size_request(window, 800, 750);
-	gtk_widget_show(window);
-	
-	// make button for every function with label
-    GtkWidget *button_install_dependencies = gtk_button_new_with_label("Installation der Abhängigkeiten");
-    GtkWidget *button_download_backup_sh = gtk_button_new_with_label("Download und vorbereiten von Open Android Backup");
-    GtkWidget *button_start_backup_sh = gtk_button_new_with_label("Starten Open Android Backup");
-    
-    // Link the click callback function with the buttons 
-    g_signal_connect(button_install_dependencies, "clicked", G_CALLBACK(install_dependencies), NULL);
-    g_signal_connect(button_download_backup_sh, "clicked", G_CALLBACK(download_backup_sh), NULL);
-    g_signal_connect(button_start_backup_sh, "clicked", G_CALLBACK(start_backup_sh), NULL);
-   
-    // Create a layout container (HBox) for the buttons
-    GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    
-    // Create a layout container (VBox) for the left and right buttons
-    GtkWidget *left_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
-    GtkWidget *right_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
-    
-    // Add the first two buttons to the left VBox
-    gtk_box_pack_start(GTK_BOX(left_vbox), button_install_dependencies, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(left_vbox), button_download_backup_sh, TRUE, TRUE, 0);
-    
-    // Add the other two buttons to the right VBox
-    gtk_box_pack_start(GTK_BOX(right_vbox), button_start_backup_sh, TRUE, TRUE, 0);
-    
-     // Add the left and right VBoxes to the main HBox
-    gtk_box_pack_start(GTK_BOX(hbox), left_vbox, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(hbox), right_vbox, TRUE, TRUE, 0);
+    GtkWidget *window;
+    GtkWidget *grid;
+    GtkWidget *button;
+    char button_labels[3][20] = {"install depends" "Download", "Starten"};
 
-    // Add the main HBox to the main window
-    gtk_container_add(GTK_CONTAINER(window), hbox);
+    gtk_init(&argc, &argv);
 
-    // show all button
-    gtk_widget_show(button_install_dependencies);
-    gtk_widget_show(button_download_backup_sh);
-    gtk_widget_show(button_start_backup_sh);
-    gtk_widget_show(left_vbox);
-    gtk_widget_show(right_vbox);
-    gtk_widget_show(hbox);
-	
-	// gtk main loop
-	gtk_main();
+    // create new windows
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Open Android Backup");
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+    // create a grid
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    // add buttons and connect them with callback
+    for (int i = 0; i < 3; i++) {
+        button = gtk_button_new_with_label(button_labels[i]);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
+        switch (i) {
+            case 0:
+                g_signal_connect(button, "clicked", G_CALLBACK(install_depends), NULL);
+                break;
+            case 1:
+                g_signal_connect(button, "clicked", G_CALLBACK(download_backup), NULL);
+                break;
+            case 2:
+                g_signal_connect(button, "clicked", G_CALLBACK(open_backup), NULL);
+                break;
+        }
+    }
+
+    /* change the style of the buttons to quick settings buttons from android 12 - css is used for this*/
+    GtkCssProvider *provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_data(provider,
+                                    "button {\n"
+                                    "    background-color: #8B0000;\n" /* darkred */
+            						"	 border: none;\n"
+            						"	 border-radius: 12px;\n" /* round corners */
+            						"	 padding: 12px 24px;\n" /* inside distance */
+            						"	 color: #ffffff;\n" /* White text */
+            						"	 font-size: 16px;\n" /* Font size */
+            						"	 font-weight: 500;\n" /* Medium font */
+            						"	 text-align: center;\n" /* Centre text */
+            						"	 box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);\n" /* Light shade */
+            						"	 transition: background-color 0.3s ease, box-shadow 0.3s ease;\n" /* Animation */
+                                    "}\n"
+                                    "button:hover {\n"
+                                    "    background-color: #A52A2A;\n" /* Brighter red on hover */
+            						"	 box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);\n" /* Amplify shadows */
+                                    "}\n",
+                                    -1,
+                                    NULL);
+    GtkStyleContext *context = gtk_widget_get_style_context(window);
+    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_object_unref(provider);
+
+    // show window
+    gtk_widget_show_all(window);
+
+    // run main-gtk-loop
+    gtk_main();
 }
     
