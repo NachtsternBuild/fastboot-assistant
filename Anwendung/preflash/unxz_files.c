8784a7659ba264a4951b27e69550b5fbbb6ac457
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-10 13:30:15 +0200

    rework to set outpath

--- Anwendung/preflash/unxz_files.c
+++ Anwendung/preflash/unxz_files.c
@@ -21,22 +21,28 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 #include "function_header.h"
-#include "file_chooser_header.h"
-#include "flash_function_header.h"
 
 // use 'unxz' to decompress .xz files
 void unxz_files(const gchar *xz_filename) 
 {
-    gchar *command = g_strdup_printf("unxz \"%s\"", xz_filename);
+    gchar *target_directory = get_home("~/Downloads/ROM-Install/");
+    if (target_directory == NULL) 
+    {
+        g_print("Fehler beim Erweitern des Verzeichnispfads.\n");
+        return;
+    }
+    
+    gchar *output_path = g_strconcat(target_directory, xz_filename, NULL);
+    gchar *command = g_strdup_printf("unxz -c \"%s\" > \"%s\"", xz_filename, output_path);
     int result = system(command);
     if (result == 0) 
     {
         g_print("Entpacken abgeschlossen: %s\n", xz_filename);
     }
-     
     else 
     {
         g_print("Fehler beim Entpacken der Datei: %s\n", xz_filename);
     }
+    g_free(output_path);
     g_free(command);
 }
