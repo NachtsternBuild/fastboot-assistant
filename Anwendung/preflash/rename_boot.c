c5e4721a8963ecc967aca6f6bd49e65db65a10ab
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-10 14:50:08 +0200

    rework output path

--- Anwendung/preflash/rename_boot.c
+++ Anwendung/preflash/rename_boot.c
@@ -27,8 +27,17 @@
 // rename a file to boot.img
 void rename_boot(const gchar *bo_filename) 
 {
+    gchar *target_directory_boot = get_home("~/Downloads/ROM-Install/");
+    if (target_directory_boot == NULL) 
+    {
+        g_print("Fehler beim Erweitern des Verzeichnispfads.\n");
+        return;
+    }
+    
     const gchar *boot_filename = "boot.img"; 
-    if (rename(bo_filename, boot_filename) == 0) 
+    gchar *target_path_boot = g_strconcat(target_directory_boot, boot_filename, NULL);
+    
+    if (rename(bo_filename, target_path_boot) == 0) 
     {
         g_print("Datei erfolgreich umbenannt: %s -> %s\n", bo_filename, boot_filename);
     } 
@@ -37,4 +46,6 @@ void rename_boot(const gchar *bo_filename)
     {
         g_print("Fehler beim Umbenennen der Datei: %s\n", bo_filename);
     }
+    g_free(target_directory_boot);
+    g_free(target_path_boot);
 }
