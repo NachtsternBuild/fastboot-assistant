17004c2755757901d44460a7df4e13b2425a781b
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-13 14:49:25 +0200

    fix #14

--- Anwendung/flash/flash_data.c
+++ Anwendung/flash/flash_data.c
@@ -65,6 +65,9 @@ void flash_data(int argc, char *argv[])
     gtk_widget_set_size_request(window, 800, 750);
 	gtk_widget_show(window);
 	
+	// Connect close function to 'destroy' signal
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	
 	// make button for every function with label
 	GtkWidget *button_metadata_img = gtk_button_new_with_label("Flash metadata.img");
     GtkWidget *button_userdata_img = gtk_button_new_with_label("Flash userdata.img");
