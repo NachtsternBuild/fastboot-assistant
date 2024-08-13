2f55c02d79e14988bd9e93df99ae377c98711c9e
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-13 14:47:17 +0200

    fix #14

--- Anwendung/flash/flash_recovery.c
+++ Anwendung/flash/flash_recovery.c
@@ -54,6 +54,9 @@ void flash_recovery(int argc, char *argv[])
     gtk_widget_set_size_request(window, 800, 750);
 	gtk_widget_show(window);
 	
+	// Connect close function to 'destroy' signal
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	
 	// make button for every function with label
     GtkWidget *button_recovery_img = gtk_button_new_with_label("Flash Recovery");
     GtkWidget *button_recovery_heimdall = gtk_button_new_with_label("Flash Recovery (heimdall)");
