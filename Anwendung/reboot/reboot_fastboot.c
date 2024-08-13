7b07490e15852234d5ab2a37a497f811374a5d59
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-13 15:11:08 +0200

    fix #14

--- Anwendung/reboot/reboot_fastboot.c
+++ Anwendung/reboot/reboot_fastboot.c
@@ -88,6 +88,9 @@ void reboot_fastboot(int argc, char *argv[])
     gtk_widget_set_size_request(window, 800, 750);
 	gtk_widget_show(window);
 	
+	// Connect close function to 'destroy' signal
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	
 	// make button for every function
     GtkWidget *button_reboot_from_adb = gtk_button_new_with_label("Neustart in Fastboot (von der ADB)");
     GtkWidget *button_reboot_from_fastboot = gtk_button_new_with_label("Neustart in Fastboot (von Fastboot)");
