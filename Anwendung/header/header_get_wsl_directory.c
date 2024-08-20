aa7e81950a2c604cdf5dd5e6aa1b21345e633836
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-20 19:10:38 +0200

    add new header

---
+++ Anwendung/header/header_get_wsl_directory.c
@@ -0,0 +1,37 @@
+/*
+ *-------------------------------------------*
+ *                Projekt 122                *
+ *-------------------------------------------*
+ *  	Apache License, Version 2.0		     *
+ *-------------------------------------------*
+ *                                           *
+ *  Programm um das installieren von 		 *
+ *	Custom-ROM und GSIs auf Android-Geräte 	 *
+ *	zu erleichtern  						 *
+ *                                           *
+ *-------------------------------------------*
+ *      (C) Copyright 2024 Elias Mörz 		 *
+ *-------------------------------------------*
+ *											 *
+ *         Headerpart - get_wsl_directory	 *
+ *											 *
+ *-------------------------------------------*
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include "program_functions.h"
+
+// Function to get the WSL equivalent path
+void get_wsl_directory(char* wsl_dir, size_t size) 
+{
+    const char* user = getenv("USER");
+    if (!user) 
+    {
+        g_print("Fehler: Konnte den Benutzernamen nicht ermitteln.\n");
+        exit(1);
+    }
+    snprintf(wsl_dir, size, "/mnt/c/Users/%s/Downloads/ROM-Install", user);
+}
