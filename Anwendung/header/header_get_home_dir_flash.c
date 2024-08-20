ea8276f93ebfbb195a37fb41b6eb0c6b14dee392
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-20 19:10:08 +0200

    add new header

---
+++ Anwendung/header/header_get_home_dir_flash.c
@@ -0,0 +1,36 @@
+/*
+ *-------------------------------------------*
+ *                Projekt 122                *
+ *-------------------------------------------*
+ *  	Apache License, Version 2.0		     *
+ *-------------------------------------------*
+ *                                           *
+ *  Programm um das installieren von 		 *
+ *	Custom-ROM und GSIs auf Android-Geräte 	 *
+ *	zu erleichtern  						 *
+ *                                           *
+ *-------------------------------------------*
+ *      (C) Copyright 2024 Elias Mörz 		 *
+ *-------------------------------------------*
+ *											 *
+ *         Headerpart - get_home_dir_flash	 *
+ *											 *
+ *-------------------------------------------*
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
+
+// Function to get the user's home directory
+const char* get_home_directory_flash() 
+{
+    const char* home_dir = getenv("HOME");
+    if (!home_dir) 
+    {
+        g_print("Fehler: Konnte das Home-Verzeichnis nicht ermitteln.\n");
+        exit(1);
+    }
+    return home_dir;
+}
