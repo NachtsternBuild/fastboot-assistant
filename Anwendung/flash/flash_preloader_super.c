964c474ca56dcfc4c04d99f0c8537e7f26675664
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-21 17:31:31 +0200

    add reworked function

--- Anwendung/flash/flash_preloader_super.c
+++ Anwendung/flash/flash_preloader_super.c
@@ -22,143 +22,113 @@
 #include "program_functions.h"
 #include "flash_function_header.h"
 
-// button 1 - flash preloader.bin on only-a-devices
-static void preloader_bin_on_a(GtkWidget *widget, gpointer data)
+
+// Button handler functions
+// function to flash preloader.bin on only-a-devices
+void preloader_bin_on_a(GtkWidget *widget, GtkWindow *window)
 {
-    system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash preloader ~/Downloads/ROM-Install/preloader.bin && exit");
+    flash_image(widget, window, "preloader", NULL, "preloader.bin");
 }
 
-// button 2 - flash preloader.bin on a/b-devices
-static void preloader_bin_on_ab(GtkWidget *widget, gpointer data)
+// function to flash preloader.bin (a/b-devices)
+void preloader_bin_on_ab(GtkWidget *widget, GtkWindow *window)
 {
-	system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash preloader_a ~/Downloads/ROM-Install/preloader.bin && fastboot flash preloader_b ~/Downloads/ROM-Install/preloader.bin && exit");
+    flash_image(widget, window, "preloader_a", "preloader_b", "preloader.bin");
 }
 
-// button 3 - flash preloader.img on only-a-devices
-static void preloader_img_on_a(GtkWidget *widget, gpointer data)
+// function to flash preloader.img (only-a-devices)
+void preloader_img_on_a(GtkWidget *widget, GtkWindow *window)
 {
-    system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash preloader ~/Downloads/ROM-Install/preloader.img && exit");
+    flash_image(widget, window, "preloader", NULL, "preloader.img");
 }
 
-// button 4 - flash preloader.img on a/b-devices
-static void preloader_img_on_ab(GtkWidget *widget, gpointer data)
+// function to flash preloader.img (a/b-devices)
+void preloader_img_on_ab(GtkWidget *widget, GtkWindow *window)
 {
-	system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash preloader_a ~/Downloads/ROM-Install/preloader.img && fastboot flash preloader_b ~/Downloads/ROM-Install/preloader.img && exit");
+    flash_image(widget, window, "preloader_a", "preloader_b", "preloader.img");
 }
 
-// button 5 - flash super.img on a/b-devices
-static void super_img(GtkWidget *widget, gpointer data)
+// function to flash super.img
+void super_img(GtkWidget *widget, GtkWindow *window)
 {
-	system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash super ~/Downloads/ROM-Install/super.img && exit");
+    flash_image(widget, window, "super", NULL, "super.img");
 }
 
-// button 6 - flash preloader.bin heimdall
-static void preloader_bin_heimdall(GtkWidget *widget, gpointer data)
+// function to flash preloader.bin (heimdall)
+void preloader_bin_heimdall(GtkWidget *widget, GtkWindow *window)
 {
-	char function_command[255];
-	open_terminal_by_desktop("heimdall flash --PRELOADER ~/Downloads/ROM-Install/preloader.img --no-reboot && exit");
+    flash_heimdall(widget, window, "PRELOADER", "preloader.bin");
 }
 
-// button 7 - flash preloader.img heimdall
-static void preloader_img_heimdall(GtkWidget *widget, gpointer data)
+// function to flash preloader.img (heimdall)
+void preloader_img_heimdall(GtkWidget *widget, GtkWindow *window)
 {
-	char function_command[255];
-	open_terminal_by_desktop("heimdall flash --PRELOADER ~/Downloads/ROM-Install/preloader.img --no-reboot && exit");
+    flash_heimdall(widget, window, "PRELOADER", "preloader.img");
 }
 
-// button 8 - flash super.img heimdall
-static void super_img_heimdall(GtkWidget *widget, gpointer data)
+// function to flash super.img (heimdall)
+void super_img_heimdall(GtkWidget *widget, GtkWindow *window)
 {
-	char function_command[255];
-	open_terminal_by_desktop("heimdall flash --SUPER ~/Downloads/ROM-Install/super.img --no-reboot && exit");
+    flash_heimdall(widget, window, "SUPER", "super.img");
 }
 
-/* start main programm */
+// main function
 void flash_preloader_super(int argc, char *argv[])
 {
-	// int gtk
-	gtk_init(&argc, &argv);
-	
-	// make main window
-	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Flash Preloader/Super");
+    gtk_init(&argc, &argv);
+    
+    // create a windows
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Flash Preloader/Super");
     gtk_container_set_border_width(GTK_CONTAINER(window), 500);
     gtk_widget_set_size_request(window, 800, 750);
-	gtk_widget_show(window);
-	
-	// Connect close function to 'destroy' signal
+
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-	// make button for every function with label
+
+    // create button
     GtkWidget *button_preloader_bin_on_a = gtk_button_new_with_label("Flashen preloader.bin (only-a-devices)");
     GtkWidget *button_preloader_bin_on_ab = gtk_button_new_with_label("Flashen preloader.bin (a/b-devices)");
     GtkWidget *button_preloader_img_on_a = gtk_button_new_with_label("Flashen preloader.img (only-a-devices)");
     GtkWidget *button_preloader_img_on_ab = gtk_button_new_with_label("Flashen preloader.img (a/b-devices)");
-    GtkWidget *button_super_img = gtk_button_new_with_label("Flashen super.img (a/b-devices)");
+    GtkWidget *button_super_img = gtk_button_new_with_label("Flashen super.img");
     GtkWidget *button_preloader_bin_heimdall= gtk_button_new_with_label("Flashen preloader.bin (heimdall)");
     GtkWidget *button_preloader_img_heimdall = gtk_button_new_with_label("Flashen preloader.img (heimdall)");
-    GtkWidget *button_super_img_heimdall = gtk_button_new_with_label("Flashen super.img (heimdall)");
+    GtkWidget *button_super_img_heimdall= gtk_button_new_with_label("Flashen super.img (heimdall)");
     
-    // Link the click callback function with the buttons 
-    g_signal_connect(button_preloader_bin_on_a, "clicked", G_CALLBACK(preloader_bin_on_a), NULL);
-    g_signal_connect(button_preloader_bin_on_ab, "clicked", G_CALLBACK(preloader_bin_on_ab), NULL);
-    g_signal_connect(button_preloader_img_on_a, "clicked", G_CALLBACK(preloader_img_on_a), NULL);
-    g_signal_connect(button_preloader_img_on_ab, "clicked", G_CALLBACK(preloader_img_on_ab), NULL);
-    g_signal_connect(button_super_img, "clicked", G_CALLBACK(super_img), NULL);
-    g_signal_connect(button_preloader_bin_heimdall, "clicked", G_CALLBACK(preloader_bin_heimdall), NULL);
-    g_signal_connect(button_preloader_img_heimdall, "clicked", G_CALLBACK(preloader_img_heimdall), NULL);
-    g_signal_connect(button_super_img_heimdall, "clicked", G_CALLBACK(super_img_heimdall), NULL);
-    
-    // Create a layout container (HBox) for the buttons
+    // connection for gtk callback
+	g_signal_connect(button_preloader_bin_on_a, "clicked", G_CALLBACK(preloader_bin_on_a), (gpointer) window);
+	g_signal_connect(button_preloader_bin_on_ab, "clicked", G_CALLBACK(preloader_bin_on_ab), (gpointer) window);
+	g_signal_connect(button_preloader_img_on_a, "clicked", G_CALLBACK(preloader_img_on_a), (gpointer) window);
+	g_signal_connect(button_preloader_img_on_ab, "clicked", G_CALLBACK(preloader_img_on_ab), (gpointer) window);
+	g_signal_connect(button_super_img, "clicked", G_CALLBACK(super_img), (gpointer) window);
+	g_signal_connect(button_preloader_bin_heimdall, "clicked", G_CALLBACK(preloader_bin_heimdall), (gpointer) window);
+	g_signal_connect(button_preloader_img_heimdall, "clicked", G_CALLBACK(preloader_img_heimdall), (gpointer) window);
+	g_signal_connect(button_super_img_heimdall, "clicked", G_CALLBACK(super_img_heimdall), (gpointer) window);
+	
+    // create widget
     GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    
-    // Create a layout container (VBox) for the left and right buttons
     GtkWidget *left_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
     GtkWidget *right_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
     
-    // Add the first two buttons to the left VBox
     gtk_box_pack_start(GTK_BOX(left_vbox), button_preloader_bin_on_a, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(left_vbox), button_preloader_bin_on_ab, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(left_vbox), button_super_img, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(left_vbox), button_super_img_heimdall, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(left_vbox), preloader_img_on_a, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(left_vbox), preloader_img_on_ab, TRUE, TRUE, 0);
     
-    // Add the other two buttons to the right VBox
-    gtk_box_pack_start(GTK_BOX(right_vbox), button_preloader_img_on_a, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(right_vbox), button_preloader_img_on_ab, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(right_vbox), button_super_img, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(right_vbox), button_super_img_heimdall, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(right_vbox), button_preloader_bin_heimdall, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(right_vbox), button_preloader_img_heimdall, TRUE, TRUE, 0);
     
-     // Add the left and right VBoxes to the main HBox
     gtk_box_pack_start(GTK_BOX(hbox), left_vbox, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(hbox), right_vbox, TRUE, TRUE, 0);
 
-    // Add the main HBox to the main window
+	// add all to the container
     gtk_container_add(GTK_CONTAINER(window), hbox);
 
-    // show all button
-    gtk_widget_show(button_preloader_bin_on_a);
-    gtk_widget_show(button_preloader_bin_on_ab);
-    gtk_widget_show(button_preloader_img_on_a);
-    gtk_widget_show(button_preloader_img_on_ab);
-    gtk_widget_show(button_super_img);
-    gtk_widget_show(button_preloader_bin_heimdall);
-    gtk_widget_show(button_preloader_img_heimdall);
-    gtk_widget_show(button_super_img_heimdall);
-    gtk_widget_show(left_vbox);
-    gtk_widget_show(right_vbox);
-    gtk_widget_show(hbox);
-	
-	// gtk main loop
-	gtk_main();
-}
+    // show all widgets
+    gtk_widget_show_all(window);
     
+    gtk_main();
+}
+
