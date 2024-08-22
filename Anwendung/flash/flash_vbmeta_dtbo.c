9715d8e872f578c43779913968de4345b8ca68ad
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-22 17:28:34 +0200

    rework flash function, fix #20

--- Anwendung/flash/flash_vbmeta_dtbo.c
+++ Anwendung/flash/flash_vbmeta_dtbo.c
@@ -22,110 +22,106 @@
 #include "program_functions.h"
 #include "flash_function_header.h"
 
-// button 1 - flash vbmeta.img on only-a-devices
-static void vbmeta_on_a(GtkWidget *widget, gpointer data)
+// Button handler functions
+// function to flash vbmeta.img on only-a-devices
+void vbmeta_on_a(GtkWidget *widget, GtkWindow *window)
 {
-    system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash --disable-verity --disable-verification vbmeta ~/Downloads/ROM-Install/vbmeta.img && exit");
+    char image_path[512];
+    set_main_dir_with_wsl(image_path, sizeof(image_path), "vbmeta.img");
+
+    char function_command[255];
+    snprintf(function_command, sizeof(function_command), "fastboot flash --disable-verity --disable-verification vbmeta %s && exit", image_path);
+    g_print(function_command);
+    open_terminal_by_desktop(function_command);
 }
 
-// button 2 - flash vbmeta.img on a/b-devices
-static void vbmeta_on_ab(GtkWidget *widget, gpointer data)
+// function to flash vbmeta.img (a/b-devices)
+void vbmeta_on_ab(GtkWidget *widget, GtkWindow *window)
 {
-	system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash --disable-verity --disable-verification vbmeta_a ~/Downloads/ROM-Install/vbmeta.img && fastboot flash --disable-verity --disable-verification vbmeta_b ~/Downloads/ROM-Install/vbmeta.img && exit");
+    char image_path[512];
+    set_main_dir_with_wsl(image_path, sizeof(image_path), "vbmeta.img");
+
+    char function_command[255];
+    snprintf(function_command, sizeof(function_command), "fastboot flash --disable-verity --disable-verification vbmeta_a %s && fastboot flash --disable-verity --disable-verification vbmeta_b %s && exit", image_path, image_path);
+    g_print(function_command);
+    open_terminal_by_desktop(function_command);
 }
 
-// button 3 - flash dtbo.img on only-a-devices
-static void dtbo_on_a(GtkWidget *widget, gpointer data)
+// function to flash dtbo.img (only-a-devices)
+void dtbo_on_a(GtkWidget *widget, GtkWindow *window)
 {
-    system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash dtbo ~/Downloads/ROM-Install/dtbo.img && exit");
+    flash_image(widget, window, "dtbo", NULL, "dtbo.img");
 }
 
-// button 4 - flash dtbo.img on a/b-devices
-static void dtbo_on_ab(GtkWidget *widget, gpointer data)
+// function to flash dtbo.img (a/b-devices)
+void dtbo_on_ab(GtkWidget *widget, GtkWindow *window)
 {
-	system("fastboot devices");
-	char function_command[255];
-	open_terminal_by_desktop("fastboot flash dtbo_a ~/Downloads/ROM-Install/dtbo.img && fastboot flash dtbo_b ~/Downloads/ROM-Install/dtbo.img && exit");
+    flash_image(widget, window, "dtbo_a", "dtbo_b", "dtbo.img");
 }
 
-
-// button 7 - flash vbmeta.img heimdall
-static void vbmeta_img_heimdall(GtkWidget *widget, gpointer data)
+// function to flash vbmeta.img (heimdall)
+void vbmeta_img_heimdall(GtkWidget *widget, GtkWindow *window)
 {
-	char function_command[255];
-	open_terminal_by_desktop("heimdall flash --VBMETA ~/Downloads/ROM-Install/vbmeta.img --no-reboot && exit");
+    flash_heimdall(widget, window, "VBMETA", "vbmeta.img");
 }
 
-/* start main programm */
+// function to flash dtbo.img (heimdall)
+void dtbo_on_ab_heimdall(GtkWidget *widget, GtkWindow *window)
+{
+    flash_heimdall(widget, window, "DTBO", "dtbo.img");
+}
+
+// main function
 void flash_vbmeta_dtbo(int argc, char *argv[])
 {
-	// int gtk
-	gtk_init(&argc, &argv);
-	
-	// make main window
-	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Flash vbmeta/dtbo");
+    gtk_init(&argc, &argv);
+    
+    // create a windows
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Flash vbmeta/dtbo");
     gtk_container_set_border_width(GTK_CONTAINER(window), 500);
     gtk_widget_set_size_request(window, 800, 750);
-	gtk_widget_show(window);
-	
-	// Connect close function to 'destroy' signal
+
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-	// make button for every function with label
-    GtkWidget *button_vbmeta_on_a = gtk_button_new_with_label("Flash vbmeta.img (only-a-devices)");
-    GtkWidget *button_vbmeta_on_ab = gtk_button_new_with_label("Flash vbmeta.img (a/b-devices)");
-    GtkWidget *button_dtbo_on_a = gtk_button_new_with_label("Flash dtbo.img (only-a-devices)");
-    GtkWidget *button_dtbo_on_ab = gtk_button_new_with_label("Flash dtbo.img (a/b-devices)");
-    GtkWidget *button_vbmeta_img_heimdall = gtk_button_new_with_label("Flash vbmeta.img (heimdall)");
+
+    // create button
+    GtkWidget *button_vbmeta_on_a = gtk_button_new_with_label("Flash vbmeta (only-a-devices)");
+    GtkWidget *button_vbmeta_on_ab = gtk_button_new_with_label("Flash vbmeta (a/b-devices)");
+    GtkWidget *button_dtbo_on_a = gtk_button_new_with_label("Flash dtbo (only-a-devices)");
+    GtkWidget *button_dtbo_on_ab = gtk_button_new_with_label("Flash dtbo (a/b-devices)");
+    GtkWidget *button_vbmeta_img_heimdall = gtk_button_new_with_label("Flash vbmeta (heimdall)");
+    GtkWidget *button_dtbo_on_ab_heimdall = gtk_button_new_with_label("Flash dtbo (heimdall)");
     
-    // Link the click callback function with the buttons 
-    g_signal_connect(button_vbmeta_on_a, "clicked", G_CALLBACK(vbmeta_on_a), NULL);
-    g_signal_connect(button_vbmeta_on_ab, "clicked", G_CALLBACK(vbmeta_on_ab), NULL);
-    g_signal_connect(button_dtbo_on_a, "clicked", G_CALLBACK(dtbo_on_a), NULL);
-    g_signal_connect(button_dtbo_on_ab, "clicked", G_CALLBACK(dtbo_on_ab), NULL);
-    g_signal_connect(button_vbmeta_img_heimdall, "clicked", G_CALLBACK(vbmeta_img_heimdall), NULL);
-    
-    // Create a layout container (HBox) for the buttons
+    // connection for gtk callback
+	g_signal_connect(button_vbmeta_on_a, "clicked", G_CALLBACK(vbmeta_on_a), (gpointer) window);
+	g_signal_connect(button_vbmeta_on_ab, "clicked", G_CALLBACK(vbmeta_on_ab), (gpointer) window);
+	g_signal_connect(button_dtbo_on_a, "clicked", G_CALLBACK(dtbo_on_a), (gpointer) window);
+	g_signal_connect(button_dtbo_on_ab, "clicked", G_CALLBACK(dtbo_on_ab), (gpointer) window);
+	g_signal_connect(button_vbmeta_img_heimdall, "clicked", G_CALLBACK(vbmeta_img_heimdall), (gpointer) window);
+	g_signal_connect(button_dtbo_on_ab_heimdall, "clicked", G_CALLBACK(dtbo_on_ab_heimdall), (gpointer) window);
+
+    // create widget
     GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    
-    // Create a layout container (VBox) for the left and right buttons
     GtkWidget *left_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
     GtkWidget *right_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
     
-    // Add the first two buttons to the left VBox
     gtk_box_pack_start(GTK_BOX(left_vbox), button_vbmeta_on_a, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(left_vbox), button_vbmeta_on_ab, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(left_vbox), button_vbmeta_img_heimdall, TRUE, TRUE, 0);
     
-    // Add the other two buttons to the right VBox
     gtk_box_pack_start(GTK_BOX(right_vbox), button_dtbo_on_a, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(right_vbox), button_dtbo_on_ab, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(right_vbox), button_vbmeta_img_heimdall, TRUE, TRUE, 0);
-        
-    // Add the left and right VBoxes to the main HBox
+    gtk_box_pack_start(GTK_BOX(right_vbox), button_dtbo_on_ab_heimdall, TRUE, TRUE, 0);
+    
     gtk_box_pack_start(GTK_BOX(hbox), left_vbox, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(hbox), right_vbox, TRUE, TRUE, 0);
 
-    // Add the main HBox to the main window
+	// add all to the container
     gtk_container_add(GTK_CONTAINER(window), hbox);
 
-    // show all button
-    gtk_widget_show(button_vbmeta_on_a);
-    gtk_widget_show(button_vbmeta_on_ab);
-    gtk_widget_show(button_dtbo_on_a);
-    gtk_widget_show(button_dtbo_on_ab);
-    gtk_widget_show(button_vbmeta_img_heimdall);    
-    gtk_widget_show(left_vbox);
-    gtk_widget_show(right_vbox);
-    gtk_widget_show(hbox);
-	
-	// gtk main loop
-	gtk_main();
-}
+    // show all widgets
+    gtk_widget_show_all(window);
     
+    gtk_main();
+}
+
