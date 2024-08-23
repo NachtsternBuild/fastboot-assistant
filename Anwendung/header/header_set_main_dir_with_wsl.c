cd8ac52e1c52e420cc61010d1ac786a3e8b6b836
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-23 17:08:23 +0200

    rework header to fix #28

--- Anwendung/header/header_set_main_dir_with_wsl.c
+++ Anwendung/header/header_set_main_dir_with_wsl.c
@@ -32,6 +32,9 @@ void set_main_dir_with_wsl(char *path, size_t size, const char *filename)
     get_wsl_directory(wsl_dir, sizeof(wsl_dir));
     
     // Use the Linux path if available, otherwise use the WSL path
+    // But the problem is, 
+    // because the WSL is recognised as Linux the other path is never used, 
+    // so a modified version must be used for windows.
     if (access(home_dir, F_OK) != -1) 
     {
         snprintf(path, size, "%s/Downloads/ROM-Install/%s", home_dir, filename);
@@ -43,3 +46,17 @@ void set_main_dir_with_wsl(char *path, size_t size, const char *filename)
     }
 }
 
+// Use the following function to build the program for windows
+/*
+// Function to set the main directory for the image path
+void set_main_dir_with_wsl(char *path, size_t size, const char *filename) 
+{
+    const char *home_dir = get_home_directory_flash();
+    char wsl_dir[512];
+    get_wsl_directory(wsl_dir, sizeof(wsl_dir));
+    
+    // Use the Linux path if available, otherwise use the WSL path
+    // This is the modified version for Windows
+    snprintf(path, size, "%s/%s", wsl_dir, filename);
+}
+*/
