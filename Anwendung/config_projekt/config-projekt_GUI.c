dc86165c304a3508429fe492b4288d5d0b898b74
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-28 16:48:17 +0200

    rework the UI
    add wsl_config

--- Anwendung/config_projekt/config-projekt_GUI.c
+++ Anwendung/config_projekt/config-projekt_GUI.c
@@ -23,45 +23,43 @@
 #include "function_header.h"
 
 #define MAX_BUFFER_SIZE 256
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
+
 // include all functions
 extern void make_dir();
 extern void remove_old();
+extern void wsl_config();
 
 // Callback functions for each button
 // function without any function
-static void start_zero_function_7(GtkWidget *widget, gpointer data) 
+static void start_make_dir_function(GtkWidget *widget, gpointer data) 
 {
-    printf("Keine Funktion!\n");
+	GtkWidget *dialog;
+    const char *message;
+    message = "Fertig!\n";
+    make_dir();
+    show_message(message);
 }
 
 // function without any function
-static void start_make_dir_function(GtkWidget *widget, gpointer data) 
+static void start_remove_old(GtkWidget *widget, gpointer data) 
 {
-    make_dir();
+    GtkWidget *dialog;
+    const char *message;
+    message = "Fertig!\n";
+    remove_old();
+    show_message(message);
 }
 
 // start make_dir-function
-static void start_zero_function_8(GtkWidget *widget, gpointer data) 
+static void start_wsl_config(GtkWidget *widget, gpointer data) 
 {
-    printf("Keine Funktion!\n");
-}
-
-// function without any function
-static void start_zero_function_9(GtkWidget *widget, gpointer data) 
-{
-    printf("Keine Funktion!\n");
-}
-
-// start lock_unlock_bootloader-function
-static void start_remove_old(GtkWidget *widget, gpointer data) 
-{
-    remove_old();
-}
-
-// function without any function
-static void start_zero_function_10(GtkWidget *widget, gpointer data) 
-{
-    printf("Keine Funktion!\n");
+    GtkWidget *dialog;
+    const char *message;
+    message = "Fertig!\n";
+    wsl_config();
+    show_message(message);
 }
 
 /* main function of preflash_GUI*/
@@ -70,76 +68,59 @@ void config_projekt_GUI(int argc, char *argv[])
     GtkWidget *window;
     GtkWidget *grid;
     GtkWidget *button;
-    char button_labels[6][20] = {" ", "Anlegen Ordner", " ", 
-                                 " ", "Aufräumen", " "};
+    GtkStyleContext *context;
+    
+    char button_labels[3][30] = {"Anlegen Ordner", "Aufräumen", "Konfigurieren"};
 
     gtk_init(&argc, &argv);
-
-    // create new windows
+	
+	css_provider(); // load css-provider
+	
+    // create the window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Zusätze");
+    gtk_window_set_title(GTK_WINDOW(window), "Fastboot-Assistant - Zusätze");
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
-    // create a grid
+    // create the grid and centre it
     grid = gtk_grid_new();
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+    
+    gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+
+    // add the grid to the window
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    // add buttons and connect them with callback
-    for (int i = 0; i < 6; i++) {
+    // add and centre all button
+    for (int i = 0; i < 3; i++) {
         button = gtk_button_new_with_label(button_labels[i]);
         gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
+
+        // execute css-provider for all buttons
+        add_css_provider(button, provider);
+        
         switch (i) {
             case 0:
-                g_signal_connect(button, "clicked", G_CALLBACK(start_zero_function_7), NULL);
-                break;
-            case 1:
                 g_signal_connect(button, "clicked", G_CALLBACK(start_make_dir_function), NULL);
                 break;
-            case 2:
-                g_signal_connect(button, "clicked", G_CALLBACK(start_zero_function_8), NULL);
-                break;
-            case 3:
-                g_signal_connect(button, "clicked", G_CALLBACK(start_zero_function_9), NULL);
-                break;
-            case 4:
+            case 1:
                 g_signal_connect(button, "clicked", G_CALLBACK(start_remove_old), NULL);
                 break;
-            case 5:
-                g_signal_connect(button, "clicked", G_CALLBACK(start_zero_function_10), NULL);
+            case 2:
+                g_signal_connect(button, "clicked", G_CALLBACK(start_wsl_config), NULL);
                 break;
         }
     }
-
-    /* change the style of the buttons to quick settings buttons from android 12 - css is used for this*/
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_data(provider,
-                                    "button {\n"
-                                    "    background-color: #8B0000;\n" /* darkred */
-            						"	 border: none;\n"
-            						"	 border-radius: 12px;\n" /* round corners */
-            						"	 padding: 12px 24px;\n" /* inside distance */
-            						"	 color: #ffffff;\n" /* White text */
-            						"	 font-size: 16px;\n" /* Font size */
-            						"	 font-weight: 500;\n" /* Medium font */
-            						"	 text-align: center;\n" /* Centre text */
-            						"	 box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);\n" /* Light shade */
-            						"	 transition: background-color 0.3s ease, box-shadow 0.3s ease;\n" /* Animation */
-                                    "}\n"
-                                    "button:hover {\n"
-                                    "    background-color: #A52A2A;\n" /* Brighter red on hover */
-            						"	 box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);\n" /* Amplify shadows */
-                                    "}\n",
-                                    -1,
-                                    NULL);
-    GtkStyleContext *context = gtk_widget_get_style_context(window);
-    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    g_object_unref(provider);
 
     // show window
     gtk_widget_show_all(window);
 
     // run main-gtk-loop
     gtk_main();
+    
+    //clean the provider
+    g_object_unref(provider);
 }
 
