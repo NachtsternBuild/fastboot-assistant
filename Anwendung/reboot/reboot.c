f00423c9dab2e51fd4e27130c9f413f2a6b4821f
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-30 18:30:38 +0200

    improve the function

--- Anwendung/reboot/reboot.c
+++ Anwendung/reboot/reboot.c
@@ -10,7 +10,7 @@
  *	zu erleichtern  						 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2024 Elias Mörz 		 *
  *-------------------------------------------*
  *											 *
  *              reboot						 *
@@ -25,40 +25,12 @@
 
 void reboot_system(GtkWidget *widget, gpointer data)
 {
-	GtkWidget *dialog;
     const char *message = "Neustart wird durchgeführt.\n";
     
     show_message(message);
-    
+    // reboot from fastboot
     system("fastboot reboot");
+    
+    const char *message1 = "Fertig!\n";
+    show_message(message1);
 }
-
-void reboot(int argc, char *argv[]) 
-{
-    // Initialize GTK
-    gtk_init(&argc, &argv);
-
-    // Create the main window
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Neustart von Fastboot");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // Create a vertical box
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_container_add(GTK_CONTAINER(window), vbox);
-
-    // Create a button
-    GtkWidget *button_reboot_system = gtk_button_new_with_label("Neustarten");
-    g_signal_connect(button_reboot_system, "clicked", G_CALLBACK(reboot_system), NULL);
-
-    // Pack the button into the box
-    gtk_box_pack_start(GTK_BOX(vbox), button_reboot_system, TRUE, TRUE, 0);
-
-    // Show all the widgets
-    gtk_widget_show_all(window);
-
-    // Run the GTK main loop
-    gtk_main();
-}
-
