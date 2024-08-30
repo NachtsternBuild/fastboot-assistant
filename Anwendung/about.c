5805310512336ceeef3679d53c144fe5cccaa20e
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-30 17:08:32 +0200

    rework the about, fix #27

--- Anwendung/about.c
+++ Anwendung/about.c
@@ -1,7 +1,30 @@
+/*
+ *-------------------------------------------*
+ *                Projekt 122 - GUI          *
+ *-------------------------------------------*
+ *          Apache License, Version 2.0      *
+ *-------------------------------------------*
+ *                                           *
+ *  Programm um das installieren von         *
+ *  Custom-ROM und GSIs auf Android-Geräte   *
+ *  zu erleichtern - about                   *
+ *                                           *
+ *-------------------------------------------*
+ *      (C) Copyright 2024 Elias Mörz        *
+ *-------------------------------------------*
+ *
+ */
+
+/* headers that used in the programm */
 #include <stdio.h>
-#include <gtk/gtk.h>
 #include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
 #include "program_functions.h"
+
+#define MAX_BUFFER_SIZE 256
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 500
 
 // function to open the GitHub website from the fastboot-assistant
 void fastboot_assistant(GtkWidget *widget, gpointer data)
@@ -38,112 +61,124 @@ void brownie_jean(GtkWidget *widget, gpointer data)
     const char *brownie_jean_url = "https://github.com/Jean28518";
 	open_url(brownie_jean_url);
 }
+// function that print so text, but no real useage
+static void about_text()
+{
+	g_print("Nix da!\n");
+}
 
+// the main function
 void about(int argc, char *argv[]) 
 {
-    // Initiate GTK
+    GtkWidget *window;
+    GtkWidget *page_about1, *page_about2, *page_about3;
+    GtkWidget *label_about2;
+    GtkWidget *button_about_1, *button_about_2, *button_about_3, *button_about_4, *button_about_5, *button_about2_1, *button_about2_2, *button_about2_3, *button_about2_4, *button_about3_1, *button_about3_2, *button_about3_3, *button_about3_4, *button_about3_5;
+
     gtk_init(&argc, &argv);
-
-    // Create main window
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    css_provider(); // load css-provider
+    
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Über das Programm");
-    gtk_widget_set_size_request(window, 700, 650);
-
-    // Connect close function to 'destroy' signal
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
+    
+     // Connect close function to 'destroy' signal
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // Frame for each information field
-    GtkWidget *frame1 = gtk_frame_new(NULL);
-    GtkWidget *frame2 = gtk_frame_new(NULL);
-    GtkWidget *frame3 = gtk_frame_new(NULL);
-    GtkWidget *frame4 = gtk_frame_new(NULL);
-    GtkWidget *frame5 = gtk_frame_new(NULL);
-    GtkWidget *frame6 = gtk_frame_new(NULL);
-    GtkWidget *frame7 = gtk_frame_new(NULL);
-    GtkWidget *frame8 = gtk_frame_new(NULL);
+	
+	GtkWidget *notebook = gtk_notebook_new();
+    gtk_container_add(GTK_CONTAINER(window), notebook);
     
-    // Vertical box layout for the frames
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_container_add(GTK_CONTAINER(window), vbox);
+    if (!GTK_IS_NOTEBOOK(notebook)) 
+    {
+    	g_warning("Notebook is not initialized properly.");
+    	return;
+	}
 
-    // Creation of text labels with the text to be displayed
-    const char *name_text = "\nFastboot Assistant\n";
-    GtkWidget *label_name = gtk_label_new(name_text);
-    gtk_container_add(GTK_CONTAINER(frame1), label_name);
+    // page 1
+    page_about1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_about_1 = gtk_button_new_with_label("Fastboot Assistant");
+    button_about_2 = gtk_button_new_with_label("Version 0.5.5.beta");
+    button_about_3 = gtk_button_new_with_label("Sweet Unix System V");
+    button_about_4 = gtk_button_new_with_label("Programm um das installieren von Custom-ROM \nund GSIs auf Android-Geräte zu erleichtern.");
+    button_about_5 = gtk_button_new_with_label("Weiter");
+    gtk_box_pack_start(GTK_BOX(page_about1), button_about_1, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about1), button_about_2, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about1), button_about_3, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about1), button_about_4, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about1), button_about_5, FALSE, FALSE, 0);
+    g_signal_connect(button_about_1, "clicked", G_CALLBACK(about_text), notebook);
+    g_signal_connect(button_about_2, "clicked", G_CALLBACK(about_text), notebook);
+    g_signal_connect(button_about_3, "clicked", G_CALLBACK(about_text), notebook);
+    g_signal_connect(button_about_4, "clicked", G_CALLBACK(about_text), notebook);
+    g_signal_connect(button_about_5, "clicked", G_CALLBACK(next_page), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_about1, gtk_label_new("Version"));
 
-    const char *licence_text = "Apache License, Version 2.0";
-    GtkWidget *label_licence = gtk_label_new(licence_text);
-    gtk_container_add(GTK_CONTAINER(frame2), label_licence);
-
-    const char *information_text = "\nProgramm um das installieren von Custom-ROM und GSIs\n "
-                                "auf Android-Geräte zu erleichtern.\n";
-    GtkWidget *label_information = gtk_label_new(information_text);
-    gtk_container_add(GTK_CONTAINER(frame3), label_information);
-
-    const char *copyright_text = "(C) Copyright 2024 Elias Mörz";
-    GtkWidget *label_copyright = gtk_label_new(copyright_text);
-    gtk_container_add(GTK_CONTAINER(frame4), label_copyright);
+    // run css-provider
+    add_css_provider(button_about_1, provider);
+    add_css_provider(button_about_2, provider);
+    add_css_provider(button_about_3, provider);
+    add_css_provider(button_about_4, provider);
+    add_css_provider(button_about_5, provider);
     
-    const char *github_text = "https://github.com/NachtsternBuild/fastboot-assistant";
-    GtkWidget *label_github = gtk_label_new(github_text);
-    gtk_container_add(GTK_CONTAINER(frame5), label_github);
 
-    const char *build_version_text = "\nVersion 0.5.4.beta\n";
-    GtkWidget *label_build_version = gtk_label_new(build_version_text);
-    gtk_container_add(GTK_CONTAINER(frame6), label_build_version);
+    // page 2
+    page_about2 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_about2_1 = gtk_button_new_with_label("(C) Copyright 2024 Elias Mörz");
+    button_about2_2 = gtk_button_new_with_label("Apache License, Version 2.0");
+    button_about2_3 = gtk_button_new_with_label("https://github.com/NachtsternBuild/fastboot-assistant");
+    label_about2 = gtk_button_new_with_label("thanks to:\n@mrrfv → Open Android Backup\n@Jean28518 → my build-fastboot-assistant.sh \nbased on his build-scripts.");
+    button_about2_4 = gtk_button_new_with_label("Weiter");
+    gtk_box_pack_start(GTK_BOX(page_about2), button_about2_1, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about2), button_about2_2, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about2), button_about2_3, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about2), label_about2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about2), button_about2_4, FALSE, FALSE, 0);
+    g_signal_connect(button_about2_1, "clicked", G_CALLBACK(about_text), notebook);
+    g_signal_connect(button_about2_2, "clicked", G_CALLBACK(about_text), notebook);
+    g_signal_connect(button_about2_3, "clicked", G_CALLBACK(about_text), notebook);
+    g_signal_connect(button_about2_4, "clicked", G_CALLBACK(next_page), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_about2, gtk_label_new("Das Programm"));
 
-    const char *build_name_text = "\nSweet Unix\n";
-    GtkWidget *label_build_name = gtk_label_new(build_name_text);
-    gtk_container_add(GTK_CONTAINER(frame7), label_build_name);
+    // run css-provider
+    add_css_provider(button_about2_1, provider);
+    add_css_provider(button_about2_2, provider);
+    add_css_provider(button_about2_3, provider);
+    add_css_provider(label_about2, provider);
+    add_css_provider(button_about2_4, provider);
+
+    // page 3
+    page_about3 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_about3_1 = gtk_button_new_with_label("GitHub Seite");
+	button_about3_2 = gtk_button_new_with_label("Versionen");
+	button_about3_3 = gtk_button_new_with_label("Feedback");
+	button_about3_4 = gtk_button_new_with_label("Open Android Backup");
+	button_about3_5 = gtk_button_new_with_label("Jean28518 GitHub");
+    gtk_box_pack_start(GTK_BOX(page_about3), button_about3_1, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about3), button_about3_2, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about3), button_about3_3, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about3), button_about3_4, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_about3), button_about3_5, FALSE, FALSE, 0);
+    g_signal_connect(button_about3_1, "clicked", G_CALLBACK(fastboot_assistant), NULL);
+    g_signal_connect(button_about3_2, "clicked", G_CALLBACK(fastboot_assistant_release), NULL);
+    g_signal_connect(button_about3_3, "clicked", G_CALLBACK(feedback), NULL);
+    g_signal_connect(button_about3_4, "clicked", G_CALLBACK(open_android_backup), NULL);
+    g_signal_connect(button_about3_5, "clicked", G_CALLBACK(brownie_jean), NULL);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_about3, gtk_label_new("Hinweise"));
+
+    // run css-provider
+    add_css_provider(button_about3_1, provider);
+    add_css_provider(button_about3_2, provider);
+    add_css_provider(button_about3_3, provider);
+    add_css_provider(button_about3_4, provider);
+    add_css_provider(button_about3_5, provider);
     
-    const char *brownies_text = "\nthanks to:\n@mrrfv → Open Android Backup\n@Jean28518 → my build-fastboot-assistant.sh \nis a modified version.\n";
-    GtkWidget *label_brownies = gtk_label_new(brownies_text);
-    gtk_container_add(GTK_CONTAINER(frame8), label_brownies);
-
-    // Add frames to the main window
-    gtk_box_pack_start(GTK_BOX(vbox), frame1, TRUE, TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), frame2, TRUE, TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), frame3, TRUE, TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), frame4, TRUE, TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), frame5, TRUE, TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), frame6, TRUE, TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), frame7, TRUE, TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), frame8, TRUE, TRUE, 5);
-
-    // Create button box for the buttons
-    GtkWidget *button_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_box_pack_start(GTK_BOX(vbox), button_box, FALSE, FALSE, 10);
-
-    // Create and add buttons to the button box
-    // the button for the website of 'Open Android Backup'
-    GtkWidget *button_open_android_backup = gtk_button_new_with_label("Open Android Backup");
-    g_signal_connect(button_open_android_backup, "clicked", G_CALLBACK(open_android_backup), NULL);
-    gtk_box_pack_start(GTK_BOX(button_box), button_open_android_backup, TRUE, TRUE, 10);
-    
-    // the button for the GitHub website
-    GtkWidget *button_fastboot_assistant = gtk_button_new_with_label("GitHub Seite");
-    g_signal_connect(button_fastboot_assistant, "clicked", G_CALLBACK(fastboot_assistant), NULL);
-    gtk_box_pack_start(GTK_BOX(button_box), button_fastboot_assistant, TRUE, TRUE, 10);
-    
-    // the buttom for the release website
-    GtkWidget *button_fastboot_assistant_release = gtk_button_new_with_label("Versionen");
-    g_signal_connect(button_fastboot_assistant_release, "clicked", G_CALLBACK(fastboot_assistant_release), NULL);
-    gtk_box_pack_start(GTK_BOX(button_box), button_fastboot_assistant_release, TRUE, TRUE, 10);
-
-    // the button for the feedback website
-    GtkWidget *button_feedback = gtk_button_new_with_label("Feedback");
-    g_signal_connect(button_feedback, "clicked", G_CALLBACK(feedback), NULL);
-    gtk_box_pack_start(GTK_BOX(button_box), button_feedback, TRUE, TRUE, 10);
-    
-    // the button for @jean28518 GitHub page 
-    GtkWidget *button_brownie_jean = gtk_button_new_with_label("Jean28518 GitHub");
-    g_signal_connect(button_brownie_jean, "clicked", G_CALLBACK(brownie_jean), NULL);
-    gtk_box_pack_start(GTK_BOX(button_box), button_brownie_jean, TRUE, TRUE, 10);
-
-    // Show all elements
+    // clean the storage
+    g_object_unref(provider);
+	
+	// show all widgets
     gtk_widget_show_all(window);
-
-    // Run the GTK main loop
+	
+	// run gtk mainloop
     gtk_main();
 }
 
