b98c79234cd7b649981f34a33444aba28fe3149d
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-30 18:46:08 +0200

    fix #27

--- Anwendung/reboot/reboot_recovery.c
+++ Anwendung/reboot/reboot_recovery.c
@@ -10,7 +10,7 @@
  *	zu erleichtern  						 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2024 Elias Mörz 		 *
  *-------------------------------------------*
  *											 *
  *              reboot_recovery				 *
@@ -24,28 +24,36 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 
+#define MAX_BUFFER_SIZE 256
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
+
 // Callback functions for each button
 // start reboot_recovery adb-function
 static void start_recovery_adb(GtkWidget *widget, gpointer data) 
 {
-    GtkWidget *dialog;
     const char *title, *message;
-    
     title = "Reboot Recovery";
     message = "Beachten sie, dass USB-Debugging aktiviert ist in den Entwickleroptionen!\n";
+    
     show_message_with_title(title, message);
+    
     system("adb reboot recovery");
+    const char *message1 = "Fertig!\n";
+    show_message(message1);
     
 }
 
 // start reboot_recovery fastboot
 static void start_recovery_fastboot(GtkWidget *widget, gpointer data) 
 {
-    GtkWidget *dialog;
     const char *message = "Beachten sie, dass sich ihr Gerät im Fastboot-Modus befindet!\n";
-    
     show_message(message);
+    
     system("fastboot reboot recovery");
+    
+    const char *message1 = "Fertig!\n";
+    show_message(message1);
 }
 
 /* main function of reboot*/
@@ -54,24 +62,37 @@ void reboot_recovery(int argc, char *argv[])
     GtkWidget *window;
     GtkWidget *grid;
     GtkWidget *button;
-    char button_labels[2][20] = {"Neustart von ADB", "Neustart von Fastboot"};
+    char button_labels[2][30] = {"Neustart von ADB", "Neustart von Fastboot"};
 
     gtk_init(&argc, &argv);
 
-    // create new windows
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Neustarten/Recovery");
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    gtk_init(&argc, &argv);
+    css_provider(); // load css-provider
 
-    // create a grid
+     // create the window
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Neustart Recovery");
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	
+    // create the grid and centre it
     grid = gtk_grid_new();
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+    
+    gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+
+    // add the grid to the window
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    // add buttons and connect them with callback
+    // add and centre all button
     for (int i = 0; i < 2; i++) {
         button = gtk_button_new_with_label(button_labels[i]);
         gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
+
+        // execute css-provider for all buttons
+        add_css_provider(button, provider);
         switch (i) {
             case 0:
                 g_signal_connect(button, "clicked", G_CALLBACK(start_recovery_adb), NULL);
@@ -81,30 +102,7 @@ void reboot_recovery(int argc, char *argv[])
                 break;
         }
     }
-
-    /* change the style of the buttons to quick settings buttons from android 12 - css is used for this*/
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_data(provider,
-                                    "button {\n"
-                                    "    background-color: #8B0000;\n" /* darkred */
-            						"	 border: none;\n"
-            						"	 border-radius: 12px;\n" /* round corners */
-            						"	 padding: 12px 24px;\n" /* inside distance */
-            						"	 color: #ffffff;\n" /* White text */
-            						"	 font-size: 16px;\n" /* Font size */
-            						"	 font-weight: 500;\n" /* Medium font */
-            						"	 text-align: center;\n" /* Centre text */
-            						"	 box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);\n" /* Light shade */
-            						"	 transition: background-color 0.3s ease, box-shadow 0.3s ease;\n" /* Animation */
-                                    "}\n"
-                                    "button:hover {\n"
-                                    "    background-color: #A52A2A;\n" /* Brighter red on hover */
-            						"	 box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);\n" /* Amplify shadows */
-                                    "}\n",
-                                    -1,
-                                    NULL);
-    GtkStyleContext *context = gtk_widget_get_style_context(window);
-    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	// cleaing the provider
     g_object_unref(provider);
 
     // show window
