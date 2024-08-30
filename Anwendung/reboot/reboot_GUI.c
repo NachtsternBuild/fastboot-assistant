bb80fefa61fe5b8e77d831f0c8f8cbea0bec22b2
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-30 18:31:05 +0200

    fix #27

--- Anwendung/reboot/reboot_GUI.c
+++ Anwendung/reboot/reboot_GUI.c
@@ -10,7 +10,7 @@
  *	zu erleichtern - reboot_GUI				 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2024 Elias Mörz 		 *
  *-------------------------------------------*
  *
  */
@@ -21,6 +21,10 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 #include "function_header.h"
+
+#define MAX_BUFFER_SIZE 256
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
 
 // include all functions
 extern void reboot_fastboot();
@@ -50,7 +54,7 @@ static void start_reboot_recovery(GtkWidget *widget, gpointer data)
 // start no function
 static void start_no_function1(GtkWidget *widget, gpointer data) 
 {
-    printf("Nix zu sehen!\n");
+    g_print("Nix zu sehen!\n");
 }
 
 // start reboot-function heimdall
@@ -62,7 +66,7 @@ static void start_reboot_heimdall(GtkWidget *widget, gpointer data)
 // start no function
 static void start_no_function2(GtkWidget *widget, gpointer data) 
 {
-    printf("Nix zu sehen!\n");
+    g_print("Nix zu sehen!\n");
 }
 
 void reboot_GUI(int argc, char *argv[]) 
@@ -70,25 +74,37 @@ void reboot_GUI(int argc, char *argv[])
     GtkWidget *window;
     GtkWidget *grid;
     GtkWidget *button;
-    char button_labels[6][20] = {"Neustart Fastboot", "Neustart", "Neustart in Recovery", 
+    char button_labels[6][30] = {"Neustart Fastboot", "Neustart", "Neustart in Recovery", 
                                  " ", "Neustart Heimdall", " "};
 
     gtk_init(&argc, &argv);
+    css_provider(); // load css-provider
 
-    // create new windows
+     // create the window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Neustart");
+    gtk_window_set_title(GTK_WINDOW(window), "Fastboot-Assistant - Neustart");
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-
-    // create a grid
+	
+    // create the grid and centre it
     grid = gtk_grid_new();
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+    
+    gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+
+    // add the grid to the window
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    // add buttons and connect them with callback
+    // add and centre all button
     for (int i = 0; i < 6; i++) {
         button = gtk_button_new_with_label(button_labels[i]);
         gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
+
+        // execute css-provider for all buttons
+        add_css_provider(button, provider);
+        
         switch (i) {
             case 0:
                 g_signal_connect(button, "clicked", G_CALLBACK(start_reboot_fastboot), NULL);
@@ -110,30 +126,7 @@ void reboot_GUI(int argc, char *argv[])
                 break;            
         }
     }
-
-    /* change the style of the buttons to quick settings buttons from android 12 - css is used for this*/
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_data(provider,
-                                    "button {\n"
-                                    "    background-color: #8B0000;\n" /* darkred */
-            						"	 border: none;\n"
-            						"	 border-radius: 12px;\n" /* round corners */
-            						"	 padding: 12px 24px;\n" /* inside distance */
-            						"	 color: #ffffff;\n" /* White text */
-            						"	 font-size: 16px;\n" /* Font size */
-            						"	 font-weight: 500;\n" /* Medium font */
-            						"	 text-align: center;\n" /* Centre text */
-            						"	 box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);\n" /* Light shade */
-            						"	 transition: background-color 0.3s ease, box-shadow 0.3s ease;\n" /* Animation */
-                                    "}\n"
-                                    "button:hover {\n"
-                                    "    background-color: #A52A2A;\n" /* Brighter red on hover */
-            						"	 box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);\n" /* Amplify shadows */
-                                    "}\n",
-                                    -1,
-                                    NULL);
-    GtkStyleContext *context = gtk_widget_get_style_context(window);
-    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	// cleaning the provider
     g_object_unref(provider);
 
     // show window
