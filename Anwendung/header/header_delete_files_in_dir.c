679c7143fbdf2a3238dc0a1c1db23a94646c8aa5
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-31 11:18:54 +0200

    fix some errors

--- Anwendung/header/header_delete_files_in_dir.c
+++ Anwendung/header/header_delete_files_in_dir.c
@@ -10,10 +10,10 @@
  *	zu erleichtern  						 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2024 Elias Mörz 		 *
  *-------------------------------------------*
  *											 *
- *        Headerpart - delete files in dir	 *
+ *        Headerpart - delete_files_in_dir	 *
  *											 *
  *-------------------------------------------*
  */
@@ -39,9 +39,11 @@ void delete_files_in_dir(const char *path)
     }
 
     // read dir
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL) 
+    {
         // skip '.' and '..'
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) 
+        {
             continue;
         }
 
@@ -49,10 +51,14 @@ void delete_files_in_dir(const char *path)
         snprintf(fullpath, sizeof(fullpath), "%s/%s", path, entry->d_name);
 
         // delet files
-        if (remove(fullpath) != 0) {
+        if (remove(fullpath) != 0) 
+        {
             perror("Fehler beim Löschen der Datei");
-        } else {
-            printf("Datei gelöscht: %s\n", fullpath);
+        } 
+        
+        else 
+        {
+            g_print("Datei gelöscht: %s\n", fullpath);
         }
     }
 
