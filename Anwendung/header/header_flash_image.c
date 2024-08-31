ccb8e182f56b5db2c32b807112939669a03508e9
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-08-31 11:11:09 +0200

    fix #35

--- Anwendung/header/header_flash_image.c
+++ Anwendung/header/header_flash_image.c
@@ -25,13 +25,16 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 
-// function to flash images
-void flash_image(GtkWidget *widget, GtkWindow *parent_window, const char *partition1, const char *partition2, const char *image_name) {
-    char image_path[512];
-    // Set the path to the image file taking into account the WSL environment
+// Function to flash images, utilizing the modified path handling for WSL
+void flash_image(GtkWidget *widget, GtkWindow *parent_window, const char *partition1, const char *partition2, const char *image_name) 
+{
+    char image_path[2048];
+    char windows_image_path[2048];
+    
+    // Set the path to the image file considering WSL
     set_main_dir_with_wsl(image_path, sizeof(image_path), image_name);
 
-    // check if image exsists
+    // Check if the image exists
     if (access(image_path, F_OK) == -1) 
     {
         char error_message[256];
@@ -39,51 +42,56 @@ void flash_image(GtkWidget *widget, GtkWindow *parent_window, const char *partit
         show_error_message(GTK_WIDGET(parent_window), error_message);
         return;
     }
-    
-    // get fastboot devices
+
+    // Convert the WSL path to Windows path
+    convert_wsl_path(windows_image_path, sizeof(windows_image_path), image_path);
+
+    // Get fastboot devices
     system("fastboot devices");
-    
+
     char function_command[512];
-    
-    // create a command for wsl
+
+    // Check if running inside WSL
     if (system("grep -q Microsoft /proc/version") == 0) 
     {
-        // use the cmd.exe to flash
+        // Use cmd.exe to execute fastboot commands from WSL with converted Windows path
         if (partition2) 
         {
-            snprintf(function_command, sizeof(function_command), 
-                     "cmd.exe /C start cmd.exe /K \"fastboot flash %s %s && fastboot flash %s %s\"", 
-                     partition1, image_path, partition2, image_path);
+            snprintf(function_command, sizeof(function_command),
+                     "cmd.exe /C start cmd.exe /K \"fastboot flash %s %s && fastboot flash %s %s\"",
+                     partition1, windows_image_path, partition2, windows_image_path);
         } 
         
         else 
         {
-            snprintf(function_command, sizeof(function_command), 
-                     "cmd.exe /C start cmd.exe /K \"fastboot flash %s %s\"", 
-                     partition1, image_path);
+            snprintf(function_command, sizeof(function_command),
+                     "cmd.exe /C start cmd.exe /K \"fastboot flash %s %s\"",
+                     partition1, windows_image_path);
         }
     } 
     
     else 
     {
-        // for native linux
+        // For native Linux environment
         if (partition2) 
         {
-            snprintf(function_command, sizeof(function_command), 
-                     "fastboot flash %s %s && fastboot flash %s %s && exit", 
+            snprintf(function_command, sizeof(function_command),
+                     "fastboot flash %s %s && fastboot flash %s %s && exit",
                      partition1, image_path, partition2, image_path);
         } 
         
         else 
         {
-            snprintf(function_command, sizeof(function_command), 
+            snprintf(function_command, sizeof(function_command),
                      "fastboot flash %s %s && exit", partition1, image_path);
         }
     }
 
-    // debuggins
+    // Debug output
     g_print("Executing: %s\n", function_command);
-    // run the command
+    // Run the command using the appropriate terminal
     open_terminal_by_desktop(function_command);
 }
+
+
 
