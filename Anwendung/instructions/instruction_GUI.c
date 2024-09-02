3b50c599b8052009782509907cd44083a69b6e4d
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-02 17:41:44 +0200

    fix #27

--- Anwendung/instructions/instruction_GUI.c
+++ Anwendung/instructions/instruction_GUI.c
@@ -10,7 +10,7 @@
  *	zu erleichtern - instruction_GUI		 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2024 Elias Mörz 		 *
  *-------------------------------------------*
  *
  */
@@ -24,6 +24,9 @@
 #include "instruction_header.h"
 
 #define MAX_BUFFER_SIZE 256
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
+
 // include all functions
 extern void instruction_adb();
 extern void instruction_flash();
@@ -49,22 +52,10 @@ static void start_instruction_prepare_flash(GtkWidget *widget, gpointer data)
     instruction_prepare_flash();
 }
 
-// function without any function
-static void start_zero_function_3(GtkWidget *widget, gpointer data) 
-{
-    printf("Keine Funktion!\n");
-}
-
 // start instruction_info-function
 static void start_instruction_info(GtkWidget *widget, gpointer data) 
 {
     instruction_info();
-}
-
-// function without any function
-static void start_zero_function_4(GtkWidget *widget, gpointer data) 
-{
-    printf("Keine Funktion!\n");
 }
 
 /* main function of flash_GUI*/
@@ -73,25 +64,36 @@ void instruction_GUI(int argc, char *argv[])
     GtkWidget *window;
     GtkWidget *grid;
     GtkWidget *button;
-    char button_labels[6][20] = {"Umgang mit ADB", "Flash Images", "Vorbereitungen", 
-                                 " ", "Info", " "};
+    char button_labels[4][20] = {"Umgang mit ADB", "Flash Images", "Vorbereitungen", "Info"};
 
     gtk_init(&argc, &argv);
+    css_provider(); // load css-provider
 
-    // create new windows
+     // create the window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Projekt 122 - Anleitungen");
+    gtk_window_set_title(GTK_WINDOW(window), "Fastboot-Assistant - Anleitungen");
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-
-    // create a grid
+	
+    // create the grid and centre it
     grid = gtk_grid_new();
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+    
+    gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+
+    // add the grid to the window
     gtk_container_add(GTK_CONTAINER(window), grid);
 
-    // add buttons and connect them with callback
-    for (int i = 0; i < 6; i++) {
+    // add and centre all button
+    for (int i = 0; i < 4; i++) {
         button = gtk_button_new_with_label(button_labels[i]);
-        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 2, i / 2, 1, 1);
+
+        // execute css-provider for all buttons
+        add_css_provider(button, provider);
+        
         switch (i) {
             case 0:
                 g_signal_connect(button, "clicked", G_CALLBACK(start_instruction_adb), NULL);
@@ -103,40 +105,11 @@ void instruction_GUI(int argc, char *argv[])
                 g_signal_connect(button, "clicked", G_CALLBACK(start_instruction_prepare_flash), NULL);
                 break;
             case 3:
-                g_signal_connect(button, "clicked", G_CALLBACK(start_zero_function_3), NULL);
-                break;
-            case 4:
                 g_signal_connect(button, "clicked", G_CALLBACK(start_instruction_info), NULL);
-                break;
-            case 5:
-                g_signal_connect(button, "clicked", G_CALLBACK(start_zero_function_4), NULL);
                 break;
         }
     }
-
-    /* change the style of the buttons to quick settings buttons from android 12 - css is used for this*/
-    GtkCssProvider *provider = gtk_css_provider_new();
-    gtk_css_provider_load_from_data(provider,
-                                    "button {\n"
-                                    "    background-color: #8B0000;\n" /* darkred */
-            						"	 border: none;\n"
-            						"	 border-radius: 12px;\n" /* round corners */
-            						"	 padding: 12px 24px;\n" /* inside distance */
-            						"	 color: #ffffff;\n" /* White text */
-            						"	 font-size: 16px;\n" /* Font size */
-            						"	 font-weight: 500;\n" /* Medium font */
-            						"	 text-align: center;\n" /* Centre text */
-            						"	 box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);\n" /* Light shade */
-            						"	 transition: background-color 0.3s ease, box-shadow 0.3s ease;\n" /* Animation */
-                                    "}\n"
-                                    "button:hover {\n"
-                                    "    background-color: #A52A2A;\n" /* Brighter red on hover */
-            						"	 box-shadow: 0 6px 8px rgba(0, 0, 0, 0.15);\n" /* Amplify shadows */
-                                    "}\n",
-                                    -1,
-                                    NULL);
-    GtkStyleContext *context = gtk_widget_get_style_context(window);
-    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	// clean the provider
     g_object_unref(provider);
 
     // show window
