81605e6af4f9d7b9525e0b2e162555a196c88849
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-03 15:59:28 +0200

    fix #27

--- Anwendung/instructions/instruction_info.c
+++ Anwendung/instructions/instruction_info.c
@@ -10,12 +10,10 @@
  *	zu erleichtern - instruction_info		 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2024 Elias Mörz 		 *
  *-------------------------------------------*
  *
  */
-
-// add unlock bootloader
 
 /* headers that used in the programm */
 #include <stdio.h>
@@ -23,49 +21,128 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 
+#define MAX_BUFFER_SIZE 256
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 400
+
+static void info_text()
+{
+	g_print("Nischt da!\n");
+}
+
 void instruction_info(int argc, char *argv[]) 
 {
-    // Initiate GTK
+	GtkWidget *window;
+    GtkWidget *page_info1, *page_info2, *page_info3, *page_info4;
+    GtkWidget *label_info1_1, *label_info1_2, *label_info2_1, *label_info2_2, *label_info3_1, *label_info3_2, *label_info4_1, *label_info4_2, *label_info4_3;
+    GtkWidget *button_info1, *button_info2, *button_info3, *button_info4, *button_info5, *button_info6, *button_info7;
+
     gtk_init(&argc, &argv);
-
-    // Create main window
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Anleitung Info");
-    gtk_widget_set_size_request(window, 1000, 950);
-
-    // Connect close function to 'destroy' signal
+    css_provider(); // load css-provider
+    
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Weitere Infos");
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
+    
+     // Connect close function to 'destroy' signal
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // Vertical box layout for the frames
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_container_add(GTK_CONTAINER(window), vbox);
+	
+	GtkWidget *notebook = gtk_notebook_new();
+    gtk_container_add(GTK_CONTAINER(window), notebook);
     
-    // show-text for Samsung devices
-    GtkWidget *inst27_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst27_label), "<b><u>Für Samsung Nutzer</u></b>");
-    gtk_box_pack_start(GTK_BOX(vbox), inst27_label, FALSE, FALSE, 5);
+    if (!GTK_IS_NOTEBOOK(notebook)) 
+    {
+    	g_warning("Notebook is not initialized properly.");
+    	return;
+	}
+
+    // page 1
+    page_info1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_info1 = gtk_button_new_with_label("Project Treble");
+    label_info1_1 = gtk_label_new("Project Treble ist eine Android-Initiative von Google, \ndie Betriebssystem- und Hardwarekomponenten trennt.");
+    label_info1_2 = gtk_label_new("Dadurch können Hersteller leichter Updates bereitstellen, \nda das OS unabhängig von chipsatzspezifischen Treibern ist.");
+    button_info2 = gtk_button_new_with_label("Weiter");
+    gtk_box_pack_start(GTK_BOX(page_info1), button_info1, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info1), label_info1_1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info1), label_info1_2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info1), button_info2, FALSE, FALSE, 0);
+    g_signal_connect(button_info1, "clicked", G_CALLBACK(info_text), notebook);
+    g_signal_connect(button_info2, "clicked", G_CALLBACK(next_page), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_info1, gtk_label_new("Version"));
+
+    // run css-provider
+    add_css_provider(button_info1, provider);
+    add_css_provider(label_info1_1, provider);
+    add_css_provider(label_info1_2, provider);
+    add_css_provider(button_info2, provider);
     
-    // show-text for Samsung devices
-    GtkWidget *inst28_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst28_label), "<b>Nutzen sie 'heimdall' statt 'fastboot' zum flashen!</b>");
-    gtk_box_pack_start(GTK_BOX(vbox), inst28_label, FALSE, FALSE, 5);
 
+    // page 2
+    page_info2 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_info3 = gtk_button_new_with_label("a/b Partitionierung");
+    label_info2_1 = gtk_label_new("Das A/B-Partitionsschema nutzt zwei Systempartitionen \nfür nahtlose Updates durch Wechsel, \nbietet höhere Sicherheit und kontinuierlichen Betrieb.");
+    label_info2_2 = gtk_label_new("Das Only-A-Schema hat nur eine Partition, \nermöglicht einfachere Verwaltung, \naber ist anfälliger für Fehler während des Updates.");
+    button_info4 = gtk_button_new_with_label("Weiter");
+    gtk_box_pack_start(GTK_BOX(page_info2), button_info3, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info2), label_info2_1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info2), label_info2_2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info2), button_info4, FALSE, FALSE, 0);
+    g_signal_connect(button_info3, "clicked", G_CALLBACK(info_text), notebook);
+    g_signal_connect(button_info4, "clicked", G_CALLBACK(next_page), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_info2, gtk_label_new("Das Programm"));
 
-    // show-text for more info title
-    GtkWidget *inst26_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst26_label), "<b><u>Weitere Infos</u></b>");
-    gtk_box_pack_start(GTK_BOX(vbox), inst26_label, FALSE, FALSE, 5);
-
-    // Frame and label for detailed information
-    GtkWidget *frame27 = gtk_frame_new(NULL);
-    const char *info_text = "A/B Partitionsschema verwendet zwei Systempartitionen, \num nahtlose Updates zu ermöglichen, \nindem es zwischen ihnen wechselt. \nDas only-A Partitionsschema \nhat nur eine Systempartition und erfordert einen Neustart für Updates. \nA/B bietet redundante Sicherheit und ermöglicht ein \nkontinuierliches Betriebssystem, während Only-A \neinfachere Verwaltung erfordert.\n \nProject Treble ist eine Android-Initiative von Google, \ndie die Trennung von Betriebssystem- und Hardwarekomponenten ermöglicht. \nDies erleichtert Herstellern \ndie Bereitstellung von Android-Updates, \nindem das Betriebssystem unabhängig \nvon den spezifischen Treibern für den Chipsatz ist.\n \nSystem-as-root ist ein Mechanismus in Android, \nbei dem das System-Image als Root-Dateisystem gemountet wird. \nDies verbessert die Trennung von System- und Vendor-Partitionen \nund erhöht die Sicherheit. \nDies erleichtert das Aktualisieren des Systems \nund die Verwaltung von Berechtigungen, \nda systemkritische Dateien \nund Konfigurationen besser geschützt und isoliert werden.\n \nEin System on a Chip (SoC) ist ein integrierter Schaltkreis, \nder alle wesentlichen Komponenten eines Computersystems, \neinschließlich CPU, GPU, Speicher und Ein-/Ausgabeschnittstellen, \nauf einem einzigen Chip vereint. \nSoCs werden häufig verwendet, \num Platz und Energie zu sparen.";
-    GtkWidget *label_info = gtk_label_new(info_text);
-    gtk_container_add(GTK_CONTAINER(frame27), label_info);
-    gtk_box_pack_start(GTK_BOX(vbox), frame27, TRUE, TRUE, 5);
+    // run css-provider
+    add_css_provider(button_info3, provider);
+    add_css_provider(label_info2_1, provider);
+    add_css_provider(label_info2_2, provider);
+    add_css_provider(button_info4, provider);
     
-    // Show all elements
+    // page 3
+    page_info3 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_info5 = gtk_button_new_with_label("System-as-root");
+	label_info3_1 = gtk_label_new("System-as-root ist ein Android-Mechanismus, \nbei dem das System-Image als Root-Dateisystem gemountet wird.");
+	label_info3_2 = gtk_label_new("Dies verbessert die Trennung von System- und Vendor-Partitionen, \nerhöht die Sicherheit und erleichtert System-Updates \nsowie die Verwaltung von Berechtigungen.");
+	button_info6 = gtk_button_new_with_label("Weiter");
+    gtk_box_pack_start(GTK_BOX(page_info3), button_info5, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info3), label_info3_1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info3), label_info3_2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info3), button_info6, FALSE, FALSE, 0);
+    g_signal_connect(button_info5, "clicked", G_CALLBACK(info_text), notebook);
+    g_signal_connect(button_info6, "clicked", G_CALLBACK(next_page), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_info3, gtk_label_new("System-as-root"));
+    
+    // run css-provider
+    add_css_provider(button_info5, provider);
+    add_css_provider(label_info3_1, provider);
+    add_css_provider(label_info3_2, provider);
+    add_css_provider(button_info6, provider);
+
+    // page 4
+    page_info4 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_info7 = gtk_button_new_with_label("Kernel");
+	label_info4_1 = gtk_label_new("Der Kernel ist der Kern eines Betriebssystems, \nder direkt mit der Hardware kommuniziert und Ressourcen wie CPU, \nSpeicher und Geräte verwaltet.");
+	label_info4_2 = gtk_label_new("Der Kernel bildet die Schnittstelle zwischen Hardware, \nAnwendungen und dem Rest des Systems und sorgt für eine \nsichere und effiziente Ausführung.");
+	label_info4_3 = gtk_label_new("Android-Versionen sind nur mit bestimmten \nKernel-Versionen kompatibel; bei Inkompatibilität startet Android nicht.");
+    gtk_box_pack_start(GTK_BOX(page_info4), button_info7, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info4), label_info4_1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info4), label_info4_2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_info4), label_info4_3, TRUE, TRUE, 0);
+    g_signal_connect(button_info7, "clicked", G_CALLBACK(info_text), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_info4, gtk_label_new("Kernel"));
+    
+
+    // run css-provider
+    add_css_provider(button_info7, provider);
+    add_css_provider(label_info4_1, provider);
+    add_css_provider(label_info4_2, provider);
+    add_css_provider(label_info4_3, provider);
+    
+    // clean the storage
+    g_object_unref(provider);
+	
+	// show all widgets
     gtk_widget_show_all(window);
-
-    // Run the GTK main loop
+	
+	// run gtk mainloop
     gtk_main();
 }
