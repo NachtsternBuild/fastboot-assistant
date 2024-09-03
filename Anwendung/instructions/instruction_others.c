c3898bf6eaf2cf9c12ba1ee70f6a0a73b7508380
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-03 11:08:44 +0200

    fix #36

--- Anwendung/instructions/instruction_others.c
+++ Anwendung/instructions/instruction_others.c
@@ -59,7 +59,7 @@ void instruction_others(int argc, char *argv[])
     // page 1
     page_others1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     button_others_1 = gtk_button_new_with_label("dtbo");
-    label_others_1 = gtk_label_new("Dabei handelt es sich um eine Partition, \ndie den Gerätebaum des Gerätes gespeichert ist. \nDieser ist essentziell für die Kommunikation der Hardware \nmit dem Bootloader und dem Kernel.");
+    label_others_1 = gtk_label_new("Dabei handelt es sich um eine Partition, \ndie den Gerätebaum des Gerätes gespeichert ist. \nDieser ist essentiell für die Kommunikation der Hardware \nmit dem Bootloader und dem Kernel.");
     button_others_2 = gtk_button_new_with_label("Weiter");
     gtk_box_pack_start(GTK_BOX(page_others1), button_others_1, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(page_others1), label_others_1, TRUE, TRUE, 0);
@@ -77,10 +77,10 @@ void instruction_others(int argc, char *argv[])
     // page 2
     page_others2 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     button_others_3 = gtk_button_new_with_label("Super");
-	label_others_1 = gtk_label_new("Dabei handelt es sich um eine Partition, die alle \nanderen Partitionen auf Geräten mit dynamischen Partitionen umfasst.");
+	label_others_2 = gtk_label_new("Dabei handelt es sich um eine Partition, die alle \nanderen Partitionen auf Geräten mit dynamischen Partitionen umfasst.");
     button_others_4 = gtk_button_new_with_label("Weiter");
     gtk_box_pack_start(GTK_BOX(page_others2), button_others_3, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(page_others1), label_others_2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_others2), label_others_2, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(page_others2), button_others_4, FALSE, FALSE, 0);
     g_signal_connect(button_others_3, "clicked", G_CALLBACK(others_text), notebook);
     g_signal_connect(button_others_4, "clicked", G_CALLBACK(next_page), notebook);
@@ -94,7 +94,7 @@ void instruction_others(int argc, char *argv[])
     // page 2
     page_others3 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     button_others_5 = gtk_button_new_with_label("vbmeta");
-	label_others_3 = gtk_label_new("Diese Partition dient der Kontrolle des Startvorgangs \ndes Androids. 'vbmeta' soll sicherstellen, \ndass der Startvorgang sicher abläuft. \nManche Custom-ROMs lassen sich nur \nstarten wenn ein neues vbmeta-Abbild geflasht wurde.");
+	label_others_3 = gtk_label_new("'vbmeta' stellt sicher, dass der Bootvorgang von \nStock-Android sicher abläuft. Manche Custom-ROMs lassen \nsich nur starten wenn ein neues vbmeta-Image geflasht wurde.");
     gtk_box_pack_start(GTK_BOX(page_others3), button_others_5, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(page_others3), label_others_3, TRUE, TRUE, 0);
     g_signal_connect(button_others_5, "clicked", G_CALLBACK(others_text), notebook);
