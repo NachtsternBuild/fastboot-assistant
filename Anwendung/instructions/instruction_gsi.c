41d2fdb6118402f31e86eb646bb7ba2805e0e97e
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-09 11:34:38 +0200

    fix #27

--- Anwendung/instructions/instruction_gsi.c
+++ Anwendung/instructions/instruction_gsi.c
@@ -21,59 +21,117 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 
-void instruction_gsi(int argc, char *argv[]) 
+static void gsi_text()
 {
-    // Initiate GTK
-    gtk_init(&argc, &argv);
+	g_print("Bringt auch nichts!\n");
+}
 
-    // Create main window
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Anleitung Generic System Image (GSI)");
-    gtk_widget_set_size_request(window, 1000, 950);
-
-    // Connect close function to 'destroy' signal
+void instruction_gsi(int argc, char *argv[]) 
+{	
+	GtkWidget *window;
+    GtkWidget *page_gsi1, *page_gsi2, *page_gsi3, *page_gsi4;
+    GtkWidget *label_gsi1_1, *label_gsi1_2, *label_gsi2_1, *label_gsi2_2, *label_gsi2_3, *label_gsi3_1, *label_gsi4_1;
+    GtkWidget *button_gsi1, *button_gsi2, *button_gsi3, *button_gsi4, *button_gsi5, *button_gsi6, *button_gsi7;
+    
+	gtk_init(&argc, &argv);
+    css_provider(); // load css-provider
+    
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Generic System Image");
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
+    
+     // Connect close function to 'destroy' signal
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // Vertical box layout for the frames
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_container_add(GTK_CONTAINER(window), vbox);
-
-    // show-text for GSI title
-    GtkWidget *inst10_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst10_label), "<b><u>Generic System Image (GSI)</u></b>");
-    gtk_box_pack_start(GTK_BOX(vbox), inst10_label, FALSE, FALSE, 5);
-
-    // Frame and label for GSI description
-    GtkWidget *frame18 = gtk_frame_new(NULL);
-    GtkWidget *label_gsi = gtk_label_new("\nGSI (Generic System Image) ist ein unverändertes Android-System-Image, \ndass eine Unterstützung von Project Treble voraussetzt. \nProject Treble ist ein Projekt des \nAndroid Open Source Project (AOSP), \ndass die hersteller- und gerätspezifischen Betriebssystemteile \nvon dem Rest des Betriebssystems trennt. \nEs dient dazu, Entwicklern und Herstellern die Möglichkeit zu geben, \nneue Android-Versionen und Funktionen zu testen, \nohne auf gerätespezifische Anpassungen erfolgen müssen. \nAußerdem ermöglicht stellt das GSI \neine Möglichkeit Android-Geräten ohne Custom-ROMs \nUnterstützung das manuelle aufspielen neuer Android-Versionen.\n");
-    gtk_container_add(GTK_CONTAINER(frame18), label_gsi);
-    gtk_box_pack_start(GTK_BOX(vbox), frame18, TRUE, TRUE, 5);
-
-    // show-text for Kernel title
-    GtkWidget *inst11_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst11_label), "<b><u>Kernel</u></b>");
-    gtk_box_pack_start(GTK_BOX(vbox), inst11_label, FALSE, FALSE, 5);
-
-    // Frame and label for Kernel description
-    GtkWidget *frame19 = gtk_frame_new(NULL);
-    GtkWidget *label_kernel = gtk_label_new("\nDer Kernel stellt den zentralen Bestandteil \neines Betriebssystems dar. \nEr kommuniziert direkt mit der Hardware und verwaltet \ngrundlegende Systemressourcen wie CPU, Speicher und Geräte. \nEr stellt eine Schnittstelle zwischen der Hardware \nund den Anwendungsprogrammen \nund dem Rest des Betriebssystemsbereit, \num eine effiziente und sichere Ausführung zu gewährleisten.\n Verschiedene Android-Versionen nur mit \neiner Reihe an Kernel-Versionen kompatibel. \nSollte das nicht der Fall sein, \nstartet das Android gar nicht erst.\n");
-    gtk_container_add(GTK_CONTAINER(frame19), label_kernel);
-    gtk_box_pack_start(GTK_BOX(vbox), frame19, TRUE, TRUE, 5);
-
-    // show-text for Flashing GSI title
-    GtkWidget *inst12_label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(inst12_label), "<b><u>Flashen eines GSI</u></b>");
-    gtk_box_pack_start(GTK_BOX(vbox), inst12_label, FALSE, FALSE, 5);
-
-    // Frame and label for Flashing GSI steps
-    GtkWidget *frame20 = gtk_frame_new(NULL);
-    GtkWidget *label_flash_gsi = gtk_label_new("\n1. Prüfen sie ob ihr Gerät mit Project Treble kompatibel ist. \n2. Laden sie ein entsprechendes GSI herunter. \n3. Vorbereiten sie die Images für den Flash. \n4. Booten sie ihr Gerät in Fastboot. \n5. Öffnen sie den Bootloader.\nManche Geräte unterstützen nicht \ndie Methode über Fastboot. \nInformieren sie sich genauer über ihr Chipssatz und das Gerät. \n \nAls Methoden des Flashs gibt es die \nAlternativen 'dirty flash'(1) und 'clean flash'(2). \nBei einem 'dirty flash' bleiben die Nutzerdaten erhalten, \naber nicht jedes GSI startet danach. \nWählen sie eine Variante. \n6.1. Führen sie einen flash des System-Images durch. \n7.1. Starten sie ihr Gerät neu. \n \n6.2. Löschen sie alle Nutzerdaten. \n7.2. Führen sie flash des System-Images aus. \n \n8. Flashen sie im Bedarfsfall ein vbmeta.img. \n9. Starten sie ihr Gerät neu.\n");
-    gtk_container_add(GTK_CONTAINER(frame20), label_flash_gsi);
-    gtk_box_pack_start(GTK_BOX(vbox), frame20, TRUE, TRUE, 5);
-
-    // Show all elements
+	
+	GtkWidget *notebook = gtk_notebook_new();
+    gtk_container_add(GTK_CONTAINER(window), notebook);
+    
+    if (!GTK_IS_NOTEBOOK(notebook)) 
+    {
+    	g_warning("Notebook is not initialized properly.");
+    	return;
+	}
+	
+	// page 1
+    page_gsi1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_gsi1 = gtk_button_new_with_label("Hinweise");
+    label_gsi1_1 = gtk_label_new("GSI (Generic System Image) ist ein Android-System-Image, dass \neine Unterstützung von Project Treble (siehe weitere Infos) \nvoraussetzt.");
+    label_gsi1_2 = gtk_label_new("GSI ermöglichen Android-Geräten ohne Custom-ROMs für manuelles \naufspielen von neueren Android-Versionen.");
+    button_gsi2 = gtk_button_new_with_label("Weiter");
+    gtk_box_pack_start(GTK_BOX(page_gsi1), button_gsi1, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi1), label_gsi1_1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi1), label_gsi1_2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi1), button_gsi2, FALSE, FALSE, 0);
+    g_signal_connect(button_gsi1, "clicked", G_CALLBACK(gsi_text), notebook);
+    g_signal_connect(button_gsi2, "clicked", G_CALLBACK(next_page), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_gsi1, gtk_label_new("Hinweise"));
+    
+    // run css-provider
+    add_css_provider(button_gsi1, provider);
+    add_css_provider(label_gsi1_1, provider);
+    add_css_provider(label_gsi1_2, provider);
+    add_css_provider(button_gsi2, provider);
+    
+    // page 2
+    page_gsi2 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_gsi3 = gtk_button_new_with_label("GSI Flashen");
+    label_gsi2_1 = gtk_label_new("1. Prüfen sie ob ihr Gerät mit Project Treble kompatibel ist. \n2. Laden sie ein entsprechendes GSI herunter. \n3. Vorbereiten sie die Images für den Flash.\n");
+    label_gsi2_2 = gtk_label_new("4. Booten sie ihr Gerät in Fastboot mit der Anwendung. \n5. Öffnen sie den Bootloader.\nManche Geräte unterstützen nicht die Methode über Fastboot.");
+    label_gsi2_3 = gtk_label_new("Als Methoden des Flashs gibt es die \nAlternativen 'dirty flash'(1) und 'clean flash'(2).");
+    button_gsi4 = gtk_button_new_with_label("Weiter");
+    gtk_box_pack_start(GTK_BOX(page_gsi2), button_gsi3, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi2), label_gsi2_1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi2), label_gsi2_2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi2), label_gsi2_3, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi2), button_gsi4, FALSE, FALSE, 0);
+    g_signal_connect(button_gsi3, "clicked", G_CALLBACK(gsi_text), notebook);
+    g_signal_connect(button_gsi4, "clicked", G_CALLBACK(next_page), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_gsi2, gtk_label_new("Vorbereitung"));
+    
+    // run css-provider
+    add_css_provider(button_gsi3, provider);
+    add_css_provider(label_gsi2_1, provider);
+    add_css_provider(label_gsi2_2, provider);
+    add_css_provider(label_gsi2_3, provider);
+    add_css_provider(button_gsi4, provider);
+    
+    // page 3
+    page_gsi3 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_gsi5 = gtk_button_new_with_label("Clean Flash");
+    label_gsi3_1 = gtk_label_new("1. Löschen sie alle Nutzerdaten. \n2. Flashen sie das System-Image \n3. Flashen sie im Bedarfsfall ein vbmeta.img. \n4. Starten sie ihr Gerät neu.");
+    button_gsi6 = gtk_button_new_with_label("Weiter");
+    gtk_box_pack_start(GTK_BOX(page_gsi3), button_gsi5, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi3), label_gsi3_1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi3), button_gsi6, FALSE, FALSE, 0);
+    g_signal_connect(button_gsi5, "clicked", G_CALLBACK(gsi_text), notebook);
+    g_signal_connect(button_gsi6, "clicked", G_CALLBACK(next_page), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_gsi3, gtk_label_new("Clean Flash"));
+    
+    // run css-provider
+    add_css_provider(button_gsi5, provider);
+    add_css_provider(label_gsi3_1, provider);
+    add_css_provider(button_gsi6, provider);
+    
+    // page 4
+    page_gsi4 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    button_gsi7 = gtk_button_new_with_label("Dirty Flash");
+    label_gsi4_1 = gtk_label_new("Nach einem'dirty flash' bleiben die Nutzerdaten erhalten, \naber nicht jedes GSI startet danach. \n1. Flashen sie das System-Image. \n2. Starten sie ihr Gerät neu.");
+    gtk_box_pack_start(GTK_BOX(page_gsi4), button_gsi7, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi4), label_gsi4_1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(page_gsi4), button_gsi2, FALSE, FALSE, 0);
+    g_signal_connect(button_gsi7, "clicked", G_CALLBACK(gsi_text), notebook);
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_gsi4, gtk_label_new("Dirty Flash"));
+    
+    // run css-provider
+    add_css_provider(button_gsi7, provider);
+    add_css_provider(label_gsi4_1, provider);
+    
+    // clean the storage
+    g_object_unref(provider);
+	
+	// show all widgets
     gtk_widget_show_all(window);
-
-    // Run the GTK main loop
+	
+	// run gtk mainloop
     gtk_main();
 }
