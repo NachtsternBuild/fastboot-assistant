bd8e1ad9e032d8653d379bc22b8118bce0db7c2f
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-18 17:22:11 +0200

    fix the problem
    the spinner aren't work

--- Anwendung/header/header_install_with_root.c
+++ Anwendung/header/header_install_with_root.c
@@ -20,21 +20,40 @@
 
 /* headers */ 
 #include <stdlib.h>
+#include <glib.h>
 #include <gtk/gtk.h>
 #include "program_functions.h"
+#include "loading_spinner.h"
 
+GtkWidget *spinner_install = NULL;
+GtkWidget *spinner_install_window = NULL;
+
+// Funktion, die den Befehl im Hintergrund ausführt
+static gpointer run_command_with_spinner(gpointer data) 
+{
+    gchar *command = (gchar *)data;
+    system(command);
+    g_free(command);
+    return NULL;
+}
+
+// Funktion zum Starten des Spinners und Ausführen des Befehls
 void install_with_root(GtkButton *button, GtkEntry *password_entry, const gchar *command) 
 {
     const gchar *password = gtk_entry_get_text(password_entry);
     gchar *full_command = g_strdup_printf("echo %s | sudo -S %s", password, command);
+	
+	start_loading_spinner(spinner_install);  // Start the spinner
 
-    // run command in terminal
-    int status = system(full_command);
+    // Befehl in einem separaten Thread ausführen, um die UI nicht zu blockieren
+    g_thread_new("command-thread", run_command_with_spinner, full_command);
 
-    // close the window after the process
+    stop_loading_spinner(spinner_install);  // Stop the spinner when the process finishes
+    
     GtkWidget *window = gtk_widget_get_toplevel(GTK_WIDGET(button));
     gtk_widget_destroy(window);
-
-    g_free(full_command);
+    
+    gtk_main();
 }
+
 
