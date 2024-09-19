afffbf68514245ce1b6c6faafac0777222385c48
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-19 18:21:12 +0200

    fix #48

--- Anwendung/Windows/preflash/backup_noroot.c
+++ Anwendung/Windows/preflash/backup_noroot.c
@@ -21,22 +21,57 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 #include "function_header.h"
+#include "loading_spinner.h"
 
 #define MAX_BUFFER_SIZE 3072
 
+char install_command_backup[2048];
+
+static void install_depends(GtkButton *button, GtkEntry *password_entry) 
+{
+    //snprintf(install_command_backup, sizeof(install_command_backup), "apt-get install p7zip-full adb curl whiptail pv bc secure-delete zenity");
+    // for windows
+    snprintf(install_command, sizeof(install_command), "apt-get install p7zip-full secure-delete whiptail curl dos2unix pv bc zenity '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev -y");
+    g_print("Installiere: %s", install_command_backup);
+    install_with_root(button, password_entry, install_command_backup);
+}
+
 // Callback functions for each button
 // install depends for open android backup
-static void install_depends(GtkWidget *widget, gpointer data) 
+static void install_depends_function(GtkWidget *widget, gpointer data) 
 {
+    GtkWidget *window;
+    GtkWidget *vbox;
+    GtkWidget *password_entry;
+    GtkWidget *info_button, *install_depends_button;
+    
     const char *message = "\nDie Installation der benötigen Abhängigkeiten kann nur mit Root-Rechten ausgeführt werden.\n";
     // show message
     show_message(message);
-    /*
-	char function_command[255];
-	open_terminal_by_desktop("sudo apt update && sudo apt-get install p7zip-full adb curl whiptail pv bc secure-delete zenity && exit");
-	*/
-	// for Windows
-	open_terminal_by_desktop("sudo apt install p7zip-full secure-delete whiptail curl dos2unix pv bc zenity '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev -y");
+    // create window
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Aktualisieren");
+    gtk_window_set_default_size(GTK_WINDOW(window), 500, 200);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+	gtk_container_add(GTK_CONTAINER(window), vbox);
+	
+	// info button
+    info_button = gtk_button_new_with_label("Legitimation");
+   	gtk_box_pack_start(GTK_BOX(vbox), info_button, TRUE, TRUE, 0);
+
+	// password input 
+    password_entry = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(password_entry), FALSE);  // hide the password
+    gtk_box_pack_start(GTK_BOX(vbox), password_entry, TRUE, TRUE, 0);
+
+    // install button
+    install_depends_button = gtk_button_new_with_label("Installieren");
+    g_signal_connect(install_depends_button, "clicked", G_CALLBACK(install_depends), password_entry);
+   	gtk_box_pack_start(GTK_BOX(vbox), install_depends_button, TRUE, TRUE, 0);
+
+    gtk_widget_show_all(window);
 }
 
 // download open android backup
@@ -44,34 +79,22 @@ static void download_backup(GtkWidget *widget, gpointer data)
 {
     const char *message1 = "\nDas Tool 'Open Android Backup' wird heruntergeladen und entpackt.\n";
     show_message(message1);
-    /*
-    char function_command[3072];
-	open_terminal_by_desktop("wget -O ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip https://github.com/mrrfv/open-android-backup/releases/download/v1.0.18/Open_Android_Backup_v1.0.18_Bundle.zip && exit");
-	system("unzip ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip -d ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle");
-	*/
-	// for Windows
-	char wsl_dir[MAX_BUFFER_SIZE];
-	char command[MAX_BUFFER_SIZE];
-    get_wsl_directory(wsl_dir, sizeof(wsl_dir));
-    snprintf(command, sizeof(command), "wget -O %s/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip https://github.com/mrrfv/open-android-backup/releases/download/v1.0.18/Open_Android_Backup_v1.0.18_Bundle.zip && exit", wsl_dir);
-    system(command);
-    snprintf(command, sizeof(command), "unzip %s/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip -d %s/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle", wsl_dir, wsl_dir);
-    system(command);
-    const char *message2 = "\nOpen Android Backup ist nun bereit.\n";
-    // show message
-    show_message(message2);
+    
+    //snprintf(install_command_backup, sizeof(install_command_backup), "wget -O ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip https://github.com/mrrfv/open-android-backup/releases/download/v1.0.18/Open_Android_Backup_v1.0.18_Bundle.zip && unzip ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip -d ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle");
+    // for windows
+    snprintf("wget -O %s/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip https://github.com/mrrfv/open-android-backup/releases/download/v1.0.18/Open_Android_Backup_v1.0.18_Bundle.zip && unzip %s/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle.zip -d %s/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle", wsl_dir, wsl_dir, wsl_dir);
+    g_print("Führe aus: %s", install_command_backup);
+    command_with_spinner(install_command_backup);
 }
 
 // open open android backup
 static void open_backup(GtkWidget *widget, gpointer data) 
 {
-    /*
-    char function_command[3072];
+	/*
 	open_terminal_by_desktop("bash ~/Downloads/ROM-Install/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle/backup.sh");
 	*/
-	// for Windows
+	// for Windows	
 	char wsl_dir[MAX_BUFFER_SIZE];
-	char command[MAX_BUFFER_SIZE];
     get_wsl_directory(wsl_dir, sizeof(wsl_dir));
     snprintf(command, sizeof(command), "%s/Backup/Noroot/Open_Android_Backup_v1.0.18_Bundle/backup.sh", wsl_dir);
     open_terminal_by_desktop(command);
@@ -86,9 +109,7 @@ void backup_noroot(int argc, char *argv[])
     char button_labels[3][30] = {"Vorbereiten", "Download", "Starten"};
 
     gtk_init(&argc, &argv);
-    
     apply_theme();
-
     
     // create the window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -116,7 +137,7 @@ void backup_noroot(int argc, char *argv[])
         add_css_provider(button, provider);
         switch (i) {
             case 0:
-                g_signal_connect(button, "clicked", G_CALLBACK(install_depends), NULL);
+                g_signal_connect(button, "clicked", G_CALLBACK(install_depends_function), NULL);
                 break;
             case 1:
                 g_signal_connect(button, "clicked", G_CALLBACK(download_backup), NULL);
