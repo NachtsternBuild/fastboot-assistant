e36bdefe6e082ce594070635bac5d47176dc704b
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-24 20:29:44 +0200

    fix some errors

--- Anwendung/header/header_install_with_root.c
+++ Anwendung/header/header_install_with_root.c
@@ -28,7 +28,7 @@
 
 GtkWidget *spinner_install = NULL;
 GtkWidget *spinner_install_window = NULL;
-
+/*
 // function that run flash command
 void *run_install_command(void *command)
 {
@@ -87,6 +87,59 @@ void install_with_root(GtkButton *button, GtkEntry *password_entry, const gchar 
     pthread_detach(thread);  // thread in the background
 
     gtk_main();
+}
+*/
+// Function that runs the flash command in a separate thread
+// Function that runs the flash command in a separate thread
+void *run_install_command(void *command)
+{
+    char *full_command = (char *)command;
+
+    // Run the command
+    system(full_command);
+
+    // Stop the spinner and close the spinner window safely from the main thread
+    gdk_threads_add_idle((GSourceFunc)gtk_spinner_stop, spinner_install);
+    gdk_threads_add_idle((GSourceFunc)gtk_widget_destroy, spinner_install_window);
+
+    const char *message = "Fertig\n";
+    // Show message (again safely from the main thread)
+    gdk_threads_add_idle((GSourceFunc)show_message, (gpointer)message);
+
+    free(full_command);  // Free memory
+    return NULL;
+}
+
+// Function to start the command and run the spinner
+void install_with_root(GtkButton *button, GtkEntry *password_entry, const gchar *command) 
+{
+    const gchar *password = gtk_entry_get_text(password_entry);
+    gchar *full_command = g_strdup_printf("echo %s | sudo -S %s", password, command);
+    
+    // Create a new window with the spinner
+    spinner_install_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(spinner_install_window), "Installation läuft...");
+    gtk_window_set_default_size(GTK_WINDOW(spinner_install_window), 200, 100);
+    
+    // No `gtk_main_quit` signal on window destroy to avoid quitting the main program
+    g_signal_connect(spinner_install_window, "destroy", G_CALLBACK(gtk_widget_destroy), NULL);
+
+    // Create spinner and add it to the window
+    spinner_install = gtk_spinner_new();
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_box_pack_start(GTK_BOX(vbox), spinner_install, TRUE, TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(spinner_install_window), vbox);
+    
+    // Start the spinner
+    gtk_spinner_start(GTK_SPINNER(spinner_install));
+
+    // Show the spinner window
+    gtk_widget_show_all(spinner_install_window);
+
+    // Flash the image in a new thread
+    pthread_t thread;
+    pthread_create(&thread, NULL, run_install_command, full_command);
+    pthread_detach(thread);  // Detach thread to run in the background
 }
 
 
