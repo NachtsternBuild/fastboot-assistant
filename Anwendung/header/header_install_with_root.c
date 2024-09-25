c32ed00b5aae4d5326b39df8874326df425c3c10
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-25 17:37:23 +0200

    fix #55

--- Anwendung/header/header_install_with_root.c
+++ Anwendung/header/header_install_with_root.c
@@ -28,68 +28,7 @@
 
 GtkWidget *spinner_install = NULL;
 GtkWidget *spinner_install_window = NULL;
-/*
-// function that run flash command
-void *run_install_command(void *command)
-{
-    char *full_command = (char *)command;
 
-    // run the command
-    system(full_command);
-
-    // close the spinner and the window
-    gtk_spinner_stop(GTK_SPINNER(spinner_install));
-    gtk_widget_destroy(spinner_install_window);
-    
-    const char *message = "Fertig\n";
-    // show message
-    show_message(message);
-
-    free(full_command);  // free memory
-    return NULL;
-}
-
-// function to start the command and run the spinner
-void install_with_root(GtkButton *button, GtkEntry *password_entry, const gchar *command) 
-{
-    int argc = 0;
-    char **argv = NULL;
-    
-    gtk_init(&argc, &argv);
-    apply_theme();
-	
-	const gchar *password = gtk_entry_get_text(password_entry);
-    gchar *full_command = g_strdup_printf("echo %s | sudo -S %s", password, command);
-	
-    // new windows with the spinner
-    spinner_install_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(spinner_install_window), " ");
-    gtk_window_set_default_size(GTK_WINDOW(spinner_install_window), 200, 100);
-    
-    // the "destroy" signal to gtk_main_quit
-    g_signal_connect(spinner_install_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // create spinner 
-    spinner_install = gtk_spinner_new();
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), spinner_install, TRUE, TRUE, 0);
-    gtk_container_add(GTK_CONTAINER(spinner_install_window), vbox);
-	
-    // run spinner
-    gtk_spinner_start(GTK_SPINNER(spinner_install));
-
-    // show new window
-    gtk_widget_show_all(spinner_install_window);
-	
-    // flash the image in new thread
-    pthread_t thread;
-    pthread_create(&thread, NULL, run_install_command, full_command);
-    pthread_detach(thread);  // thread in the background
-
-    gtk_main();
-}
-*/
-// Function that runs the flash command in a separate thread
 // Function that runs the flash command in a separate thread
 void *run_install_command(void *command)
 {
@@ -140,6 +79,8 @@ void install_with_root(GtkButton *button, GtkEntry *password_entry, const gchar 
     pthread_t thread;
     pthread_create(&thread, NULL, run_install_command, full_command);
     pthread_detach(thread);  // Detach thread to run in the background
+    
+    free(password);   
 }
 
 
