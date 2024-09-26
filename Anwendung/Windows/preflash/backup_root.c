aba42da1707f181d7c2a226a4e6f0927898014fb
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-26 19:43:29 +0200

    fix memory error

--- Anwendung/Windows/preflash/backup_root.c
+++ Anwendung/Windows/preflash/backup_root.c
@@ -27,55 +27,52 @@
 void backup_root() 
 {
     char command[512];
-	char *homeDir = getenv("HOME");
-	char *backup_predir[2048];
-	char *backup_dir[2048];	
-	char *adb = adb_command();
-	// WSL Logik
-	const char *user = getenv("USER");
-	if (user == NULL) 
-	{	
-    	g_print("Fehler: Konnte den Benutzernamen nicht ermitteln.\n");
-    	exit(1);  // close the program if there are errors
-	}
+    char *homeDir = getenv("HOME");
+    char backup_predir[2048];
+    char backup_dir[2048];	
+    char *adb = adb_command();
 	
-	if (homeDir == NULL) 
-	{
-    	fprintf(stderr, "Fehler: Konnte das Home-Verzeichnis nicht finden.\n");
-    	exit(1);  // close the program if there are errors
-	}
-	char wsl_setup_base[2048];
-	snprintf(wsl_setup_base, sizeof(wsl_setup_base), "/mnt/c/Users/%s", user);
-
+	// WSL logic
+    const char *user = getenv("USER");
+    if (user == NULL) 
+    {	
+        g_print("Fehler: Konnte den Benutzernamen nicht ermitteln.\n");
+        exit(1);
+    }
+	// linux logic
+    if (homeDir == NULL) 
+    {
+        fprintf(stderr, "Fehler: Konnte das Home-Verzeichnis nicht finden.\n");
+        exit(1);
+    }
+    char wsl_setup_base[2048];
+    snprintf(wsl_setup_base, sizeof(wsl_setup_base), "/mnt/c/Users/%s", user);
+	
 	// for linux
-	//snprintf(backup_predir, sizeof(backup_predir), "%s", homeDir);
-	// for wsl
-	snprintf(backup_predir, sizeof(backup_predir), "%s", wsl_setup_base);
-
-	// create full path for the config.txt
-	snprintf(backup_dir, sizeof(backup_dir), "%s/Downloads/ROM-Install/Backup", backup_predir);
+    //snprintf(backup_predir, sizeof(backup_predir), "%s", homeDir);
+    // for windows
+    snprintf(backup_predir, sizeof(backup_predir), "%s", wsl_setup_base);
+    // create full path
+    snprintf(backup_dir, sizeof(backup_dir), "%s/Downloads/ROM-Install/Backup", backup_predir);
     g_print("Sicherung aller verfügbaren Partitionen nach %s.\n", backup_dir);
-
-    // create backup path
+	// create backup path
     snprintf(command, sizeof(command), "mkdir -p %s", backup_dir);
     execute_command(command);
-
-    // connect device with adb
+	// wait for a device
     snprintf(command, sizeof(command), "%s wait-for-device", adb);
     execute_command(command);
-
-    // get partitions
+	// write partition list to partition.txt
     snprintf(command, sizeof(command), "%s shell %s -c \"ls %s\" > %s/partitions.txt", adb, SU, BLOCK_PATH, backup_dir);
     execute_command(command);
     
     char file_partition[2048];
     snprintf(file_partition, sizeof(file_partition), "%s/partitions.txt", backup_dir);
     
-
-    FILE *file = fopen( file_partition , "r");
+    FILE *file = fopen(file_partition, "r");
     if (!file) 
     {
-        g_print("Fehler beim Öffnen der partitions.txt");
+        g_print("Fehler beim Öffnen der partitions.txt\n");
+        exit(1); 
     }
 
     char partition[128];
@@ -83,13 +80,12 @@ void backup_root()
     {
         // delete linebreak
         partition[strcspn(partition, "\r\n")] = 0;
-
-        // get slots 
+		// get slots 
         snprintf(command, sizeof(command), "%s shell %s -c \"ls %s%s_a\" >/dev/null 2>&1", adb, SU, BLOCK_PATH, partition);
         int has_slot_a = system(command);
 
-        if (has_slot_a == 0) 
-        { // devices with a/b slots
+        if (has_slot_a == 0) // for devices with a/b slots
+        {
             for (char slot = 'a'; slot <= 'b'; slot++) 
             {
                 snprintf(command, sizeof(command), "%s/%s_%c.img", backup_dir, partition, slot);
@@ -100,9 +96,8 @@ void backup_root()
                 execute_command(command);
             }
         } 
-        
-        else 
-        { // devices without a/b slots
+        else // for devices without a/b slots
+        {
             snprintf(command, sizeof(command), "%s/%s.img", backup_dir, partition);
             g_print("Sichere %s nach %s\n", partition, command);
 
@@ -115,3 +110,4 @@ void backup_root()
     fclose(file);
     g_print("Sicherung abgeschlossen. Dateien befinden sich in %s\n", backup_dir);
 }
+
