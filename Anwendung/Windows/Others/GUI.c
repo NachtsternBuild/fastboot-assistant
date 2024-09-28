1f4d4b05f18dcc8ae4681e11e648a6f65876f138
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-09-28 16:11:04 +0200

    improve the function
    use the smaller UI function from the main project

--- Anwendung/Windows/Others/GUI.c
+++ Anwendung/Windows/Others/GUI.c
@@ -44,6 +44,8 @@ extern void updater();
 extern void about();
 extern void make_dir();
 
+extern void run_first_run_setup();
+
 
 // Callback functions for each button
 // start get_devices-function
@@ -122,7 +124,7 @@ void config_dir_setup(const char *pfad)
 
 
 // config the program
-static void config_start() 
+void config_start() 
 {
     const char *message;
     message = "Konfiguration beendet!\n";
@@ -130,159 +132,6 @@ static void config_start()
     wsl_config();
     show_message(message);
     
-}
-
-static void setup_text(GtkButton *button, gpointer data) 
-{
-	g_print("Hier gibt's nichts!\n");
-}
-
-// function that run the setup
-static void run_first_run_setup(GtkCssProvider *provider) 
-{
-    GtkWidget *window;
-    GtkWidget *page1, *page2, *page3, *page4, *page5;
-    GtkWidget *label_welcome_1, *label_welcome_2, *label_page2_1, *label_page2_2, *label_page3_1, *label_page3_2, *label_page3_3, *label_page3_4, *label_page4_1, *label_page4_2, *label_page4_3, *label_page4_4, *label_end_1, *label_end_2;
-    GtkWidget *button_welcome_1, *button_toggle_theme, *button_welcome_2, *button_page2_1, *button_dir, *button_page2_2, *button_page3_1, *button_page3_2, *button_page4_1, *button_page4_2, *button_end_1, *button_end_2;
-
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Fastboot Assistant Setup");
-    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    
-    // Connect close function to 'destroy' signal
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-	// create notebook
-	GtkWidget *notebook = gtk_notebook_new();
-    gtk_container_add(GTK_CONTAINER(window), notebook);
-    
-    // for debugging
-    if (!GTK_IS_NOTEBOOK(notebook)) 
-    {
-    	g_warning("Notebook is not initialized properly.");
-    	return;
-	}
-
-    // page 1
-    page1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    label_welcome_1 = gtk_label_new(" ");
-    button_welcome_1 = gtk_button_new_with_label("Willkommen zum Fastboot Assistant!");
-    button_toggle_theme = gtk_button_new_with_label("Thema wechseln (hell/dunkel)");
-    label_welcome_2 = gtk_label_new(" ");
-    button_welcome_2 = gtk_button_new_with_label("Weiter");
-    gtk_box_pack_start(GTK_BOX(page1), label_welcome_1, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page1), button_welcome_1, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(page1), button_toggle_theme, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(page1), label_welcome_2, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page1), button_welcome_2, FALSE, FALSE, 0);
-    g_signal_connect(button_welcome_1, "clicked", G_CALLBACK(setup_text), notebook);
-    g_signal_connect(button_toggle_theme, "clicked", G_CALLBACK(toggle_theme), notebook);
-    g_signal_connect(button_welcome_2, "clicked", G_CALLBACK(next_page), notebook);
-    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page1, gtk_label_new("Begrüßung"));
-
-    // run css-provider
-    add_css_provider(button_welcome_1, provider);
-    add_css_provider(button_toggle_theme, provider);
-    add_css_provider(button_welcome_2, provider);
-    
-
-    // page 2
-    page2 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    button_page2_1 = gtk_button_new_with_label("Zum korrekten Ausführen muss der \nFastboot-Assistant einige Konfigurationen vornehmen.");
-    label_page2_1 = gtk_label_new("1. Es werden alle benötigten Ordner erstellt.");
-    label_page2_2 = gtk_label_new("2. Anpassungen an das Betriebssystem werden durchgeführt.");
-    button_dir = gtk_button_new_with_label("Konfigurieren");
-    button_page2_2 = gtk_button_new_with_label("Weiter");
-    gtk_box_pack_start(GTK_BOX(page2), button_page2_1, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page2), label_page2_1, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page2), label_page2_2, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page2), button_dir, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page2), button_page2_2, FALSE, FALSE, 0);
-    g_signal_connect(button_page2_1, "clicked", G_CALLBACK(setup_text), notebook);
-    g_signal_connect(button_dir, "clicked", G_CALLBACK(config_start), NULL);
-    g_signal_connect(button_page2_2, "clicked", G_CALLBACK(next_page), notebook);
-    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page2, gtk_label_new("Konfiguration"));
-
-    // run css-provider
-    add_css_provider(button_page2_1, provider);
-    add_css_provider(label_page2_1, provider);
-    add_css_provider(label_page2_2, provider);
-    add_css_provider(button_dir, provider);
-    add_css_provider(button_page2_2, provider);
-
-    // page 3
-    page3 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    button_page3_1 = gtk_button_new_with_label("Hinweise:");
-	label_page3_1 = gtk_label_new("1. Die Verwendung erfolgt auf eigene Gefahr.");
-	label_page3_2 = gtk_label_new("2. Das Gerät kann beschädigt werden.");
-	label_page3_3 = gtk_label_new("3. Nicht jedes Gerät unterstützt alle Funktionen");
-	label_page3_4 = gtk_label_new("4. Nicht alle Betriebssystemvarianten haben den gleichen Funktionsumfang.");
-    button_page3_2 = gtk_button_new_with_label("Weiter");
-    gtk_box_pack_start(GTK_BOX(page3), button_page3_1, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(page3), label_page3_1, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page3), label_page3_2, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page3), label_page3_3, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page3), label_page3_4, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page3), button_page3_2, FALSE, FALSE, 0);
-    g_signal_connect(button_page3_1, "clicked", G_CALLBACK(setup_text), notebook);
-    g_signal_connect(button_page3_2, "clicked", G_CALLBACK(next_page), notebook);
-    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page3, gtk_label_new("Hinweise"));
-
-    // run css-provider
-    add_css_provider(button_page3_1, provider);
-    add_css_provider(label_page3_1, provider);
-    add_css_provider(label_page3_2, provider);
-    add_css_provider(label_page3_3, provider);
-    add_css_provider(label_page3_4, provider);
-    add_css_provider(button_page3_2, provider);
-    
-    // page 4
-    page4 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    button_page4_1 = gtk_button_new_with_label("Verwendung:");
-	label_page4_1 = gtk_label_new("1. Lesen sie vor der Verwendung die \nAnleitungen.");
-	label_page4_2 = gtk_label_new("2. Kopieren sie immer alle Systemabbilder \ndes Flashs nach ~/Downloads/ROM-Install.");
-	label_page4_3 = gtk_label_new("3. Sollten sie ein Samsung-Gerät flashen \nwollen nutzen sie immer 'heimdall'.");
-	label_page4_4 = gtk_label_new("4. Prüfen sie regelmäßig, ob es Updates für das Programm gibt.");
-    button_page4_2 = gtk_button_new_with_label("Weiter");
-    gtk_box_pack_start(GTK_BOX(page4), button_page4_1, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(page4), label_page4_1, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page4), label_page4_2, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page4), label_page4_3, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page4), label_page4_4, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page4), button_page4_2, FALSE, FALSE, 0);
-    g_signal_connect(button_page4_1, "clicked", G_CALLBACK(setup_text), notebook);
-    g_signal_connect(button_page4_2, "clicked", G_CALLBACK(next_page), notebook);
-    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page4, gtk_label_new("Verwendung"));
-
-    // run css-provider
-	add_css_provider(button_page4_1, provider);
-	add_css_provider(label_page4_1, provider);
-	add_css_provider(label_page4_2, provider);
-	add_css_provider(label_page4_3, provider);
-	add_css_provider(label_page4_4, provider);
-	add_css_provider(button_page4_2, provider);
-	
-	// page 5
-    page5 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    label_end_1 = gtk_label_new(" ");
-    button_end_1 = gtk_button_new_with_label("Viel Spaß bei der Verwendung des Fastboot Assistants!");
-    label_end_2 = gtk_label_new(" ");
-    button_end_2 = gtk_button_new_with_label("Fertig");
-    gtk_box_pack_start(GTK_BOX(page5), label_end_1, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page5), button_end_1, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(page5), label_end_2, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page5), button_end_2, FALSE, FALSE, 0);
-    g_signal_connect(button_end_1, "clicked", G_CALLBACK(setup_text), notebook);
-    g_signal_connect(button_end_2, "clicked", G_CALLBACK(button_finish), notebook);
-	gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page5, gtk_label_new("Ende"));
-
-    // run css-provider
-    add_css_provider(button_end_1, provider);
-	add_css_provider(button_end_2, provider);
-	
-    gtk_widget_show_all(window);
-
-    gtk_main();
 }
 
 /* the main function */
@@ -310,7 +159,7 @@ int main(int argc, char *argv[])
     	exit(1);  // close the program if there are errors
 	}
 
-	// WSL Logik
+	// WSL logic
 	const char *user = getenv("USER");
 	if (user == NULL) 
 	{	
@@ -325,7 +174,6 @@ int main(int argc, char *argv[])
 	//snprintf(setup_dir, sizeof(setup_dir), "%s", homeDir);
 	// for wsl
 	snprintf(setup_dir, sizeof(setup_dir), "%s", wsl_setup_base);
-	g_print(setup_dir);
 
 	// create the dir for the config
 	snprintf(fish_path, sizeof(fish_path), "%s/Downloads/ROM-Install/config", setup_dir);
@@ -416,8 +264,12 @@ int main(int argc, char *argv[])
         }
     }
 	
-	// clean the storage
-    g_object_unref(provider);
+	// free the provider
+	if (provider != NULL) 
+	{
+	    g_object_unref(provider);
+	}
+
     
     // show all button
     gtk_widget_show_all(window);
