95aa11e22fb353b29b6792ccae3ba5041bb2da39
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-10-29 18:11:22 +0100

    add #65 and fix #67
    add a modified function, which I already used in the functions for renaming files. With this I fix #67.

--- Anwendung/reboot/reboot.c
+++ Anwendung/reboot/reboot.c
@@ -22,8 +22,12 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include "program_functions.h"
+#include "function_header.h"
+#include "file_chooser_header.h"
+#include "flash_function_header.h"
 
-void reboot_system(GtkWidget *widget, gpointer data)
+// reboot system
+static void reboot_system(GtkWidget *widget, gpointer data)
 {
     const char *message = "Neustart wird durchgeführt.\n";    
     show_message(message);
@@ -33,4 +37,80 @@ void reboot_system(GtkWidget *widget, gpointer data)
     char command[256];
     snprintf(command, sizeof(command), "%s reboot", device_command);
     free(device_command);
+}
+
+// boot to an image
+static void boot_to_image(const gchar *i_filename) 
+{
+    gchar *target_directory_image = get_home("~/Downloads/ROM-Install/");
+    if (target_directory_image == NULL) 
+    {
+        g_print("Fehler beim Erweitern des Verzeichnispfads.\n");
+        return;
+    }
+
+    // create fastboot command
+    char *device_command = fastboot_command(); 
+    gchar *target_path_image = g_strdup_printf("%s boot %s", device_command, i_filename);
+    command_with_spinner(target_path_image);
+
+    // free memory
+    free(device_command);
+    g_free(target_directory_image);
+    g_free(target_path_image);
+}
+
+/* start main programm */
+void reboot(int argc, char *argv[])
+{
+    GtkWidget *window;
+    GtkWidget *grid;
+    GtkWidget *button;
+    char button_labels[2][30] = {"Neustart System", "Neustart Image(.img)"};
+
+    gtk_init(&argc, &argv);
+    apply_theme();
+
+    // create the window
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Neustarten");
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	
+    // create the grid and centre it
+    grid = gtk_grid_new();
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+    
+    gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+
+    // add the grid to the window
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    // add and centre all button
+    for (int i = 0; i < 2; i++) {
+        button = gtk_button_new_with_label(button_labels[i]);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
+
+        // execute css-provider for all buttons
+        add_css_provider(button, provider);
+        switch (i) {
+            case 0:
+                g_signal_connect(button, "clicked", G_CALLBACK(reboot_system), NULL);
+                break;
+            case 1:
+                g_signal_connect(button, "clicked", G_CALLBACK(boot_to_image), NULL);
+                break;
+        }
+    }
+	
+	// clean the storage
+    g_object_unref(provider);
+    
+    // show window
+    gtk_widget_show_all(window);
+
+    // run main-gtk-loop
+    gtk_main();
 }
