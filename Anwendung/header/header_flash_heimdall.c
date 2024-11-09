05815c19e0706e98442df7348526815e90d898ff
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-09 12:06:05 +0100

    for #59
    rework this for GTK4

--- Anwendung/header/header_flash_heimdall.c
+++ Anwendung/header/header_flash_heimdall.c
@@ -2,19 +2,19 @@
  *-------------------------------------------*
  *                Projekt 122                *
  *-------------------------------------------*
- *  	Apache License, Version 2.0		     *
+ *      Apache License, Version 2.0          *
  *-------------------------------------------*
  *                                           *
- *  Programm um das installieren von 		 *
- *	Custom-ROM und GSIs auf Android-Geräte 	 *
- *	zu erleichtern  						 *
+ *  Programm um das Installieren von         *
+ *  Custom-ROM und GSIs auf Android-Geräte   *
+ *  zu erleichtern                           *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2024 Elias Mörz 		 *
+ *      (C) Copyright 2024 Elias Mörz        *
  *-------------------------------------------*
- *											 *
- *         Headerpart - flash_heimdall		 *
- *											 *
+ *                                           *
+ *         Headerpart - flash_heimdall       *
+ *                                           *
  *-------------------------------------------*
  */
 
@@ -25,33 +25,34 @@
 #include <pthread.h>
 #include "program_functions.h"
 
-// global var for the spinner
+// global vars
 GtkWidget *spinner_heimdall;
 GtkWidget *spinner_window_heimdall;
 
-// function that run the command
+// function to run the command
 void *run_heimdall_command(void *command)
 {
     char *function_command = (char *)command;
 
-    // run the command
-    g_print("Executing: %s\n", function_command);
+    // run command
+    g_print("Log: Run: %s\n", function_command);
     system(function_command);
 
-    // close the spinner and the window
+    // close spinner and window
     gtk_spinner_stop(GTK_SPINNER(spinner_heimdall));
-    gtk_widget_destroy(spinner_window_heimdall);
+    gtk_window_destroy(GTK_WINDOW(spinner_window_heimdall));
 
-    free(function_command);  // clean the memory
+    free(function_command);  // free memory
     return NULL;
 }
 
-// flash the images with heimdall
+// function to flash with heimdall
 void flash_heimdall(GtkWidget *widget, GtkWindow *parent_window, const char *partition, const char *image_name)
 {
     char image_path[3072];
     set_main_dir_with_wsl(image_path, sizeof(image_path), image_name);
     apply_theme();
+    apply_language();
 
     // check if the image exsists
     if (access(image_path, F_OK) == -1)
@@ -67,23 +68,23 @@ void flash_heimdall(GtkWidget *widget, GtkWindow *parent_window, const char *par
     snprintf(function_command, 3072, "heimdall flash --%s %s --no-reboot && exit", partition, image_path);
 
     // create new window
-    spinner_window_heimdall = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    spinner_window_heimdall = gtk_window_new();
     gtk_window_set_title(GTK_WINDOW(spinner_window_heimdall), " ");
     gtk_window_set_default_size(GTK_WINDOW(spinner_window_heimdall), 200, 100);
 
-    // create new spinner in the window
+    // create the spinner
     spinner_heimdall = gtk_spinner_new();
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), spinner_heimdall, TRUE, TRUE, 0);
-    gtk_container_add(GTK_CONTAINER(spinner_window_heimdall), vbox);
+    gtk_box_append(GTK_BOX(vbox), spinner_heimdall);
+    gtk_window_set_child(GTK_WINDOW(spinner_window_heimdall), vbox);
 
     // start the spinner
     gtk_spinner_start(GTK_SPINNER(spinner_heimdall));
 
     // show new window
-    gtk_widget_show_all(spinner_window_heimdall);
+    gtk_widget_show(spinner_window_heimdall);
 
-    // new thread for the flash
+    // run the command in a new thread
     pthread_t thread;
     pthread_create(&thread, NULL, run_heimdall_command, function_command);
     pthread_detach(thread);  // thread in the background
