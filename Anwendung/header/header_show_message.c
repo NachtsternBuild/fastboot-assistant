8eee8f93f1c4e5cdc8727d559bc4b13dce77dd3d
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-18 11:07:55 +0100

    fix #69

--- Anwendung/header/header_show_message.c
+++ Anwendung/header/header_show_message.c
@@ -2,19 +2,19 @@
  *-------------------------------------------*
  *                Projekt 122                *
  *-------------------------------------------*
- *  	Apache License, Version 2.0		     *
+ *      Apache License, Version 2.0          *
  *-------------------------------------------*
  *                                           *
- *  Programm um das installieren von 		 *
- *	Custom-ROM und GSIs auf Android-Geräte 	 *
- *	zu erleichtern  						 *
+ *  Programm um das Installieren von         *
+ *  Custom-ROM und GSIs auf Android-Geräte   *
+ *  zu erleichtern                           *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2024 Elias Mörz        *
  *-------------------------------------------*
- *											 *
- *              Headerpart - gtk_message	 *
- *											 *
+ *                                           *
+ *              Headerpart - gtk_message     *
+ *                                           *
  *-------------------------------------------*
  */
 
@@ -26,13 +26,19 @@
 
 void show_message(const char *message) 
 {
-    GtkDialog *dialog = GTK_DIALOG(gtk_message_dialog_new(NULL,
-                                                          GTK_DIALOG_MODAL,
-                                                          GTK_MESSAGE_INFO,
-                                                          GTK_BUTTONS_OK,
-                                                          "%s", message));
+    GtkWidget *dialog;
 
-    gtk_dialog_run(dialog);
-    gtk_window_destroy(GTK_WINDOW(dialog));
-}
+    // Create a message dialog with markup
+    dialog = gtk_message_dialog_new_with_markup(NULL, 
+                                                GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                GTK_MESSAGE_INFO,
+                                                GTK_BUTTONS_OK,
+                                                "%s", message);  // Sets the main message directly
+
+    // Connect signal to destroy dialog after response
+    g_signal_connect(dialog, "response", G_CALLBACK(gtk_window_destroy), NULL);
+
+    // Display the dialog
+    gtk_window_present(GTK_WINDOW(dialog));
+}   
 
