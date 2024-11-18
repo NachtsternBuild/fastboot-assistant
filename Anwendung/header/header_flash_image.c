f7ad723fb177c9ff42f2282253d41b2d93b60382
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-18 11:06:21 +0100

    fix #69

--- Anwendung/header/header_flash_image.c
+++ Anwendung/header/header_flash_image.c
@@ -25,17 +25,18 @@
 #include <gtk/gtk.h>
 #include <pthread.h>
 #include "program_functions.h"
+#include "language_check.h"
 
 // global vars
 GtkWidget *spinner_window_flash;
 GtkWidget *spinner_flash;
 
-// run the command 
+// run the command
 void *run_flash_command(void *command)
 {
     char *function_command = (char *)command;
 
-    // run the command 
+    // run the command
     g_print("Log: Run: %s\n", function_command);
     system(function_command);
 
@@ -47,41 +48,41 @@ void *run_flash_command(void *command)
     return NULL;
 }
 
-// function to flash a image
+// function to flash an image
 void flash_image(GtkWidget *widget, GtkWindow *parent_window, const char *partition1, const char *partition2, const char *image_name)
 {
     char image_path[3072];
-    apply_theme(); 
+    apply_theme();
     apply_language();
 
     // create the path for the image
     set_main_dir_with_wsl(image_path, sizeof(image_path), image_name);
     
-    char *function_command = malloc(4096);
+    char *function_command = malloc(8192);  // Increase buffer size
 
-    // check if the image exsists
+    // check if the image exists
     if (access(image_path, F_OK) == -1)
     {
         char error_message[3072];
-        snprintf(error_message, sizeof(error_message), "Fehler: Image-Datei '%s' nicht gefunden.", image_path);
+        snprintf(error_message, sizeof(error_message), "Log: Error: Image file '%s' not found.\n", image_path);
         show_error_message(GTK_WIDGET(parent_window), error_message);
+        free(function_command);  // Free allocated memory
         return;
     }
 
     // create the command
+    const char *device_command = fastboot_command();
+
     // for a/b-devices
     if (partition2)
     {
-        const char *device_command = fastboot_command();
-        snprintf(function_command, 4096, "%s flash %s %s && fastboot flash %s %s && exit", device_command, partition1, image_path, partition2, image_path);
-        free(device_command);
+        snprintf(function_command, 8192, "%s flash %s %s && %s flash %s %s && exit",
+                 device_command, partition1, image_path, device_command, partition2, image_path);
     }
-    // for only-a-devices 
+    // for only-a-devices
     else
     {
-        const char *device_command = fastboot_command();
-        snprintf(function_command, 4096, "%s flash %s %s && exit", device_command, partition1, image_path);
-        free(device_command);
+        snprintf(function_command, 8192, "%s flash %s %s && exit", device_command, partition1, image_path);
     }
 
     // create a new window
@@ -99,7 +100,7 @@ void flash_image(GtkWidget *widget, GtkWindow *parent_window, const char *partit
     gtk_spinner_start(GTK_SPINNER(spinner_flash));
 
     // show the window
-    gtk_widget_show(spinner_window_flash);
+    gtk_widget_set_visible(spinner_window_flash, TRUE);  // Replaced gtk_widget_show
 
     // run command in a new thread
     pthread_t thread;
