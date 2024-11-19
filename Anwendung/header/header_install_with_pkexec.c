06945146a1102effdd98ff787325b0026b4eb09c
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-19 18:32:40 +0100

    fix #66
    use pkexec, that use the system dialog

---
+++ Anwendung/header/header_install_with_pkexec.c
@@ -0,0 +1,34 @@
+/*
+ *-------------------------------------------*
+ *                Projekt 122                *
+ *-------------------------------------------*
+ *      Apache License, Version 2.0          *
+ *-------------------------------------------*
+ *                                           *
+ *  Programm um das Installieren von         *
+ *  Custom-ROM und GSIs auf Android-Geräte   *
+ *  zu erleichtern                           *
+ *                                           *
+ *-------------------------------------------*
+ *      (C) Copyright 2024 Elias Mörz        *
+ *-------------------------------------------*
+ *                                           *
+ *       Headerpart - install_with_pkexec    *
+ *                                           *
+ *-------------------------------------------*
+ */
+
+/* headers */ 
+#include <glib.h>
+#include <stdlib.h>
+#include <gtk/gtk.h>
+#include "program_functions.h"
+
+// function to run a command via root use pkexec
+void install_with_pkexec(const gchar *command)
+{
+    gchar *full_command = g_strdup_printf("pkexec %s", command);
+    system(full_command);
+    g_free(full_command);
+}
+
