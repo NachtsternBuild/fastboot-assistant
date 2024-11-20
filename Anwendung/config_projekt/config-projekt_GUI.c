2a1875bb87062c38b21c75c52f7df1e20b0da6cf
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-20 16:45:41 +0100

    for #59 and #26
    rework the function for GTK4 with two Languages

--- Anwendung/config_projekt/config-projekt_GUI.c
+++ Anwendung/config_projekt/config-projekt_GUI.c
@@ -7,7 +7,7 @@
  *                                           *
  *  Programm um das installieren von 		 *
  *	Custom-ROM und GSIs auf Android-Geräte 	 *
- *	zu erleichtern - preflash_GUI			 *
+ *	zu erleichtern - config-projekt_GUI 	 *
  *                                           *
  *-------------------------------------------*
  *      (C) Copyright 2024 Elias Mörz 		 *
@@ -19,10 +19,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
+#include "language_check.h"
 #include "program_functions.h"
 #include "function_header.h"
-
-#define MAX_BUFFER_SIZE 256
 
 // include all functions
 extern void make_dir();
@@ -30,64 +29,61 @@ extern void remove_old();
 extern void wsl_config();
 
 // Callback functions for each button
-// function without any function
+// function that start make_dir
 static void start_make_dir_function(GtkWidget *widget, gpointer data) 
 {
-    const char *message;
-    message = "Fertig!\n";
+    const char *message = strcmp(language, "de") == 0 ? "Fertig!": "Ready!";
     make_dir();
     show_message(message);
 }
 
-// function without any function
+// function that start remove_old
 static void start_remove_old(GtkWidget *widget, gpointer data) 
 {
     remove_old();
 }
 
-// start make_dir-function
+// function that start wsl_config
 static void start_wsl_config(GtkWidget *widget, gpointer data) 
 {
     wsl_config();
 }
 
-/* main function of preflash_GUI*/
+// function that start toggle_language
+static void start_toggle_language(GtkWidget *widget, gpointer data)
+{
+	toggle_language();
+}
+
+/* main function of config-projekt_GUI*/
 void config_projekt_GUI(int argc, char *argv[]) 
 {
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
+	g_print("Log: config_projekt_GUI\n");
+	GtkWidget *window, *grid, *button;
+    char button_labels[4][30];
     
-    char button_labels[3][30] = {"Anlegen Ordner", "Aufräumen", "Konfigurieren"};
-
-    gtk_init(&argc, &argv);
-	
-	apply_theme();
-	
-    // create the window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Fastboot-Assistant - Zusätze");
+    gtk_init();
+    apply_theme();
+    apply_language();
+    set_button_labels_config_projekt(button_labels);
+    
+    window = gtk_window_new();
+    const char *config_projekt_window = strcmp(language, "de") == 0 ? "Zusätze" : "Additions";
+    gtk_window_set_title(GTK_WINDOW(window), config_projekt_window);
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // create the grid and centre it
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
-
-    // add the grid to the window
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    // add and centre all button
-    for (int i = 0; i < 3; i++) {
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+    
+    for (int i = 0; i < 4; i++) 
+    {
         button = gtk_button_new_with_label(button_labels[i]);
-        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
-
-        // execute css-provider for all buttons
-        add_css_provider(button, provider);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 2, i / 2, 1, 1);
         
         switch (i) {
             case 0:
@@ -99,16 +95,23 @@ void config_projekt_GUI(int argc, char *argv[])
             case 2:
                 g_signal_connect(button, "clicked", G_CALLBACK(start_wsl_config), NULL);
                 break;
+            case 3;
+            	g_signal_connect(button, "clicked", G_CALLBACK(start_toggle_language); NULL);
+            	break;
         }
     }
     
-    // clean the storage
-    g_object_unref(provider);
+    // free the provider
+	if (provider != NULL) 
+	{
+	    g_object_unref(provider);
+	}
+	
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
-    // show window
-    gtk_widget_show_all(window);
-
-    // run main-gtk-loop
-    gtk_main();
+     // run GTK main loop
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop); 
+    
+    g_print("Log: end config_projekt_GUI\n");
 }
-
