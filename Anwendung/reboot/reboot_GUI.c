41b639f14a5522b6ca4a1dad3acbeb88dd919baa
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-21 16:59:40 +0100

    for #59 and #26
     support GTK4 and the second language

--- Anwendung/reboot/reboot_GUI.c
+++ Anwendung/reboot/reboot_GUI.c
@@ -55,41 +55,54 @@ static void start_reboot_heimdall(GtkWidget *widget, gpointer data)
     reboot_heimdall();
 }
 
+// Function to set up button labels based on the language
+void set_button_labels_reboot(char labels[][30]) 
+{
+    if (strcmp(language, "en") == 0) 
+    {
+        strcpy(labels[0], "Reboot Fastboot");
+        strcpy(labels[1], "Reboot");
+        strcpy(labels[2], "Reboot Recovery");
+        strcpy(labels[3], "Reboot (heimdall)");
+    } 
+    
+    else 
+    {
+        strcpy(labels[0], "Neustart Fastboot");
+        strcpy(labels[1], "Neustart");
+        strcpy(labels[2], "Neustart in Recovery");
+        strcpy(labels[3], "Neustart (heimdall)");
+    }
+}
+
 void reboot_GUI(int argc, char *argv[]) 
 {
-    g_print("Log: reboot_GUI\n");
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
-    char button_labels[4][30] = {"Neustart Fastboot", "Neustart", "Neustart in Recovery", "Neustart Heimdall"};
-
-    gtk_init(&argc, &argv);
+	g_print("Log: reboot_GUI\n");
+	GtkWidget *window, *grid, *button;
+    char button_labels[4][30];
+    
+    gtk_init();
     apply_theme();
-
-     // create the window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Fastboot-Assistant - Neustart");
+    apply_language();
+    set_button_labels_reboot(button_labels);
+    
+    window = gtk_window_new();
+    const char *reboot_window = strcmp(language, "de") == 0 ? "Neustart" : "Reboot";
+    gtk_window_set_title(GTK_WINDOW(window), reboot_window);
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-    // create the grid and centre it
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
-
-    // add the grid to the window
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    // add and centre all button
-    for (int i = 0; i < 4; i++) {
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+    
+    for (int i = 0; i < 4; i++) 
+    {
         button = gtk_button_new_with_label(button_labels[i]);
         gtk_grid_attach(GTK_GRID(grid), button, i % 2, i / 2, 1, 1);
-
-        // execute css-provider for all buttons
-        add_css_provider(button, provider);
         
         switch (i) {
             case 0:
@@ -106,13 +119,18 @@ void reboot_GUI(int argc, char *argv[])
                 break;          
         }
     }
-	// cleaning the provider
-    g_object_unref(provider);
+    
+    // free the provider
+	if (provider != NULL) 
+	{
+	    g_object_unref(provider);
+	}
+	
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
-    // show window
-    gtk_widget_show_all(window);
-
-    // run main-gtk-loop
-    gtk_main();
-	g_print("Log: end reboot_GUI\n");
+     // run GTK main loop
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop); 
+    
+    g_print("Log: end reboot_GUI\n");
 }
