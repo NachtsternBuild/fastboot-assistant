c65d185818072df041f72ce4600cb9f7f7ad2d9e
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-21 16:58:56 +0100

    for #59 and #26
    support GTK4 and the second language

--- Anwendung/reboot/reboot_heimdall.c
+++ Anwendung/reboot/reboot_heimdall.c
@@ -30,11 +30,8 @@
 static void reboot_from_adb_heimdall(GtkWidget *widget, gpointer data)
 {
     g_print("Log: reboot_from_adb_heimdall\n");
-    const char *title, *message;
-    
-    title = "Reboot from ADB";
-    message = "Beachten sie, dass USB-Debugging aktiviert ist in den Entwickleroptionen!\n";
-    show_message_with_title(title, message);
+    const char *message = strcmp(language, "de") == 0 ? "Beachten sie, dass USB-Debugging aktiviert ist in den Entwickleroptionen!" : "Please note that USB debugging is activated in the developer options!";
+    show_message(message);
     
     char *device_command = adb_command();
     char command[256];
@@ -56,50 +53,61 @@ static void heimdall_help(GtkWidget *widget, gpointer data)
 static void get_pit(GtkWidget *widget, gpointer data) 
 {
 	g_print("Log: get_pit");
-    const char *message = "Beachten sie, dass sich ihr Gerät im Download-Modus befindet!\n";
+    const char *message = strcmp(language, "de") == 0 ? "Beachten sie, dass sich ihr Gerät im Download-Modus befindet!" : "Please note that your device is in download mode!";
     show_message(message);
     
     open_terminal_by_desktop("heimdall print-pit");
     g_print("Log: end get_pit\n");
 }
 
+// Function to set up button labels based on the language
+void set_button_labels_reboot_heim(char labels[][30]) 
+{
+    if (strcmp(language, "en") == 0) 
+    {
+        strcpy(labels[0], "Restart in Download");
+        strcpy(labels[1], "Help");
+        strcpy(labels[2], "Output pit");
+    } 
+    
+    else 
+    {
+        strcpy(labels[0], "In Download neustarten");
+        strcpy(labels[1], "Hilfe");
+        strcpy(labels[2], "Pit ausgeben");
+    }
+}
+
 /* start main programm */
 void reboot_heimdall(int argc, char *argv[])
 {
 	g_print("Log: reboot_heimdall\n");
-	GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
-    char button_labels[3][30] = {"In Download neustarten", "Hilfe", "Pit ausgeben"};
-
-    gtk_init(&argc, &argv);
+	GtkWidget *window, *grid, *button;
+    char button_labels[4][30];
     
+    gtk_init();
     apply_theme();
+    apply_language();
+    set_button_labels_reboot_heim(button_labels);
     
-    // create the window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Neustarten mit Heimdall");
+    window = gtk_window_new();
+    const char *reboot_heim_window = strcmp(language, "de") == 0 ? "Neustart (heimdall)" : "Reboot (heimdall)";
+    gtk_window_set_title(GTK_WINDOW(window), reboot_heim_window);
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // create the grid and centre it
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
-
-    // add the grid to the window
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    // add and centre all button
-    for (int i = 0; i < 3; i++) {
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+    
+    for (int i = 0; i < 3; i++) 
+    {
         button = gtk_button_new_with_label(button_labels[i]);
         gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
-
-        // execute css-provider for all buttons
-        add_css_provider(button, provider);
+        
         switch (i) {
             case 0:
                 g_signal_connect(button, "clicked", G_CALLBACK(reboot_from_adb_heimdall), NULL);
@@ -112,13 +120,18 @@ void reboot_heimdall(int argc, char *argv[])
                 break;
         }
     }
-	// clean the provider
-    g_object_unref(provider);
+    
+    // free the provider
+	if (provider != NULL) 
+	{
+	    g_object_unref(provider);
+	}
+	
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
-    // show window
-    gtk_widget_show_all(window);
-
-    // run main-gtk-loop
-    gtk_main();
+     // run GTK main loop
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop); 
+    
     g_print("Log: end reboot_heimdall\n");
 }
