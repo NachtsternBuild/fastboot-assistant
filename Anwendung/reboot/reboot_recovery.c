6c32b290b813544b42711241bf622a622f1b8bd2
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-21 16:56:36 +0100

    for #59 and #26
    support GTK4 and the second language

--- Anwendung/reboot/reboot_recovery.c
+++ Anwendung/reboot/reboot_recovery.c
@@ -31,10 +31,8 @@
 static void start_recovery_adb(GtkWidget *widget, gpointer data) 
 {
     g_print("Log: start_recovery_adb\n");
-    const char *title, *message;
-    title = "Reboot Recovery";
-    message = "Beachten sie, dass USB-Debugging aktiviert ist in den Entwickleroptionen!\n";    
-    show_message_with_title(title, message);
+    const char *message = strcmp(language, "de") == 0 ? "Beachten sie, dass USB-Debugging aktiviert ist in den Entwickleroptionen!" : "Please note that USB debugging is activated in the developer options!";    
+    show_message(message);
     
     char *device_command = adb_command();
     char command[256];
@@ -48,7 +46,7 @@ static void start_recovery_adb(GtkWidget *widget, gpointer data)
 static void start_recovery_fastboot(GtkWidget *widget, gpointer data) 
 {
     g_print("Log: start_recovery_fastboot\n");
-    const char *message = "Beachten sie, dass sich ihr Gerät im Fastboot-Modus befindet!\n";
+    const char *message = strcmp(language, "de") == 0 ? "Beachten sie, dass sich ihr Gerät im Fastboot-Modus befindet!" : "Please note that your device is in fastboot mode!";
     show_message(message);
     
     char *device_command = fastboot_command();
@@ -59,44 +57,53 @@ static void start_recovery_fastboot(GtkWidget *widget, gpointer data)
     g_print("Log: end start_recovery_fastboot\n");
 }
 
+// Function to set up button labels based on the language
+void set_button_labels_reboot(char labels[][30]) 
+{
+    if (strcmp(language, "en") == 0) 
+    {
+        strcpy(labels[0], "Restart ADB");
+        strcpy(labels[1], "Restarting Fastboot");
+    }
+    
+    else
+    {
+    	strcpy(labels[0], "Neustart von ADB");
+    	strcpy(labels[1], "Neustart von Fastboot");
+    }
+} 
+
+
 /* main function of reboot*/
 void reboot_recovery(int argc, char *argv[]) 
 {
     g_print("Log: reboot_recovery\n");
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
-    char button_labels[2][30] = {"Neustart von ADB", "Neustart von Fastboot"};
-
-    gtk_init(&argc, &argv);
-
-    gtk_init(&argc, &argv);
+    GtkWidget *window, *grid, *button;
+    char button_labels[2][30];
+    
+    gtk_init();
     apply_theme();
-
-     // create the window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Neustart Recovery");
+    apply_language();
+    set_button_labels_reboot(button_labels);
+    
+    window = gtk_window_new();
+    const char *reboot_reco_window = strcmp(language, "de") == 0 ? "Neustart Recovery" : "Reboot Recovery";
+    gtk_window_set_title(GTK_WINDOW(window), reboot_reco_window);
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-    // create the grid and centre it
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
-
-    // add the grid to the window
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    // add and centre all button
-    for (int i = 0; i < 2; i++) {
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+    
+    for (int i = 0; i < 2; i++) 
+    {
         button = gtk_button_new_with_label(button_labels[i]);
-        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
-
-        // execute css-provider for all buttons
-        add_css_provider(button, provider);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 2, i / 2, 1, 1);
+        
         switch (i) {
             case 0:
                 g_signal_connect(button, "clicked", G_CALLBACK(start_recovery_adb), NULL);
@@ -106,13 +113,18 @@ void reboot_recovery(int argc, char *argv[])
                 break;
         }
     }
-	// cleaing the provider
-    g_object_unref(provider);
+    
+    // free the provider
+	if (provider != NULL) 
+	{
+	    g_object_unref(provider);
+	}
+	
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
-    // show window
-    gtk_widget_show_all(window);
-
-    // run main-gtk-loop
-    gtk_main();
+     // run GTK main loop
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop); 
+    
     g_print("Log: end reboot_recovery\n");
 }
