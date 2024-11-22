3bb76a3322eae22d625c118e70d65fdd3b11233b
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-22 16:20:45 +0100

    for #59 and #26
    support GTK4 and the second language

--- Anwendung/preflash/prepare.c
+++ Anwendung/preflash/prepare.c
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
+#include "language_check.h"
 #include "program_functions.h"
 #include "function_header.h"
 #include "file_chooser_header.h"
@@ -26,44 +27,61 @@
 
 #define MAX_BUFFER_SIZE 256
 
+// Function to set up button labels based on the language
+void set_button_labels_prepare(char labels[][30]) 
+{
+    if (strcmp(language, "en") == 0) 
+    {
+        strcpy(labels[0], "Boot.img");
+        strcpy(labels[1], "Recovery.img");
+        strcpy(labels[0], "Vendor.img");
+        strcpy(labels[1], "System.img");
+        strcpy(labels[0], "payload.zip");
+        strcpy(labels[1], "Decompress (xz)");
+    } 
+    
+    else 
+    {
+        strcpy(labels[0], "Boot.img");
+        strcpy(labels[1], "Recovery.img");
+        strcpy(labels[0], "Vendor.img");
+        strcpy(labels[1], "System.img");
+        strcpy(labels[0], "payload.zip");
+        strcpy(labels[1], "Dekomprimieren (xz)");
+    }
+}
+
 // Callback functions for each button
 /* main function of prepare*/
 void prepare(int argc, char *argv[]) 
 {
     g_print("Log: prepare\n");
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
-    char button_labels[6][30] = {"Boot.img", "Recovery.img", "Vendor.img", 
-                                 "System.img", "payload.zip", "Dekomprimieren (xz)"};
-
-    gtk_init(&argc, &argv);
+    GtkWidget *window, *grid, *button;
+    char button_labels[6][30];
+    
+    gtk_init();
     apply_theme();
-
-     // create the window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Vorbereiten:");
+    apply_language();
+    set_button_labels_prepare(button_labels);
+    
+    window = gtk_window_new();
+    const char *prepare_window = strcmp(language, "de") == 0 ? "Vorbereiten:" : "Prepare:";
+    gtk_window_set_title(GTK_WINDOW(window), prepare_window);
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-    // create the grid and centre it
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
-
-    // add the grid to the window
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    // add and centre all button
-    for (int i = 0; i < 6; i++) {
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+    
+    for (int i = 0; i < 2; i++) 
+    {
         button = gtk_button_new_with_label(button_labels[i]);
-        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
-
-        // execute css-provider for all buttons
-        add_css_provider(button, provider);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 2, i / 2, 1, 1);
+        
         switch (i) {
             case 0:
                 g_signal_connect(button, "clicked", G_CALLBACK(show_file_chooser), (gpointer)rename_boot);
@@ -85,14 +103,18 @@ void prepare(int argc, char *argv[])
                 break;            
         }
     }
+    
+     // free the provider
+	if (provider != NULL) 
+	{
+	    g_object_unref(provider);
+	}
 	
-	// clean the storage
-    g_object_unref(provider);
-	
-    // show window
-    gtk_widget_show_all(window);
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
-    // run main-gtk-loop
-    gtk_main();
+     // run GTK main loop
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop); 
+    
     g_print("Log: end prepare\n");
 }
