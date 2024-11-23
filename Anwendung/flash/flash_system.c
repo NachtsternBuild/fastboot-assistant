97e00a649999000c648cac99c5b1bf4115896b89
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-23 12:56:50 +0100

    for #59 and #26
    - support GTK4 and the second language
    - use command with spinner

--- Anwendung/flash/flash_system.c
+++ Anwendung/flash/flash_system.c
@@ -20,6 +20,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <gtk/gtk.h>
+#include "language_check.h"
 #include "program_functions.h"
 #include "function_header.h"
 #include "flash_function_header.h"
@@ -41,7 +42,7 @@ void flash_system_inactive(const char *slot)
 	
 	g_print("Log: Run: %s\n", function_command);
     // run command in the terminal
-    open_terminal_by_desktop(function_command);
+    command_with_spinner(function_command);
     free(device_command);
     g_print("Log: end flash_system_inactive");
 }
@@ -55,7 +56,7 @@ void system_to_activ(GtkWidget *widget, GtkWindow *window)
     char function_command[3072];
     char *device_command = fastboot_command();
     snprintf(function_command, sizeof(function_command), "%s erase system", device_command);
-    g_print("Führe aus: %s", function_command);
+    g_print("Log: Run: %s", function_command);
     system(function_command);
     free(device_command);
     
@@ -82,7 +83,7 @@ void system_to_inactiv(GtkWidget *widget, GtkWindow *window)
     
     // get active slot
     check_active_slot(active_slot, sizeof(active_slot));
-    g_print("Aktiver Slot: %s\n", active_slot);
+    g_print("Log: active slot: %s\n", active_slot);
 
     // get inactive slot
     if (strcmp(active_slot, "a") == 0) 
@@ -97,13 +98,13 @@ void system_to_inactiv(GtkWidget *widget, GtkWindow *window)
     
     else 
     {
-        fprintf(stderr, "Unbekannter aktiver Slot: %s\n", active_slot);
+        fprintf(stderr, "Unknown active slot: %s\n", active_slot);
         exit(EXIT_FAILURE);
     }
 	char function_command[3072];
     char *device_command = fastboot_command();
     snprintf(function_command, sizeof(function_command), "%s erase system_%s", device_command, inactive_slot);
-    g_print("Führe aus: %s", function_command);
+    g_print("Log: Run: %s", function_command);
     system(function_command);
     free(device_command);
     
@@ -112,43 +113,53 @@ void system_to_inactiv(GtkWidget *widget, GtkWindow *window)
     g_print("Log: system_to_inactiv");
 }
 
-// main function
+
+// Function to set up button labels based on the language
+void set_button_labels_flash_system(char labels[][30]) 
+{
+    if (strcmp(language, "en") == 0) 
+    {
+        strcpy(labels[0], "System");
+        strcpy(labels[1], "System (heimdall)");
+        strcpy(labels[2], "System (inactive)");
+    } 
+    
+    else 
+    {
+        strcpy(labels[0], "System");
+        strcpy(labels[1], "System (heimdall)");
+        strcpy(labels[2], "System (inactive)");
+    }
+}
+
+/* main function - flash_system */
 void flash_system(int argc, char *argv[])
 {
 	g_print("Log: flash_system");
-	GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
+	GtkWidget *window, *grid, *button;
+    char button_labels[3][30];
     
-    char button_labels[3][30] = {"System", "System (heimdall)", "System (inactive)"};
-
-    gtk_init(&argc, &argv);
-	apply_theme();
-	
-    // create the window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Flash:");
+    gtk_init();
+    apply_theme();
+    apply_language();
+    set_button_labels_flash_system(button_labels);
+    
+    window = gtk_window_new();
+    gtk_window_set_title(GTK_WINDOW(window), "Flashen:");
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // create the grid and centre it
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+    gtk_window_set_child(GTK_WINDOW(window), grid);
 
-    // add the grid to the window
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    // add and centre all button
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) 
+    {
         button = gtk_button_new_with_label(button_labels[i]);
         gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
-
-        // execute css-provider for all buttons
-        add_css_provider(button, provider);
         
         switch (i) {
             case 0:
@@ -163,14 +174,17 @@ void flash_system(int argc, char *argv[])
         }
     }
     
-    // clean the storage
-    g_object_unref(provider);
-
-    // show window
-    gtk_widget_show_all(window);
-
-    // run main-gtk-loop
-    gtk_main();
+    // free the provider
+	if (provider != NULL) 
+	{
+	    g_object_unref(provider);
+	}
 	
-	g_print("Log: end flash_system");
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
+
+     // run GTK main loop
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop); 
+    
+    g_print("Log: end flash_system\n");
 }
