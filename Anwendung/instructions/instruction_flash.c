eaac3ef5a25ff8391b40553eaefd2e8e6ada1bf8
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-27 16:24:51 +0100

    for #59 and #26
    support GTK4 and the second language

--- Anwendung/instructions/instruction_flash.c
+++ Anwendung/instructions/instruction_flash.c
@@ -19,8 +19,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
+#include "language_check.h"
 #include "program_functions.h"
-#include "instruction_header.h"
+#include "function_header.h"
 
 #define MAX_BUFFER_SIZE 256
 
@@ -67,45 +68,61 @@ static void inst_others(GtkWidget *widget, gpointer data)
 	instruction_others();
 }
 
+// Function to set up button labels based on the language
+void set_button_labels_instruction_flash(char labels[][30]) 
+{
+    if (strcmp(language, "en") == 0) 
+    {
+        strcpy(labels[0], "Rooting");
+        strcpy(labels[1], "Vendor (VNDK)");
+        strcpy(labels[2], "Generic System Image (GSI)");
+        strcpy(labels[3], "Custom ROMs");
+        strcpy(labels[4], "Recovery");
+        strcpy(labels[5], "Others");
+    } 
+    
+    else 
+    {
+        strcpy(labels[0], "Rooten");
+        strcpy(labels[1], "Vendor (VNDK)");
+        strcpy(labels[2], "Generic System Image (GSI)");
+        strcpy(labels[3], "Custom ROMs");
+        strcpy(labels[4], "Recovery");
+        strcpy(labels[5], "Anderes");
+    }
+}
+
 /* start main programm */
 void instruction_flash(int argc, char *argv[])
 {
 	g_print("Log: instruction_flash\n");
-	GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *button;
-    char button_labels[6][30] = {"Rooten", "Vendor (VNDK)", "Generic System Image (GSI)", 
-    							 "Custom ROMs", "Recovery", "Anderes"};
-
-    gtk_init(&argc, &argv);
-    apply_theme();
+	GtkWidget *window, *grid, *button;
+    char button_labels[6][30];
     
-     // create the window
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Anleitungen - Flash");
+    gtk_init();
+    apply_theme();
+    apply_language();
+    set_button_labels_instruction_flash(button_labels);
+    
+    window = gtk_window_new();
+    const char *instruction_flash_window = strcmp(language, "de") == 0 ? "Anleitungen - Flashen" : "Instructions - Flashing";
+    gtk_window_set_title(GTK_WINDOW(window), instruction_flash_window);
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-    // create the grid and centre it
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
     gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-    
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
-
-    // add the grid to the window
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    // add and centre all button
-    for (int i = 0; i < 6; i++) {
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+    
+    for (int i = 0; i < 6; i++) 
+    {
         button = gtk_button_new_with_label(button_labels[i]);
         gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
-
-        // execute css-provider for all buttons
-        add_css_provider(button, provider);
         
-        switch (i) {
+         switch (i) {
             case 0:
                 g_signal_connect(button, "clicked", G_CALLBACK(inst_root), NULL);
                 break;
@@ -126,13 +143,19 @@ void instruction_flash(int argc, char *argv[])
                 break;          
         }
     }
-	// cleaning the provider
-    g_object_unref(provider);
+    
+    // free the provider
+	if (provider != NULL) 
+	{
+	    g_object_unref(provider);
+	}
+	
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
-    // show window
-    gtk_widget_show_all(window);
-
-    // run main-gtk-loop
-    gtk_main();
+     // run GTK main loop
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop); // GTK-Hauptschleife starten
+    
     g_print("Log: end instruction_flash\n");
-}   
+}
+
