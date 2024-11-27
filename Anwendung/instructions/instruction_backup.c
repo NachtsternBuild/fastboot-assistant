1d92921578972dfa82b530f7cc9ab99db5c1b9ff
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-27 16:07:47 +0100

    for #59 and #26
    support GTK4 and the second language

--- Anwendung/instructions/instruction_backup.c
+++ Anwendung/instructions/instruction_backup.c
@@ -19,66 +19,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
+#include "language_check.h"
 #include "program_functions.h"
-
-static void backup_text()
-{
-	g_print("Log: Die Lösung ist: 42–39+38+1");
-}
 
 void instruction_backup(int argc, char *argv[]) 
 {
     g_print("Log: instruction_backup\n");
-    GtkWidget *window;
+    // GTK init
+    gtk_init();
+
+    apply_theme();
+    apply_language();
+    
+    GtkWidget *window, *notebook;
     GtkWidget *page_backup1;
     GtkWidget *label_backup1_1, *label_backup1_2, *label_backup1_3;
     GtkWidget *button_backup1;
-
-    gtk_init(&argc, &argv);
-    apply_theme();
     
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    // create the main window
+    window = gtk_window_new();
     gtk_window_set_title(GTK_WINDOW(window), "Backup");
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    
-     // Connect close function to 'destroy' signal
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	
-	GtkWidget *notebook = gtk_notebook_new();
-    gtk_container_add(GTK_CONTAINER(window), notebook);
-    
-    if (!GTK_IS_NOTEBOOK(notebook)) 
-    {
-    	g_warning("Notebook is not initialized properly.");
-    	return;
-	}
-	
-	// page 1
-    page_backup1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+
+    // create the notebook
+    notebook = gtk_notebook_new();
+    gtk_window_set_child(GTK_WINDOW(window), notebook);
+
+    // page 1
+    page1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+
+    // button and label
     button_backup1 = gtk_button_new_with_label("Backups");
-    label_backup1_1 = gtk_label_new("Das Backup dient dazu, sie vor Datenverlust zu schützen \nund ist vor jedem flash Vorgang zu empfehlen. \nEs gibt zwei Varianten des Backups: ");
-    label_backup1_2 = gtk_label_new("1. Das Backup mit Root 2. Das Backup ohne Root. \nBeim Backup mit Root werden die Partitionen als Images \nkopiert und können später wieder geflasht werden.");
-    label_backup1_3 = gtk_label_new("Beim Backup ohne Root kommt 'Open Android Backup' zum Einsatz. \nEs ist ein Open Source Tool für Linux.");
-    gtk_box_pack_start(GTK_BOX(page_backup1), button_backup1, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(page_backup1), label_backup1_1, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page_backup1), label_backup1_2, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(page_backup1), label_backup1_3, TRUE, TRUE, 0);
-    g_signal_connect(button_backup1, "clicked", G_CALLBACK(backup_text), notebook);
-    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page_backup1, gtk_label_new("Backup"));
+    label_backup1_1 = gtk_label_new(g_strcmp0(language, "de") == 0 ? "Das Backup dient dazu, sie vor Datenverlust zu schützen \nund ist vor jedem flash Vorgang zu empfehlen. \nEs gibt zwei Varianten des Backups: " : "The backup serves to protect you from data loss \nand is recommended before every flash operation. \nThere are two types of backup: ");
+    label_backup1_2 = gtk_label_new(g_strcmp0(language, "de") == 0 ? "1. Das Backup mit Root 2. Das Backup ohne Root. \nBeim Backup mit Root werden die Partitionen als Images \nkopiert und können später wieder geflasht werden." : "1. the backup with root 2. the backup without root. \nWhen backing up with root, the partitions are copied as images and can be flashed again later.");
+    label_backup1_3 = gtk_label_new(g_strcmp0(language, "de") == 0 ? "Beim Backup ohne Root kommt 'Open Android Backup' zum Einsatz. \nEs ist ein Open Source Tool für Linux." : "'Open Android Backup' is used for backups without root. \nIt is an open source tool for Linux.");
     
-    // run css-provider
-    add_css_provider(button_backup1, provider);
-    add_css_provider(label_backup1_1, provider);
-    add_css_provider(label_backup1_2, provider);
-    add_css_provider(label_backup1_3, provider);
+    // add everything to the page
+    gtk_box_append(GTK_BOX(page1), button_backup1);
+    gtk_box_append(GTK_BOX(page1), label_backup1_1);
+    gtk_box_append(GTK_BOX(page1), label_backup1_2);
+    gtk_box_append(GTK_BOX(page1), label_backup1_3);
+
+    // add page to the notebook
+    gtk_notebook_append_page(GTK_NOTEBOOK(notebook), page1, gtk_label_new("Backup");
     
-    // clean the storage
-    g_object_unref(provider);
-	
-	// show all widgets
-    gtk_widget_show_all(window);
-	
-	// run gtk mainloop
-    gtk_main();
+    // show all widgets
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
+
+     // run GTK main loop
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(loop); 
     g_print("Log: end instruction_backup\n");
 }
