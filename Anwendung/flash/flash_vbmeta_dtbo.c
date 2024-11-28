7ff3decb814ed93739934d199c6d0babe89f4264
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:25:25 +0100

    fix an error

--- Anwendung/flash/flash_vbmeta_dtbo.c
+++ Anwendung/flash/flash_vbmeta_dtbo.c
@@ -112,7 +112,7 @@ void flash_vbmeta_dtbo(int argc, char *argv[])
     char button_labels[6][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_flash_vbmeta_dtbo(button_labels);
