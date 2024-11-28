9140a82672f8926d3d1f29f72b5b01a3f294855b
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 16:59:25 +0100

    fix #77

--- Anwendung/reboot/reboot_fastboot.c
+++ Anwendung/reboot/reboot_fastboot.c
@@ -94,7 +94,7 @@ void set_button_labels_reboot_fast(char labels[][30])
 {
     if (strcmp(language, "en") == 0) 
     {
-        strcpy(labels[0], "Restart from ADB");
+        strcpy(labels[0], "Restart ADB");
         strcpy(labels[1], "Restarting Fastboot");
         strcpy(labels[2], "Fastboot help");
         strcpy(labels[3], "Bootloader variables");
@@ -112,11 +112,12 @@ void set_button_labels_reboot_fast(char labels[][30])
 /* start main programm */
 void reboot_fastboot(int argc, char *argv[])
 {
-    g_print("Log: reboot_fastboot\n");
-    GtkWidget *window, *grid, *button;
+	g_print("Log: reboot_fastboot\n");
+	GtkWidget *window, *grid, *button;
     char button_labels[4][30];
     
     gtk_init();
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_reboot_fast(button_labels);
@@ -125,7 +126,7 @@ void reboot_fastboot(int argc, char *argv[])
     const char *reboot_fast_window = strcmp(language, "de") == 0 ? "Neustart Fastboot" : "Reboot Fastboot";
     gtk_window_set_title(GTK_WINDOW(window), reboot_fast_window);
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), main_loop);
     
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
@@ -164,8 +165,7 @@ void reboot_fastboot(int argc, char *argv[])
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
      // run GTK main loop
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-    g_main_loop_run(loop); 
+    g_main_loop_run(main_loop); 
     
     g_print("Log: end reboot_fastboot\n");
 }
