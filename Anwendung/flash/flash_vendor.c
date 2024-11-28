f00100f083797b33a605108c0fa2823185008cd9
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 16:55:34 +0100

    fix #77

--- Anwendung/flash/flash_vendor.c
+++ Anwendung/flash/flash_vendor.c
@@ -92,6 +92,7 @@ void flash_vendor(int argc, char *argv[])
     char button_labels[6][30];
     
     gtk_init();
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_flash_vendor(button_labels);
@@ -99,7 +100,7 @@ void flash_vendor(int argc, char *argv[])
     window = gtk_window_new();
     gtk_window_set_title(GTK_WINDOW(window), "Flashen:");
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), main_loop);
     
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
@@ -144,8 +145,7 @@ void flash_vendor(int argc, char *argv[])
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
      // run GTK main loop
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-    g_main_loop_run(loop); 
+    g_main_loop_run(main_loop); 
     
     g_print("Log: end flash_vendor\n");
 }
