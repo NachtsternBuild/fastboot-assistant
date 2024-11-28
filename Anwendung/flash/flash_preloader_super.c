2fc57cc71257c7cc5d3307896df9d0b0596617cc
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:23:33 +0100

    fix an error

--- Anwendung/flash/flash_preloader_super.c
+++ Anwendung/flash/flash_preloader_super.c
@@ -108,7 +108,7 @@ void flash_preloader_super(int argc, char *argv[])
     char button_labels[8][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_flash_preloader_super(button_labels);
