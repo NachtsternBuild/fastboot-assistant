ae1687aba884318e2dd1db08ede64fa3653e98fb
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:26:06 +0100

    fix an error

--- Anwendung/flash/flash_vendor.c
+++ Anwendung/flash/flash_vendor.c
@@ -92,7 +92,7 @@ void flash_vendor(int argc, char *argv[])
     char button_labels[6][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_flash_vendor(button_labels);
