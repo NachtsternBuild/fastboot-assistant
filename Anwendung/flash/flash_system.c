5ab02a7ab6770b1e408a5a4b5b42c10033f82851
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:25:05 +0100

    fix an error

--- Anwendung/flash/flash_system.c
+++ Anwendung/flash/flash_system.c
@@ -140,7 +140,7 @@ void flash_system(int argc, char *argv[])
     char button_labels[3][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_flash_system(button_labels);
