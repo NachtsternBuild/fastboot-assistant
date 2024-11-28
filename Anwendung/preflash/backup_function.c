69b8366fba2758a63b26024c7b0455bb930030b0
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:15:42 +0100

    fix an error

--- Anwendung/preflash/backup_function.c
+++ Anwendung/preflash/backup_function.c
@@ -76,7 +76,7 @@ void backup_function(int argc, char *argv[])
     char button_labels[2][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_backup(button_labels);
