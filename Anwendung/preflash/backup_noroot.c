ef6c47562f56f30d76c8529216d2d09606192d23
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:06:12 +0100

    fix #77

--- Anwendung/preflash/backup_noroot.c
+++ Anwendung/preflash/backup_noroot.c
@@ -123,6 +123,7 @@ void backup_noroot(int argc, char *argv[])
     char button_labels[3][30];
     
     gtk_init();
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_backup_noroot(button_labels);
@@ -130,7 +131,7 @@ void backup_noroot(int argc, char *argv[])
     window = gtk_window_new();
     gtk_window_set_title(GTK_WINDOW(window), "Open Android Backup");
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), main_loop);
     
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
@@ -169,8 +170,7 @@ void backup_noroot(int argc, char *argv[])
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
      // run GTK main loop
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-    g_main_loop_run(loop); // GTK-Hauptschleife starten
+    g_main_loop_run(main_loop); // GTK-Hauptschleife starten
     
     g_print("Log: end backup_noroot\n");
 }
