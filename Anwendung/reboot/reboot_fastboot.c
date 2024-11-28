69945b0996f722f8e065153d776660b64c0da17c
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:14:29 +0100

    fix an error

--- Anwendung/reboot/reboot_fastboot.c
+++ Anwendung/reboot/reboot_fastboot.c
@@ -117,7 +117,7 @@ void reboot_fastboot(int argc, char *argv[])
     char button_labels[4][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_reboot_fast(button_labels);
