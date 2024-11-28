c2a0c6d2773360b5a8343c06407b8f235c750ba4
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:23:11 +0100

    fix an error

--- Anwendung/flash/flash_others.c
+++ Anwendung/flash/flash_others.c
@@ -89,7 +89,7 @@ void flash_others(int argc, char *argv[])
     char button_labels[2][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_flash_others(button_labels);
