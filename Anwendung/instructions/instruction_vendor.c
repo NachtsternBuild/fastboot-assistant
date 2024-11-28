9a28d8f14d495a87bead94f98f67e59ada3cf5ff
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:39:50 +0100

    fix #77

--- Anwendung/instructions/instruction_vendor.c
+++ Anwendung/instructions/instruction_vendor.c
@@ -28,7 +28,7 @@ void instruction_vendor(int argc, char *argv[])
     
     // GTK init
     gtk_init();
-
+	main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     
@@ -41,7 +41,7 @@ void instruction_vendor(int argc, char *argv[])
     window = gtk_window_new();
     gtk_window_set_title(GTK_WINDOW(window), "Vendor Native Development Kit (VNDK)");
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-	g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+	g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), main_loop);
 
     // create the notebook
     notebook = gtk_notebook_new();
@@ -69,7 +69,6 @@ void instruction_vendor(int argc, char *argv[])
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
      // run GTK main loop
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-    g_main_loop_run(loop); 
+    g_main_loop_run(main_loop); 
     g_print("Log: end instruction_vendor\n");
 }
