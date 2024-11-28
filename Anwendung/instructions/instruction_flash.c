38906a549be3fd35cd876ab2bdb08e7f0e019297
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:36:56 +0100

    fix #77

--- Anwendung/instructions/instruction_flash.c
+++ Anwendung/instructions/instruction_flash.c
@@ -101,6 +101,7 @@ void instruction_flash(int argc, char *argv[])
     char button_labels[6][30];
     
     gtk_init();
+    main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_instruction_flash(button_labels);
@@ -109,7 +110,7 @@ void instruction_flash(int argc, char *argv[])
     const char *instruction_flash_window = strcmp(language, "de") == 0 ? "Anleitungen - Flashen" : "Instructions - Flashing";
     gtk_window_set_title(GTK_WINDOW(window), instruction_flash_window);
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_window_destroy), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), main_loop);
     
     grid = gtk_grid_new();
     gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
@@ -154,8 +155,7 @@ void instruction_flash(int argc, char *argv[])
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
      // run GTK main loop
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-    g_main_loop_run(loop); // GTK-Hauptschleife starten
+    g_main_loop_run(main_loop); // GTK-Hauptschleife starten
     
     g_print("Log: end instruction_flash\n");
 }
