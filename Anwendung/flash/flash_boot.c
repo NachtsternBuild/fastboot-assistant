d7e47d75cd09af170fa5cd71918ef3f073a95c8d
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:22:28 +0100

    fix an error

--- Anwendung/flash/flash_boot.c
+++ Anwendung/flash/flash_boot.c
@@ -92,7 +92,7 @@ void flash_boot(int argc, char *argv[])
     char button_labels[6][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_flash_boot(button_labels);
