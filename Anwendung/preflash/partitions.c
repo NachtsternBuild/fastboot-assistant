5f03b87a25afb2fe76ce9f6c99dab8291576c4e4
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:17:37 +0100

    fix an error

--- Anwendung/preflash/partitions.c
+++ Anwendung/preflash/partitions.c
@@ -218,7 +218,7 @@ void partitions(int argc, char *argv[])
     char button_labels[9][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_partitions(button_labels);
