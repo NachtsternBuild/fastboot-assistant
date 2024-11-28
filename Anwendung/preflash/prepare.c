da3ae2065214bb7ad80fb30ec925fca9edd3bfcd
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-28 17:17:06 +0100

    fix an error

--- Anwendung/preflash/prepare.c
+++ Anwendung/preflash/prepare.c
@@ -60,7 +60,7 @@ void prepare(int argc, char *argv[])
     char button_labels[6][30];
     
     gtk_init();
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
     set_button_labels_prepare(button_labels);
