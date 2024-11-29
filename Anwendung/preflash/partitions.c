061952ae62cda079913010d9d1e650fd1c135707
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-29 15:57:32 +0100

    fix #78

--- Anwendung/preflash/partitions.c
+++ Anwendung/preflash/partitions.c
@@ -276,6 +276,7 @@ void partitions(int argc, char *argv[])
 	if (provider != NULL) 
 	{
 	    g_object_unref(provider);
+	    provider = NULL;
 	}
 	
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
