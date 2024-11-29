936e600e237ab5c363d30b5ebd8c26e118c4ba04
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-29 15:48:50 +0100

    fix #78

--- Anwendung/reboot/reboot_heimdall.c
+++ Anwendung/reboot/reboot_heimdall.c
@@ -127,6 +127,7 @@ void reboot_heimdall(int argc, char *argv[])
 	if (provider != NULL) 
 	{
 	    g_object_unref(provider);
+	    provider = NULL;
 	}
 	
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
