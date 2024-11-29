546511fa6101bd4b9b6554a5146970bf5632e30e
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-29 15:29:51 +0100

    fix #78

--- Anwendung/flash/flash_preloader_super.c
+++ Anwendung/flash/flash_preloader_super.c
@@ -161,6 +161,7 @@ void flash_preloader_super(int argc, char *argv[])
 	if (provider != NULL) 
 	{
 	    g_object_unref(provider);
+	    provider = NULL;
 	}
 	
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
