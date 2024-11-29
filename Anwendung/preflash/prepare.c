cb0e3e0b6ab85c4df53e799581f292d3fc7d0997
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-29 15:56:59 +0100

    fix #78

--- Anwendung/preflash/prepare.c
+++ Anwendung/preflash/prepare.c
@@ -109,6 +109,7 @@ void prepare(int argc, char *argv[])
 	if (provider != NULL) 
 	{
 	    g_object_unref(provider);
+	    provider = NULL;
 	}
 	
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
