42a15259540c12bfdfb23731e23d9c505634e9c4
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-29 15:29:02 +0100

    fix #78

--- Anwendung/flash/flash_system.c
+++ Anwendung/flash/flash_system.c
@@ -179,6 +179,7 @@ void flash_system(int argc, char *argv[])
 	if (provider != NULL) 
 	{
 	    g_object_unref(provider);
+	    provider = NULL;
 	}
 	
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
