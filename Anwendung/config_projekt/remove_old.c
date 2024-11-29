078a250b6f64669452d665debf408f52ff8d714b
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-29 15:51:35 +0100

    fix #78

--- Anwendung/config_projekt/remove_old.c
+++ Anwendung/config_projekt/remove_old.c
@@ -161,6 +161,7 @@ void remove_old(int argc, char *argv[])
 	if (provider != NULL) 
 	{
 	    g_object_unref(provider);
+	    provider = NULL;
 	}
 	
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
