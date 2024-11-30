b016c7662001f8deeb5c25f8c96d60d5ab67b83b
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-30 11:07:18 +0100

    fix #79

--- Anwendung/reboot/reboot.c
+++ Anwendung/reboot/reboot.c
@@ -136,5 +136,11 @@ void reboot(int argc, char *argv[])
      // run GTK main loop
     g_main_loop_run(main_loop); 
     
+    if (main_loop != NULL) 
+	{
+    	g_main_loop_unref(main_loop);
+    	main_loop = NULL;
+	}
+    
     g_print("Log: end reboot\n");
 }
