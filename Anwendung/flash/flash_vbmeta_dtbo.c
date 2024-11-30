009add36aed28e20891d31c297a6a39f1b2b08a0
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-30 12:39:53 +0100

    fix #79

--- Anwendung/flash/flash_vbmeta_dtbo.c
+++ Anwendung/flash/flash_vbmeta_dtbo.c
@@ -168,5 +168,11 @@ void flash_vbmeta_dtbo(int argc, char *argv[])
      // run GTK main loop
     g_main_loop_run(main_loop); 
     
+    if (main_loop != NULL) 
+	{
+    	g_main_loop_unref(main_loop);
+    	main_loop = NULL;
+	}
+    
     g_print("Log: end flash_vbmeta_dtbo\n");
 }
