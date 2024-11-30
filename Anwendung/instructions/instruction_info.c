7bd271d0527874c6a6a794c3435b83f1a1b263e3
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-30 13:09:45 +0100

    fix #79

--- Anwendung/instructions/instruction_info.c
+++ Anwendung/instructions/instruction_info.c
@@ -136,5 +136,12 @@ void instruction_info(int argc, char *argv[])
 
      // run GTK main loop
     g_main_loop_run(main_loop); 
+    
+    if (main_loop != NULL) 
+	{
+    	g_main_loop_unref(main_loop);
+    	main_loop = NULL;
+	}
+    
     g_print("Log: end instruction_info\n");
 }
