4d6cf6e08832dd92434cc72ffff0596368b256cc
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-30 10:46:36 +0100

    fix #79

--- Anwendung/updater.c
+++ Anwendung/updater.c
@@ -272,6 +272,13 @@ void updater(void)
 	
 	// run GTK main loop
     g_main_loop_run(main_loop); 
+    
+    if (main_loop != NULL) 
+	{
+    	g_main_loop_unref(main_loop);
+    	main_loop = NULL;
+	}
+	
     g_print("Log: end updater\n");
 }
 
