6a088602c69c30262bc56cfee4d4a0edeb54f8bf
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-30 12:01:36 +0100

    fix #79

--- Anwendung/preflash/partitions.c
+++ Anwendung/preflash/partitions.c
@@ -282,7 +282,13 @@ void partitions(int argc, char *argv[])
     gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
      // run GTK main loop
-    g_main_loop_run(main_loop); // GTK-Hauptschleife starten
+    g_main_loop_run(main_loop);
+    
+    if (main_loop != NULL) 
+	{
+    	g_main_loop_unref(main_loop);
+    	main_loop = NULL;
+	}
     
     g_print("Log: end partitions\n");
 }
