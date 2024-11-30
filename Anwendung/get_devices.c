a4798fdef28357410dfae3bd7a4bc4cf0113c990
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-30 10:48:11 +0100

    fix #79

--- Anwendung/get_devices.c
+++ Anwendung/get_devices.c
@@ -152,5 +152,11 @@ void get_devices(int argc, char *argv[])
      // run GTK main loop
     g_main_loop_run(main_loop); 
     
+    if (main_loop != NULL) 
+	{
+    	g_main_loop_unref(main_loop);
+    	main_loop = NULL;
+	}
+    
     g_print("Log: End of get_devices\n");
 }
