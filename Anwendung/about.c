04bf48666835190f12b92bd2b1d66a49c63bf6fc
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-30 10:46:12 +0100

    fix #79

--- Anwendung/about.c
+++ Anwendung/about.c
@@ -175,5 +175,11 @@ void about(int argc, char *argv[])
      // run GTK main loop
     g_main_loop_run(main_loop); 
     
+    if (main_loop != NULL) 
+	{
+    	g_main_loop_unref(main_loop);
+    	main_loop = NULL;
+	}
+    
     g_print("Log: end about\n");
 }
