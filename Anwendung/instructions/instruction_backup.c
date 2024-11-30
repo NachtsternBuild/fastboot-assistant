c82842bc588dfe3b4a54cb82a74397894f9ee99d
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-11-30 13:08:14 +0100

    fix #79

--- Anwendung/instructions/instruction_backup.c
+++ Anwendung/instructions/instruction_backup.c
@@ -69,5 +69,12 @@ void instruction_backup(int argc, char *argv[])
 
      // run GTK main loop
     g_main_loop_run(main_loop); 
+    
+    if (main_loop != NULL) 
+	{
+    	g_main_loop_unref(main_loop);
+    	main_loop = NULL;
+	}
+	
     g_print("Log: end instruction_backup\n");
 }
