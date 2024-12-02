209f280326cb818922bcadb8a8d6b3c9fc734035
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-12-02 16:59:05 +0100

    fix an error
    use the new close_dialog function

--- Anwendung/header/header_show_message.c
+++ Anwendung/header/header_show_message.c
@@ -25,42 +25,39 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 
-static void close_dialog(GtkButton *button, GtkWidget *dialog) 
-{
-    gtk_window_destroy(GTK_WINDOW(dialog));
-}
-
-// function to show message
+// Function to display a message
 void show_message(const char *message) 
 {
     GtkWidget *dialog;
     GtkWidget *content_area;
     GtkWidget *label;
     GtkWidget *ok_button;
-    
-    // Format the message
+
+    // Create formatted message
     const char *formatted_message = g_strdup_printf("\n\n%s\n\n", message);
 
-    // Create the dialog window without using deprecated functions
+    // Create dialog window
     dialog = gtk_window_new();
     gtk_window_set_title(GTK_WINDOW(dialog), "Information");
     gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
 
-    // Create a vertical box for the dialog content and add it to the dialog
+    // Vertical container for the contents of the dialog
     content_area = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_window_set_child(GTK_WINDOW(dialog), content_area);
 
-    // Create the label with the message and add it to the content area
+    // Create and add a label with the message
     label = gtk_label_new(NULL);
     gtk_label_set_markup(GTK_LABEL(label), formatted_message);
     gtk_box_append(GTK_BOX(content_area), label);
 
-    // Create an OK button and connect it to close the dialog
+    // Create an OK button and link it to the close function
     ok_button = gtk_button_new_with_label("OK");
     g_signal_connect(ok_button, "clicked", G_CALLBACK(close_dialog), dialog);
     gtk_box_append(GTK_BOX(content_area), ok_button);
 
     // Display the dialog and all its components
     gtk_widget_set_visible(dialog, TRUE);
+    
+    g_free((char *)formatted_message);
 }
 
