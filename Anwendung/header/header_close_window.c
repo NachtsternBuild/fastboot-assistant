4684c82ebd6e1df5bb196f91ebc2a77b33ae07e9
Elias Mörz <hallo.schneller50kmh@gmail.com>
2024-12-11 16:52:03 +0100

    for #87
    add function that close the window and end the mainloop

--- Anwendung/header/header_close_window.c
+++ Anwendung/header/header_close_window.c
@@ -28,11 +28,23 @@
 // close dialog
 void close_dialog(GtkButton *button, GtkWidget *dialog) 
 {
-    gtk_window_close(GTK_WINDOW(dialog));
+    gtk_window_destroy(GTK_WINDOW(dialog));
+    //gtk_window_close(GTK_WINDOW(dialog));
 }
 
 // close window
 void close_window(GtkButton *button, GtkWidget *window) 
 {
-    gtk_window_close(GTK_WINDOW(window));
+    gtk_window_destroy(GTK_WINDOW(window));
+    //gtk_window_close(GTK_WINDOW(window));
 }
+
+// close window
+void close_window_mainloop(GtkWidget *widget, gpointer data) 
+{
+    GtkWidget *window = GTK_WIDGET(data);
+    g_print("Log: Close window, end main loop.\n");
+    gtk_window_destroy(GTK_WINDOW(window));
+    g_main_loop_quit(main_loop);
+}
+
