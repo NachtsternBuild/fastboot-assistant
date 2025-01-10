74c7ac1b2c335855b7fb21c8503bfa6ec551cc0d
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-01-10 17:43:56 +0100

    rework function
    - use new log
    - use new flash function

--- Anwendung/flash/flash_images.c
+++ Anwendung/flash/flash_images.c
@@ -10,7 +10,7 @@
  *	zu erleichtern - flash_images		  	 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2025 Elias Mörz 		 *
  *-------------------------------------------*
  *
  */
@@ -30,12 +30,22 @@
 
 static void system_images(GtkWidget *widget, GtkWindow *window)
 {
-    flash_image(widget, window, "system", NULL, "system.img");
+    flash_image(widget, window, "system", NULL, "system.img", NULL);
 }
 
 static void userdata_images(GtkWidget *widget, GtkWindow *window)
 {
-    flash_image(widget, window, "userdata", NULL, "userdata.img");
+    flash_image(widget, window, "userdata", NULL, "userdata.img", NULL);
+}
+
+static void vbmeta_ab_images(GtkWidget *widget, GtkWindow *window)
+{
+    flash_image(widget, window, "vbmeta_a", "vbmeta_b", "vbmeta.img", "--disable-verity --disable-verification");
+}
+
+static void vbmeta_a_images(GtkWidget *widget, GtkWindow *window)
+{
+    flash_image(widget, window, "vbmeta", NULL, "vbmeta.img", "--disable-verity --disable-verification");
 }
 
 // function to flash the partitions
@@ -44,68 +54,59 @@ int flash_partition(const char *partition, const char *image_path, int ab_device
     // flash every partition on a/b-device
     if (ab_device && strcmp(partition, "system") != 0 && strcmp(partition, "userdata") != 0 && strcmp(partition, "vbmeta") != 0) 
     {
-        g_print("Log: Flash other Images (a/b).\n");
+        LOG_INFO("Flash other Images (a/b).");
         char function_command[3072];
         char *device_command = fastboot_command();
         snprintf(function_command, sizeof(function_command), "%s flash %s_a %s && %s flash %s_b %s", device_command, partition, image_path, device_command, partition, image_path);
-        g_print("Log: Run: %s\n", function_command);
+        LOG_INFO("Run: %s", function_command);
         command_with_spinner(function_command);
         free(device_command);
-        g_print("Log: Finished.\n");
+        LOG_INFO("Finished.");
     } 
+    
     // flash system on all devices
     else if (strcmp(partition, "system") == 0 && ab_device) 
     {
         char function_command[3072];
     	char *device_command = fastboot_command();
     	snprintf(function_command, sizeof(function_command), "%s erase system", device_command);
-    	g_print("Log: Run: %s", function_command);
+    	LOG_INFO("Run: %s", function_command);
     	system(function_command);
     	free(device_command);
         system_images(NULL, NULL);  // Passing NULL for widget and window
     }
+    
     // flash userdata on all devices
     else if (strcmp(partition, "userdata") == 0 && ab_device) 
     {
         userdata_images(NULL, NULL);  // Passing NULL for widget and window
     }
+    
     // flash vbmeta on a/b-devices
     else if (strcmp(partition, "vbmeta") == 0 && ab_device) 
     {
-        g_print("Log: Flash vbmeta on a/b-devices.\n");
-        char function_command[3072];
-        char *device_command = fastboot_command();
-        snprintf(function_command, sizeof(function_command), "%s flash --disable-verity --disable-verification vbmeta_a %s && %s flash --disable-verity --disable-verification vbmeta_b %s && exit", device_command, image_path, device_command, image_path);
-        g_print("Log: Run: %s", function_command);
-        open_terminal_by_desktop(function_command);
-        free(device_command);
-        g_print("Log: Finished.\n");
+        vbmeta_ab_images(NULL, NULL);
     }
+    
     // flash vbmeta on only-a-devices
     else if (strcmp(partition, "vbmeta") == 0)
     {
-        g_print("Log: Flash vbmeta on only-a-devices.\n");
-        char function_command[3072];
-        char *device_command = fastboot_command();
-        snprintf(function_command, sizeof(function_command), "%s flash --disable-verity --disable-verification vbmeta %s && exit", device_command, image_path);
-        g_print("Log: Run: %s", function_command);
-        open_terminal_by_desktop(function_command);
-        free(device_command);
-        g_print("Log: Finished.\n");
+        vbmeta_a_images(NULL, NULL);
     }
+    
     // for all only-a-devices - flash all partitions
     else 
     {
-        g_print("Log: Flash partitions without slot.\n");
+        LOG_INFO("Flash partitions without slot.");
         char function_command[3072];
         char *device_command = fastboot_command();
         snprintf(function_command, sizeof(function_command), "%s flash %s %s", device_command, partition, image_path);
-        g_print("Log: Run: %s\n", function_command);
+        LOG_INFO("Run: %s", function_command);
         command_with_spinner(function_command);
         free(device_command);
-        g_print("Log: Finished.\n");
+        LOG_INFO("Finished.");
     }
-    g_print("Log: Image %s successfully flashed on partition %s.\n", image_path, partition);
+    LOG_INFO("Image %s successfully flashed on partition %s.", image_path, partition);
     return 0;  // Return 0 to indicate success
 }
 
@@ -120,7 +121,7 @@ void flash_dir(const char *directory)
     dir = opendir(directory);
     if (dir == NULL) 
     {
-        perror("Log: Directory cannot be opened.\n");
+        LOG_ERROR("irectory cannot be opened.");
         exit(EXIT_FAILURE);
     }
     
@@ -141,7 +142,7 @@ void flash_dir(const char *directory)
 
             if (flash_partition(partition, image_path, ab_device) != 0) 
             {
-                fprintf(stderr, "Log: Error when flashing the partition %s\n", partition);
+                LOG_ERROR("Error when flashing the partition %s", partition);
                 exit(EXIT_FAILURE);
             }
         }
@@ -154,37 +155,24 @@ void flash_dir(const char *directory)
 /* main function - flash_images */
 void flash_images() 
 {
-    g_print("Log: flash_images\n");
-    // for Linux
-    const char *home = getenv("HOME");
-    if (home == NULL) 
-    {
-        g_print("Log: Environment variable HOME is not set.\n");
-        exit(EXIT_FAILURE);
-    }
+    LOG_INFO("flash_images");
     
-    // WSL logic
-    const char *user = getenv("USER");
-    if (user == NULL) 
-    {    
-        g_print("Log: Error: Could not determine the user name.\n");
-        exit(EXIT_FAILURE);  // close the program if there are errors
+    const char config_file[2048];
+    get_config_file_path(config_file, sizeof(config_file));
+    // load the path
+    const char *directory = load_path_from_file(config_file);
+
+    if (directory) 
+    {
+        LOG_INFO("Loaded path: %s", directory);
     }
 
-    char wsl_setup_base[MAX_PATH];
-    snprintf(wsl_setup_base, sizeof(wsl_setup_base), "/mnt/c/Users/%s", user);
-
-    // Set default path to ~/Downloads/ROM-Install
-    char directory[MAX_PATH];
-    // for Linux 
-    snprintf(directory, sizeof(directory), "%s/Downloads/ROM-Install", home);
-    // for WSL
-    // snprintf(directory, sizeof(directory), "%s/Downloads/ROM-Install", wsl_setup_base);
-
-    g_print("Log: Images from the %s directory are flashed.\n", directory);
+    LOG_INFO("Start flashing Images from the %s directory.", directory);
     // start flash from directory
     flash_dir(directory);
     
-    g_print("Log: end flash_images\n");
+    g_free(directory); // free the info (because g_file_get_contents was used)
+    
+    LOG_INFO("end flash_images");
 }
 
