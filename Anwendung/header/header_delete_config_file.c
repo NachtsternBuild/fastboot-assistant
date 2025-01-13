ded27135afa277ec94d59f050ed4b449acf5eb4b
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-01-13 10:48:01 +0100

    add new logging

--- Anwendung/header/header_delete_config_file.c
+++ Anwendung/header/header_delete_config_file.c
@@ -10,7 +10,7 @@
  *	zu erleichtern  						 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2024 Elias Mörz 		 *
+ *      (C) Copyright 2025 Elias Mörz 		 *
  *-------------------------------------------*
  *											 *
  *      Headerpart - delete_config_file		 *
@@ -34,28 +34,8 @@
 // function that delete the dark.txt
 void delete_config_file() 
 {
-    // use the full path
-    char *home = getenv("HOME");
-    if (home == NULL) 
-    {
-        perror("Log: Error when retrieving the home directory.\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    // WSL logic
-	const char *user = getenv("USER");
-	if (user == NULL) 
-	{	
-    	g_print("Log: Error: Could not determine the user name.\n");
-    	exit(1);  // close the program if there are errors
-	}
-
     char dir_path[512];
-    //char wsl_dir[512];
-    // for windows
-	//snprintf(wsl_dir, sizeof(wsl_dir), "/mnt/c/Users/%s", user);
-	//snprintf(dir_path, sizeof(dir_path), "%s/Downloads/ROM-Install/config", wsl_dir);
-    snprintf(dir_path, sizeof(dir_path), "%s/Downloads/ROM-Install/config", home);
+    get_config_dir(dir_path, sizeof(dir_path));
 
     char path[512];
     snprintf(path, sizeof(path), "%s/%s", dir_path, CONFIG_SETUP_FILE);
@@ -63,11 +43,12 @@ void delete_config_file()
     // try to remove the file
     if (remove(path) == 0) 
     {
-        g_print("Log: File '%s' successfully deleted.\n", path);
+        LOG_INFO("File '%s' successfully deleted.", path);
     } 
+    
     else 
     {
-        perror("Log: Error deleting the file.\n");
+        LOG_ERROR("Error deleting the file.");
     }
 }
 
