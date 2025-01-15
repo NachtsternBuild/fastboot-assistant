7f7aa1a05f510d80711a2e4a4291b61e4291061a
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-01-15 17:11:36 +0100

    for #90
    create an UI with choice between devmode and stable updater

--- Anwendung/updater.c
+++ Anwendung/updater.c
@@ -2,295 +2,106 @@
  *-------------------------------------------*
  *                Projekt 122 - GUI          *
  *-------------------------------------------*
- *       Apache License, Version 2.0         *
+ *  	Apache License, Version 2.0		     *
  *-------------------------------------------*
  *                                           *
- *  Programm, um das Installieren von       *
- *  Custom-ROMs und GSIs auf Android-Geräte  *
- *  zu erleichtern - updater                 *
+ *  Programm um das installieren von 		 *
+ *	Custom-ROM und GSIs auf Android-Geräte 	 *
+ *	zu erleichtern - updater				 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2025 Elias Mörz        *
+ *      (C) Copyright 2025 Elias Mörz 		 *
  *-------------------------------------------*
  *
  */
 
+/* headers that used in the programm */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <gtk/gtk.h>
 #include "language_check.h"
 #include "program_functions.h"
+#include "function_header.h"
 
-char output_file[2048];
-char package_url[2048];
-char bash_script_path[2048];
-GtkWidget *update_window_install;
-GtkWidget *info_button;
+#define MAX_BUFFER_SIZE 256
 
-// Function to quit the application - updater
-gboolean quit_application_updater(gpointer data)
+// include all functions
+extern void backup_root();
+extern void backup_noroot();
+
+// Callback functions for each button
+// function to start the normal updater 
+static void start_updater_stable(GtkWidget *widget, gpointer data) 
 {
-    exit(1);
-    return FALSE;  // Return FALSE to ensure the timeout callback is called only once
+    updater_stable();
 }
 
-// Function to create and execute the Bash script
-void create_and_run_bash_script(const char *package_url, const char *package_type)
+// function to start the updater with devmode
+static void start_updater_devmode(GtkWidget *widget, gpointer data) 
 {
-    char updater_dir[2048];
-	char updater_path[2048];
-	char mkdir_command[2048];
+    updater_devmode();
+}
 
-	get_config_file_path(updater_dir, sizeof(updater_dir));
-	
-	// Create directory for updater
-    snprintf(updater_path, sizeof(updater_path), "%s/Updater", updater_dir);
-    
-    // create ~/.config/fasboot-assistant/Updater
-    snprintf(mkdir_command, sizeof(mkdir_command), "mkdir -p %s", updater_path);
-    if (system(mkdir_command) != 0)
+// Function to set up button labels based on the language
+void set_button_labels_updater(char labels[][30]) 
+{
+    if (strcmp(language, "en") == 0) 
     {
-        LOG_ERROR("Failed to create directory.");
-        exit(1);
-    }
-
-    // Path to the Bash script
-    snprintf(bash_script_path, sizeof(bash_script_path), "%s/install_update.sh", updater_path);
-
-    // Write Bash script content
-    FILE *script_file = fopen(bash_script_path, "w");
-    if (!script_file)
-    {
-        LOG_ERROR("Failed to create the Bash script.\n");
-        exit(1);
+        strcpy(labels[0], "Updater");
+        strcpy(labels[1], "Updater (for development)");
     }
     
-	const char *package = ".deb";
-	//const char *package = ".zip";
-	LOG_INFO("%s", package);
-	LOG_INFO("%s", package_url);
-	LOG_INFO("%s", package_type);
-	
-    fprintf(script_file,
-        "#!/bin/bash\n"
-        "UPDATER_PID=%d\n"
-        "PACKAGE_URL=\"%s\"\n"
-        "PACKAGE_TYPE=\"%s\"\n"
-        "kill -9 $UPDATER_PID\n"
-        "echo \"Starting update process...\"\n"
-        "echo \"Download package...\"\n"
-        "curl -L \"$PACKAGE_URL\" -o ~/.config/fastboot-assistant/Updater/fastboot-assistant$PACKAGE_TYPE\n"
-        "echo \"Installing package...\"\n"
-        "if [[ \"$PACKAGE_TYPE\" == \".deb\" ]]; then\n"
-        "  sudo apt install ~/.config/fastboot-assistant/Updater/fastboot-assistant$PACKAGE_TYPE\n"
-        "else\n"
-        "  echo \"Unsupported package type: $PACKAGE_TYPE\"\n"
-        "  exit 1\n"
-        "fi\n"
-        "echo \"Update completed.\"\n",
-        getpid(), package_type, package);
-      
-	// the skript for WSL
-	/*
-	fprintf(script_file,
-	    "#!/bin/bash\n\n"
-	    "UPDATER_PID=%d\n"
-	    "PACKAGE_URL=\"%s\"\n"
-        "PACKAGE_TYPE=\"%s\"\n"
-	    "USER_NAME=$(whoami)\n"
-	    "WSL_DIR=\"/mnt/c/Users/$USER_NAME\"\n"
-	    "OUTPUT_PATH=\"$WSL_DIR/.config/fastboot-assistant/Updater\"\n"
-	    "OUTPUT_FILE=\"$OUTPUT_PATH/fastboot-assistant.zip\"\n"
-	    "DEB_ON_WSL=\"$OUTPUT_PATH/fastboot-assistant.deb\"\n\n"
-	    "UNZIP_COMMAND=\"unzip \\\"$OUTPUT_FILE\\\" -d \\\"$OUTPUT_PATH\\\"\"\n"
-	    "kill -9 $UPDATER_PID\n"
-	    "echo \"Starting update process...\"\n"
-	    "curl -L \"$PACKAGE_URL\" -o \"$OUTPUT_FILE\"\n"
-	    "if ! eval $UNZIP_COMMAND; then\n"
-	    "    echo \"Error: Failed to unzip $OUTPUT_FILE\"\n"
-	    "    exit 1\n"
-	    "fi\n\n"
-	    "cd \"$OUTPUT_PATH\" || { echo \"Error: Failed to change directory to $OUTPUT_PATH\"; exit 1; }\n\n"
-	    "INSTALL_COMMAND=\"sudo dpkg -i \\\"$DEB_ON_WSL\\\" \"\n"
-	    "if ! eval $INSTALL_COMMAND; then\n"
-	    "    echo \"Error: Failed to install $DEB_ON_WSL\"\n"
-	    "    exit 1\n"
-	    "fi\n\n"
-	    "echo \"Update completed.\"\n",
-	    getpid(), package_type, package);
-	*/
-	// close file
-    fclose(script_file);
-
-    // Set executable permissions
-    char chmod_command[2048];
-    snprintf(chmod_command, sizeof(chmod_command), "chmod a+x %s", bash_script_path);
-    if (system(chmod_command) != 0)
+    else
     {
-        LOG_ERROR("Failed to set executable permissions.");
-        exit(1);
+    	strcpy(labels[0], "Updater");
+    	strcpy(labels[1], "Updater (für Entwicklung)");
     }
-    
-    char cat_command[2048];
-    snprintf(cat_command, sizeof(cat_command), "cat %s", bash_script_path);
-    LOG_INFO("Run: %s", cat_command);
-    system(cat_command);
+} 
 
-    // Run the script in a new terminal
-    open_terminal_by_desktop(bash_script_path);
-}
-
-// function to extract the version from the url
-const char* extract_version_from_url(const char* url) 
-{
-    const char* version_start = strstr(url, "/releases/download/");
-    if (version_start) 
-    {
-        version_start += strlen("/releases/download/");
-        const char* version_end = strchr(version_start, '/');
-        if (version_end) 
-        {
-            static char version[64];
-            size_t version_length = version_end - version_start;
-            if (version_length < sizeof(version)) 
-            {
-                strncpy(version, version_start, version_length);
-                version[version_length] = '\0';
-                return version;
-            }
-        }
-    }
-    return NULL;
-}
-
-// Function to retrieve the latest release URL from GitHub
-void get_latest_release_url(const char *repo, const char *package_type, char *url_buffer, size_t buffer_size) 
-{
-    LOG_INFO("get_latest_release_url");
-    char command[2048];
-    // get the url from the github api
-    snprintf(command, sizeof(command),
-             "curl -s https://api.github.com/repos/%s/releases/latest | grep 'browser_download_url' | grep '%s' | head -n 1 | cut -d '\"' -f 4",
-             repo, package_type);
-    FILE *fp = popen(command, "r");
-    if (fp == NULL) 
-    {
-        LOG_ERROR("Error when executing the command.");
-        exit(1);
-    }
-
-    if (fgets(url_buffer, buffer_size, fp) == NULL) 
-    {
-        LOG_ERROR("Error when retrieving the URL.");
-        pclose(fp);
-        exit(1);
-    }
-
-    url_buffer[strcspn(url_buffer, "\n")] = '\0';  // remove linebreak
-    pclose(fp);
-    LOG_INFO("end get_latest_release_url");
-}
-
-// function to download the file
-int download_file(const char *url, const char *outfilename) 
-{
-    char command[2048];
-    snprintf(command, sizeof(command), "curl -L %s -o %s", url, outfilename);
-    return system(command);
-}
-
-// function to check the typ of the package
-int verify_package_type(const char *filepath, const char *expected_extension) 
-{
-    const char *ext = strrchr(filepath, '.');
-    return ext && strcmp(ext, expected_extension) == 0;
-}
-
-/* main function of the updater */
-void updater(void)
+/* main function of the backup_function */
+void updater(int argc, char *argv[]) 
 {
     LOG_INFO("updater");
-
+    GtkWidget *window, *grid, *button;
+    char button_labels[2][30];
+    
     gtk_init();
-    main_loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
     apply_theme();
     apply_language();
+    set_button_labels_updater(button_labels);
+    
+    window = gtk_window_new();
+    gtk_window_set_title(GTK_WINDOW(window), "Updater");
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
+    g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), main_loop);
+    
+    grid = gtk_grid_new();
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+    gtk_window_set_child(GTK_WINDOW(window), grid);
+    
+    for (int i = 0; i < 2; i++) 
+    {
+        button = gtk_button_new_with_label(button_labels[i]);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 2, i / 2, 1, 1);
+        
+         // Connect buttons to corresponding functions
+        switch (i) {
+            case 0:
+                g_signal_connect(button, "clicked", G_CALLBACK(start_updater_stable), NULL);
+                break;
+            case 1:
+                g_signal_connect(button, "clicked", G_CALLBACK(start_updater_devmode), NULL);
+                break;
+        }
+    }
 	
-	/* please change the package type for the different packages → .deb or .zip*/
-    const char *repo = "NachtsternBuild/fastboot-assistant";
-    const char *package_type = ".deb";
-    //const char *package_type = ".zip"; 
+    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
 
-    get_latest_release_url(repo, package_type, package_url, sizeof(package_url));
-
-    if (strlen(package_url) > 0)
-    {
- 		LOG_INFO("Latest version URL: %s", package_url);
-                
-        const char* version = extract_version_from_url(package_url);
-        if (version) 
-        {
-            LOG_INFO("Extracted version: %s", version);
-        }
-        
-        else 
-        {
-            LOG_INFO("Could not extract version from URL.");
-        }
-
-		char output_directory[2048];
-		
-		get_config_file_path(output_directory, sizeof(output_directory));
-
-        snprintf(output_file, sizeof(output_file), "%s/Updater/fastboot-assistant%s", output_directory, package_type);
-        
-        LOG_INFO("Package downloading: %s", output_file);
-        if (!verify_package_type(output_file, package_type)) 
-        {
-            LOG_ERROR("The downloaded package is not a %s package.", package_type);
-            exit(1);
-        }
-
-        const char *confirmation = strcmp(language, "de") == 0 ? "Bestätigung" : "Confirmation";
-        GtkWidget *confirmation_window = gtk_window_new();  
-        gtk_window_set_title(GTK_WINDOW(confirmation_window), confirmation);
-
-        gtk_window_set_default_size(GTK_WINDOW(confirmation_window), 500, 500);
-        g_signal_connect(confirmation_window, "destroy", G_CALLBACK(on_window_destroy), NULL);
-
-        GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-        gtk_window_set_child(GTK_WINDOW(confirmation_window), vbox);
-
-        GtkWidget *message_label = gtk_label_new(g_strcmp0(language, "de") == 0 ? "\n\nMöchten sie das Update installieren?\n\n" : "\n\nWould you like to install the update?\n\n");
-        gtk_box_append(GTK_BOX(vbox), message_label);
-        
-        // Version message einfügen
-		char version_message[256];
-		snprintf(version_message, sizeof(version_message), g_strcmp0(language, "de") == 0 ? "Gefundene Version: %s\n\n" : "Found version: %s\n\n", version);
-
-		GtkWidget *version_label = gtk_label_new(version_message);
-		gtk_box_append(GTK_BOX(vbox), version_label);
-
-        GtkWidget *confirm_button = gtk_button_new_with_label(g_strcmp0(language, "de") == 0 ? "Installieren" : "Install");
-        gtk_box_append(GTK_BOX(vbox), confirm_button);
-
-        g_signal_connect(confirm_button, "clicked", G_CALLBACK(create_and_run_bash_script), package_url);
-			
-        GtkWidget *cancel_button = gtk_button_new_with_label(g_strcmp0(language, "de") == 0 ? "Später Installieren" : "Install later");
-        gtk_box_append(GTK_BOX(vbox), cancel_button);
-        g_signal_connect(cancel_button, "clicked", G_CALLBACK(close_window_mainloop), confirmation_window);
-
-        // show all widgets
-    	gtk_window_present(GTK_WINDOW(confirmation_window)); // gtk_window_present instead of gtk_widget_show 
-    }
-     
-    else 
-    {
-        LOG_ERROR("No release URL found.");
-    }
-
-    // run GTK main loop
+     // run GTK main loop
     g_main_loop_run(main_loop); 
     
     // free the provider
@@ -305,6 +116,6 @@ void updater(void)
     	g_main_loop_unref(main_loop);
     	main_loop = NULL;
 	}
-	
+    
     LOG_INFO("end updater");
 }
