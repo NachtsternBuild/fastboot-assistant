94b22f149b5b8a9a01aef302cc9f93cad1099843
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-01-23 17:41:41 +0100

    for #91
    - use new program structure
    - use new logging

--- Anwendung/preflash/rename_system.c
+++ Anwendung/preflash/rename_system.c
@@ -10,7 +10,7 @@
  *	zu erleichtern - rename_system			 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2023 Elias Mörz 		 *
+ *      (C) Copyright 2025 Elias Mörz 		 *
  *-------------------------------------------*
  *
  */
@@ -22,31 +22,34 @@
 #include "program_functions.h"
 #include "function_header.h"
 #include "file_chooser_header.h"
+#include "flash_function_header.h"
 
 // rename a file to system.img
 void rename_system(const gchar *sys_filename) 
 {
-    gchar *target_directory_system = get_home("~/Downloads/ROM-Install/");
-    if (target_directory_system == NULL) 
-    {
-        g_print("Fehler beim Erweitern des Verzeichnispfads.\n");
-        return;
-    }
+    char rename_system_path[4096];
+    get_config_file_path(rename_system_path, sizeof(rename_system_path));
+    // load the path
+    const char *target_directory_system = load_path_from_file(rename_system_path);
 
-    const gchar *system_filename = "system.img";
+    if (target_directory_system) 
+    {
+        LOG_INFO("Loaded path: %s", target_directory_system);
+    }
     
+    const gchar *system_filename = "system.img"; 
     gchar *target_path_system = g_strconcat(target_directory_system, system_filename, NULL);
     
     if (rename(sys_filename, target_path_system) == 0) 
     {
-        g_print("Datei erfolgreich umbenannt: %s -> %s\n", sys_filename, system_filename);
+        LOG_INFO("File renamed successfully: %s → %s", sys_filename, system_filename);
     } 
     
     else 
     {
-        g_print("Fehler beim Umbenennen der Datei: %s\n", sys_filename);
+        LOG_ERROR("Error renaming the file: %s", sys_filename);
     }
     
-    g_free(target_directory_system);
+    free(target_directory_system);
     g_free(target_path_system);
 }
