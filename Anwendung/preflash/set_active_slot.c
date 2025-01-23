85535d51fc13ae4ace5dd9f78f691165f895e197
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-01-23 17:44:30 +0100

    new logging

--- Anwendung/preflash/set_active_slot.c
+++ Anwendung/preflash/set_active_slot.c
@@ -10,7 +10,7 @@
  *	zu erleichtern - set_active_slot		 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2024 Elias Mörz 		 *
+ *      (C) Copyright 2025 Elias Mörz 		 *
  *-------------------------------------------*
  *
  */
@@ -31,25 +31,26 @@ char slot_command[2048];
 // button 1 - set slot a
 static void set_slot_a(GtkWidget *widget, gpointer data)
 {
-    g_print("Log: set_slot_a\n");
+    LOG_INFO("set_slot_a");
     char *device_command = fastboot_command();
 	snprintf(slot_command, sizeof(slot_command), "%s set_active a && %s getvar current-slot", device_command, device_command);
-	g_print("Log: run: %s \n", slot_command);
+	LOG_INFO("Run: %s", slot_command);
     command_with_spinner(slot_command);
     free(device_command);
-    g_print("Log: end set_slot_a\n");
+    LOG_INFO("end set_slot_a");
 }
 
 // button 2 - set slot b
 static void set_slot_b(GtkWidget *widget, gpointer data)
 {
-    g_print("Log: set_slot_b\n");
-    char *device_command = fastboot_command();
+    LOG_INFO("set_slot_b");
+    char *device_command = fastboot_command();- use new program structure
+- use new logging
 	snprintf(slot_command, sizeof(slot_command), "%s set_active b && %s getvar current-slot", device_command, device_command);
-	g_print("Log: run:%s \n", slot_command);
+	LOG_INFO("Run: %s", slot_command);
     command_with_spinner(slot_command);
     free(device_command);
-    g_print("Log: end set_slot_b\n");
+    LOG_INFO("end set_slot_b");
 }
 
 // Function to set up button labels based on the language
@@ -71,7 +72,7 @@ void set_button_labels_slot(char labels[][30])
 /* start main programm */
 void set_active_slot(int argc, char *argv[])
 {
-    g_print("Log: set_active_slot\n");
+    LOG_INFO("set_active_slot");
     GtkWidget *window, *grid, *button;
     char button_labels[2][30];
     
@@ -127,6 +128,6 @@ void set_active_slot(int argc, char *argv[])
     	main_loop = NULL;
 	}
 	
-    g_print("Log: end set_active_slot\n");
+    LOG_INFO("end set_active_slot");
 }
     
