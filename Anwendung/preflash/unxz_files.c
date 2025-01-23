e3724bb6312f7b3205ffd652e6ebd188edeffe65
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-01-23 18:12:58 +0100

    for #91
    - use new program structure
    - use new logging
    - rework logic that create the decompressed filename

--- Anwendung/preflash/unxz_files.c
+++ Anwendung/preflash/unxz_files.c
@@ -10,7 +10,7 @@
  *	zu erleichtern - unxz_files				 *
  *                                           *
  *-------------------------------------------*
- *      (C) Copyright 2024 Elias Mörz 		 *
+ *      (C) Copyright 2025 Elias Mörz 		 *
  *-------------------------------------------*
  *
  */
@@ -20,69 +20,50 @@
 #include <gtk/gtk.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include "program_functions.h"
 
-#define TARGET_DIR "Downloads/ROM-Install" // target dir
-
+// function to decompress xz-files
 void unxz_files(const char *xz_filename) 
 {
-    // get home dir
-    char *homeDir = getenv("HOME");
-	if (homeDir == NULL) 
-	{
-    	fprintf(stderr, "Log: Error: Could not find the home directory.\n");
-    	exit(1);  // close the program if there are errors
-	}
-    
-    // WSL Logic
-	const char *user = getenv("USER");
-	if (user == NULL) 
-	{	
-    	g_print("Log: Error: Could not determine the user name.\n");
-    	exit(1);  // close the program if there are errors
-	}
+    char unxz_file_system_path[4096];
+    get_config_file_path(unxz_file_system_path, sizeof(unxz_file_system_path));
+    // load the path
+    const char *file_path_unxz = load_path_from_file(unxz_file_system_path);
 
-	char wsl_setup_base[2048];
-	snprintf(wsl_setup_base, sizeof(wsl_setup_base), "/mnt/c/Users/%s", user);
-
-    // make full path
-    char output_dir_xz[512];
-    // for linux
-    snprintf(output_dir_xz, sizeof(output_dir_xz), "%s/%s", homeDir, TARGET_DIR);
-    // for wsl
-    //snprintf(output_dir_xz, sizeof(output_dir_xz), "%s/%s", wsl_setup_base, TARGET_DIR);
-
-    // remove .xz from the output
-    char output_xz_filename[512];
-    const char *dot_xz = strrchr(xz_filename, '.');
-    if (dot_xz && strcmp(dot_xz, ".xz") == 0) 
+    if (file_path_unxz) 
     {
-        size_t base_length = dot_xz - xz_filename;
-        strncpy(output_xz_filename, xz_filename, base_length);
-        output_xz_filename[base_length] = '\0';
-    } 
-    
-    else 
-    {
-        strncpy(output_xz_filename, xz_filename, sizeof(output_xz_filename));
-        g_print("Log: No xz-compressed file!\n");
+        LOG_INFO("Loaded path: %s", file_path_unxz);
     }
-    
-    // make full path to outout file
-    char output_file_xz[512];
-    snprintf(output_file_xz, sizeof(output_file_xz), "%s", output_xz_filename);
+
+    // generate output filename by removing ".xz" extension
+    const char *dot_xz = strrchr(xz_filename, '.');
+    if (!dot_xz || strcmp(dot_xz, ".xz") != 0) 
+    {
+        LOG_ERROR("Input file is not an xz-compressed file.");
+    }
+
+    char output_xz_filename[512];
+    size_t base_length = dot_xz - xz_filename;
+    if (base_length >= sizeof(output_xz_filename)) 
+    {
+        LOG_INFO("Output filename buffer too small.");
+        exit(1);
+    }
+    // create full file name
+    snprintf(output_xz_filename, sizeof(output_xz_filename), "%s/%.*s", file_path_unxz, (int)base_length, xz_filename);
 
     // create unxz command
     char command[1024];
-    snprintf(command, sizeof(command), "unxz -c %s > %s", xz_filename, output_file_xz);
-
-    // output of the command
-    g_print("Log: Run: %s\n", command);
-
-    // run command
-    int result = system(command);
-    if (result != 0) 
+    int command_length = snprintf(command, sizeof(command), "unxz -c '%s' > '%s'", xz_filename, output_xz_filename);
+    if (command_length < 0 || command_length >= sizeof(command)) 
     {
-        g_print("Log: Error when executing the command.\n");
-        exit(EXIT_FAILURE);
+        LOG_INFO("Command buffer too small");
+    	exit(1);
     }
+
+    // run the command with spinner
+    LOG_INFO("Run: %s", command);
+    command_with_spinner(command);
+    LOG_INFO("File successfully decompressed to: %s", output_xz_filename);
 }
+
