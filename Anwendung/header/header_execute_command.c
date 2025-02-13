8efd02f2ef3af43bbe9ad2c9d6853890523c0905
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-02-13 16:59:13 +0100

    rework function
    - add debugging option
    - make the code a little bit safer

--- Anwendung/header/header_execute_command.c
+++ Anwendung/header/header_execute_command.c
@@ -24,27 +24,43 @@
 #include <gtk/gtk.h>
 #include "program_functions.h"
 
-// Function to execute a command and capture its output
+/* for debugging *
+*
+* Usage:
+* DEBUG 0 ← no debugging
+* DEBUG 1 ← active debugging
+*/
+
+#define DEBUG 1
+
+// function to execute a command and capture its output
 char *execute_command(const char *command) 
 {
     FILE *fp;
     char buffer[2048];
-    char *result = NULL;
+    auto_free char *result = calloc(1, 8192);  // autofree the memory
+
+    if (!result) 
+        return NULL;
 
     fp = popen(command, "r");
-    if (fp == NULL) 
-    {
-        LOG_ERROR("Error when executing the command.");
-        exit(1);
-    }
+    if (!fp) 
+        return NULL;
 
     while (fgets(buffer, sizeof(buffer), fp) != NULL) 
     {
-        result = strdup(buffer);
+        strncat(result, buffer, 8192 - strlen(result) - 1);
     }
 
     pclose(fp);
 
-    return result;
+#ifdef DEBUG
+    // output for debugging
+    LOG_INFO("Command Output: %s", result);
+#endif
+
+    return result;  // autofree the memory
 }
+
+
 
