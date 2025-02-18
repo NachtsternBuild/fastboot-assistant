8e2b2af40b14cfb9add68d2bc183dcaca1e859b6
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-02-18 14:27:00 +0100

    for #80
    - use new way to get the images
    - use dynamic adb command
    - improve the infos about the treble updater
    - use two languages
    - add bootctl info

--- Anwendung/treble_updater.c
+++ Anwendung/treble_updater.c
@@ -32,23 +32,30 @@ GtkWidget *system_checkbox;
 GtkWidget *treble_spinner;
 GtkWidget *restarting_system;
 GtkWidget *root_status_label;
+GtkWidget *bootctl_status_label;
 GtkWidget *free_space1;
 GtkWidget *free_space2;
 GtkWidget *boot_image_file_chooser;
 GtkWidget *system_image_file_chooser;
 GtkWidget *set_active_slot_checkbox;
 GtkWidget *treble_spinner_window;
+GtkWidget *info_treble;
+GtkWidget *info2_treble;
 
 
 // get the active slot with adb
 char *get_inactive_slot() 
 {
     FILE *fp;
+    char slot_command[2048];
     char buffer[128];
     auto_free char *inactive_slot = malloc(4);
+    auto_free char *device_command = adb_command();
     if (!inactive_slot) return NULL; // if malloc won't work
-
-    fp = popen("adb shell getprop ro.boot.slot_suffix", "r");
+	
+	snprintf(slot_command, sizeof(slot_command), "%s shell getprop ro.boot.slot_suffix", device_command);
+	
+    fp = popen(slot_command, "r");
     if (!fp) 
     {
         LOG_ERROR("Error: Unable to get current slot.\n");
@@ -74,28 +81,50 @@ char *get_inactive_slot()
 // function that flash the images in the inactive slot and set a new active slot
 void *flash_thread_func(void *arg) 
 {      
+    char config_file[4096];
+    char boot_image[4096];
+    char system_image[4096];
+    char command_build[4096];
+    const char *boot_image_name = "boot.img";
+	const char *system_image_name = "system.img";
+	
+    // get the inactive slot
     char *inactive_slot = get_inactive_slot();
-    const char *boot_image_path = "~/Downloads/ROM-Install/boot.img";
-    const char *system_image_path = "~/Downloads/ROM-Install/system.img";
-    char command_build[512];
+    // get the adb command
+    auto_free char *device_command = adb_command();
+    // get the config file
+    get_config_file_path(config_file, sizeof(config_file));
+    // load the path
+    const char *image_path = load_path_from_file(config_file);
+
+    if (image_path) 
+    {
+        LOG_INFO("Loaded path: %s", image_path);
+    }
+	
+	// create the path from the images
+	snprintf(boot_image, sizeof(boot_image), "%s/%s", image_path, boot_image_name);
+	snprintf(system_image, sizeof(system_image), "%s/%s", image_path, system_image_name);
     
-    char *command = execute_command("adb wait-for-device");
+    // wait for a device
+    snprintf(command_build, sizeof(command_build), "%s wait-for-device", device_command);
+    char *command = execute_command(command_build);
 	
 	// multiple options for the treble updater
 	// flash new boot.img
     if (gtk_check_button_get_active(GTK_CHECK_BUTTON(boot_checkbox)))
 	{
     	// push image to the device
-    	snprintf(command_build, sizeof(command_build), "adb push %s /data/local/tmp/boot.img", boot_image_path);
+    	snprintf(command_build, sizeof(command_build), "%s push %s /data/local/tmp/boot.img", device_command, boot_image);
     	command = execute_command(command_build);
     	
     	// copy the image in the inactive slot
     	// with 'wait' and 'sync' the adb should wait that 'dd' finished
-    	snprintf(command_build, sizeof(command_build), "adb shell su -c dd if=/data/local/tmp/boot.img of=/dev/block/by-name/boot%s & PID=$!; wait $PID; sync'", inactive_slot);
+    	snprintf(command_build, sizeof(command_build), "%s shell su -c dd if=/data/local/tmp/boot.img of=/dev/block/by-name/boot%s & PID=$!; wait $PID; sync'", device_command, inactive_slot);
     	command = execute_command(command_build);
     	
     	// remove the image in tmp/
-    	snprintf(command_build, sizeof(command_build), "adb shell su -c rm -f /data/local/tmp/boot.img");
+    	snprintf(command_build, sizeof(command_build), "%s shell su -c rm -f /data/local/tmp/boot.img", device_command);
     	command = execute_command(command_build);
 	}
 
@@ -103,40 +132,48 @@ void *flash_thread_func(void *arg)
 	if (gtk_check_button_get_active(GTK_CHECK_BUTTON(system_checkbox))) 
 	{
     	// push image to the device
-    	snprintf(command_build, sizeof(command_build), "adb push %s /data/local/tmp/system.img", system_image_path);
+    	snprintf(command_build, sizeof(command_build), "%s push %s /data/local/tmp/system.img", device_command, system_image);
     	command = execute_command(command_build);
     	
     	// copy the image in the inactive slot
     	// with 'wait' and 'sync' the adb should wait that 'dd' finished
-    	snprintf(command_build, sizeof(command_build), "adb shell su -c 'dd if=/data/local/tmp/system.img of=/dev/block/by-name/system%s & PID=$!; wait $PID; sync'", inactive_slot);
+    	snprintf(command_build, sizeof(command_build), "%s shell su -c 'dd if=/data/local/tmp/system.img of=/dev/block/by-name/system%s & PID=$!; wait $PID; sync'", device_command, inactive_slot);
 		command = execute_command(command_build);
 
     	// remove the image in tmp/
-    	snprintf(command_build, sizeof(command_build), "adb shell su -c rm -f /data/local/tmp/system.img");
+    	snprintf(command_build, sizeof(command_build), "%s shell su -c rm -f /data/local/tmp/system.img", device_command);
     	command = execute_command(command_build);
 	}
 	
-	// for the security that 
-	command = execute_command("adb wait-for-device");
+	// for the security that adb is ready
+	snprintf(command_build, sizeof(command_build), "%s wait-for-device", device_command);
+	command = execute_command(device_command);
 	
 	// get the bootctl
-	command = execute_command("adb shell su -c bootctl");
+	snprintf(command_build, sizeof(command_build), "%s shell su -c bootctl", device_command);
+	command = execute_command(command_build);
 	
   	// set the active slot with the bootctl
-   	snprintf(command_build, sizeof(command_build), "adb shell su -c bootctl set-active-boot-slot %d", inactive_slot[1] == 'b' ? 0 : 1);
+   	snprintf(command_build, sizeof(command_build), "%s shell su -c bootctl set-active-boot-slot %d", device_command, inactive_slot[1] == 'b' ? 0 : 1);
    	command = execute_command(command_build);
     
     // alternative way to set new active slot
     // I won't use this, because using bootctl is better	
    	if (gtk_check_button_get_active(GTK_CHECK_BUTTON(set_active_slot_checkbox)))
    	{
-    	snprintf(command_build, sizeof(command_build), "adb shell su -c setprop persist.sys.boot.slot %s", inactive_slot);
+    	snprintf(command_build, sizeof(command_build), "%s shell su -c setprop persist.sys.boot.slot %s", device_command, inactive_slot);
     	command = execute_command(command_build);
 	}
 	        
     // stop the spinner
     gtk_spinner_stop(GTK_SPINNER(treble_spinner));
     gtk_window_destroy(GTK_WINDOW(treble_spinner_window));
+    
+    if (image_path != NULL) 
+	{
+    	g_free((gpointer)image_path); // free this (because g_file_get_contents was used)
+    	// I'll try to create a autofree for g_free and this to
+	}
     
     return NULL;
 }
@@ -173,7 +210,11 @@ void update_device(GtkWidget *widget, gpointer user_data)
 void restart_system(GtkWidget *widget, gpointer user_data)
 {
 	LOG_INFO("Restart with ADB");
-	char *command = execute_command("adb reboot");
+	char command_build[2048];
+	auto_free char *device_command = adb_command();
+	
+	snprintf(command_build, sizeof(command_build), "%s reboot", device_command);
+	char *command = execute_command(command_build);
 }
 
 /* main function - treble_updater */
@@ -195,37 +236,44 @@ void treble_updater(int argc, char *argv[])
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
     gtk_window_set_child(GTK_WINDOW(window), vbox);
     
+    info_treble = gtk_label_new(g_strcmp0(language, "de") == 0 ? "\nDer Treble Updater funktioniert nur mit a/b-Geräten. \nEs müssen Root-Rechte bereitgestellt werden.\n" : "\nThe Treble Updater only works with a/b devices. \nRoot rights must be provided.\n");
+    info2_treble = gtk_label_new(g_strcmp0(language, "de") == 0 ? "Der Treble Updater hat die Aufgabe ein neues Boot/System-Image \nin den inaktiven Slot zu flashen und \ndiesen aktiv zusetzen.\n" : "The Treble Updater has the task of flashing \na new boot/system image into the inactive slot \nand making it active.\n");
+    
     // label for some free space
     free_space1 = gtk_label_new("  ");
     gtk_box_append(GTK_BOX(vbox), free_space1);
     
     // root status label
-    root_status_label = gtk_label_new("Prüfe Root-Rechte...");
+    root_status_label = gtk_label_new("Check Root permission...");
     gtk_box_append(GTK_BOX(vbox), root_status_label);
-   
+    
+    // bootctl status label
+    bootctl_status_label = gtk_label_new("Search for bootctl...");
+    gtk_box_append(GTK_BOX(vbox), bootctl_status_label);
+    
     // label for some free space
     free_space2 = gtk_label_new("  ");
     gtk_box_append(GTK_BOX(vbox), free_space2);
     
     // checkbox for boot.img
-    boot_checkbox = gtk_check_button_new_with_label("boot.img");
+    boot_checkbox = gtk_check_button_new_with_label(g_strcmp0(language, "de") == 0 ? "Flashe neues Boot-Image" : "Flash new boot image");
     gtk_box_append(GTK_BOX(vbox), boot_checkbox);
 	
 	// checkbox for system.img
-    system_checkbox = gtk_check_button_new_with_label("system.img");
+    system_checkbox = gtk_check_button_new_with_label(g_strcmp0(language, "de") == 0 ? "Flashe neues System-Image" : "Flash new system image");
     gtk_box_append(GTK_BOX(vbox), system_checkbox);
 	
 	// use option to set active slot with setprop
-    set_active_slot_checkbox = gtk_check_button_new_with_label("Alternative für den Slot Wechsel");
+    set_active_slot_checkbox = gtk_check_button_new_with_label(g_strcmp0(language, "de") == 0 ? "Alternative für Slotwechsel (nicht empfohlen)" : "Alternative for slot change (not recommended)");
     gtk_box_append(GTK_BOX(vbox), set_active_slot_checkbox);
 	
 	// Start the flash
-    start_button = gtk_button_new_with_label("Start Flash");
+    start_button = gtk_button_new_with_label(g_strcmp0(language, "de") == 0 ? "Starte Flash" : "Start Flash");
     gtk_box_append(GTK_BOX(vbox), start_button);
     g_signal_connect(start_button, "clicked", G_CALLBACK(update_device), NULL);
 	
 	// restart the system
-	restarting_system = gtk_button_new_with_label("Neustart");
+	restarting_system = gtk_button_new_with_label(g_strcmp0(language, "de") == 0 ? "Neustarten" : "Reboot Device");
 	gtk_box_append(GTK_BOX(vbox), restarting_system);
 	g_signal_connect(restarting_system, "clicked", G_CALLBACK(restart_system), NULL);
 	
@@ -233,6 +281,8 @@ void treble_updater(int argc, char *argv[])
 	
 	// check root access at the start
     check_root_access();
+    // chech for android bootctl
+    get_bootctl();
 	
     // run GTK main loop
     g_main_loop_run(main_loop);
