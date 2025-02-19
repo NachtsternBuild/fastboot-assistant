5f510bd5a2e08f4d245928d14a2943a8fc01e220
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-02-19 16:13:38 +0100

    for #94
    - add function to clean the log
    - add function to restart the setup
    - add function to get a new main dir for the fastboot-assistant
    - add function to switch the theme

--- Anwendung/config_projekt/config-projekt_GUI.c
+++ Anwendung/config_projekt/config-projekt_GUI.c
@@ -16,8 +16,12 @@
  */
 
 /* headers that used in the programm */
+#include <glib.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <gtk/gtk.h>
 #include "language_check.h"
 #include "program_functions.h"
@@ -27,6 +31,9 @@
 extern void make_dir();
 extern void remove_old();
 extern void wsl_config();
+extern void post_update();
+
+char success_message[4096];
 
 // Callback functions for each button
 // function that start make_dir
@@ -48,13 +55,98 @@ static void start_wsl_config(GtkWidget *widget, gpointer data)
 {
     wsl_config();
 }
-/*
-// function that start toggle_language
-static void start_toggle_language(GtkWidget *widget, gpointer data)
+
+// function start post_update
+static void run_post_update(GtkWidget *widget, gpointer data)
 {
-	toggle_language(NULL, NULL);
+	post_update();
 }
-*/
+
+// function restart the update
+static void run_setup(GtkWidget *widget, gpointer data)
+{
+	LOG_INFO("run_setup");
+	char restart_setup[2048];
+	char restart_setup_file_dark[2048];
+	char restart_setup_file_path[2048];
+	
+	// get config dir
+    get_config_dir(restart_setup, sizeof(restart_setup));
+    
+    // config files
+    snprintf(restart_setup_file_dark, sizeof(restart_setup_file_dark), "%s/dark.txt", restart_setup);
+    snprintf(restart_setup_file_path, sizeof(restart_setup_file_path), "%s/path_config.txt", restart_setup);
+	
+	// delete config files
+	delete_config_files(restart_setup_file_dark);
+	delete_config_files(restart_setup_file_path);
+	
+	// start first setup
+	run_first_run_setup(provider);
+	LOG_INFO("end run_setup");
+}
+
+// function to clean the log
+static void clean_log(GtkWidget *widget, gpointer data)
+{
+	LOG_INFO("clean_log");
+	char log_dir_settings[4096];
+    char log_file_settings_path[4096];
+    char log_file_settings[4096];
+        
+    // get config dir
+    get_config_dir(log_dir_settings, sizeof(log_dir_settings));
+    
+    // log file with path
+    snprintf(log_file_settings_path, sizeof(log_file_settings_path), "%s/log", log_dir_settings);
+    snprintf(log_file_settings, sizeof(log_file_settings), "%s/log/log.txt", log_dir_settings);
+    // delete files in dir
+    LOG_INFO("Remove: %s", log_file_settings_path);
+    delete_files_in_dir(log_file_settings_path);
+    
+    // create log_dir
+    // only if it not exsists
+    if (access(log_file_settings_path, F_OK) == -1) 
+    {
+        create_directory(log_file_settings_path);
+    }
+    // open log file or create it
+    int log_fd = open(log_file_settings, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (log_fd == -1) 
+    {
+        LOG_ERROR("Error when opening the log file.");
+        exit(1);
+    }
+    close(log_fd);
+    
+    snprintf(success_message, sizeof(success_message), g_strcmp0(language, "de") == 0 ? "Beendet" : "Completed");
+    show_message(success_message);
+    
+    LOG_INFO("end clean_log");
+}
+
+// function to create a new setup dir
+static void new_setup_dir(GtkWidget *widget, gpointer data)
+{
+	LOG_INFO("new_setup_dir");
+	char old_dir_path[4096];
+    char old_dir_file[4096];
+    
+    // logic to remove old path_config.txt
+    // get config dir
+    get_config_dir(old_dir_path, sizeof(old_dir_path));
+    
+    // remove path_config
+    snprintf(old_dir_file, sizeof(old_dir_file), "%s/path_config.txt", old_dir_path);
+    // delete file in dir
+    delete_config_files(old_dir_file);
+	
+	// start the folder chooser
+	show_folder_chooser(widget, data);
+	
+	LOG_INFO("end new_setup_dir");
+}
+
 // Function to set up button labels based on the language
 void set_button_labels_config_projekt(char labels[][30]) 
 {
@@ -64,6 +156,11 @@ void set_button_labels_config_projekt(char labels[][30])
         strcpy(labels[1], "Clean up");
         strcpy(labels[2], "Configure");
         strcpy(labels[3], "Change language");
+        strcpy(labels[4], "Change theme");
+        strcpy(labels[5], "Clean log");
+        strcpy(labels[6], "Define new directory");
+        strcpy(labels[7], "Post update");
+        strcpy(labels[8], "Restart Setup");
     } 
     
     else 
@@ -72,6 +169,11 @@ void set_button_labels_config_projekt(char labels[][30])
         strcpy(labels[1], "Aufräumen");
         strcpy(labels[2], "Konfigurieren");
         strcpy(labels[3], "Sprache wechseln");
+        strcpy(labels[4], "Thema wechseln");
+        strcpy(labels[5], "Log löschen");
+        strcpy(labels[6], "Neues Verzeichnis festlegen");
+        strcpy(labels[7], "Update Nachbearbeitung");
+        strcpy(labels[8], "Erneutes Setup");
     }
 }
 
@@ -80,7 +182,7 @@ void config_projekt_GUI(int argc, char *argv[])
 {
 	LOG_INFO("config_projekt_GUI");
 	GtkWidget *window, *grid, *button;
-    char button_labels[4][30];
+    char button_labels[9][30];
     
     gtk_init();
     GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
@@ -101,10 +203,10 @@ void config_projekt_GUI(int argc, char *argv[])
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
     gtk_window_set_child(GTK_WINDOW(window), grid);
     
-    for (int i = 0; i < 4; i++) 
+    for (int i = 0; i < 9; i++) 
     {
         button = gtk_button_new_with_label(button_labels[i]);
-        gtk_grid_attach(GTK_GRID(grid), button, i % 2, i / 2, 1, 1);
+        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
         
         switch (i) {
             case 0:
@@ -118,6 +220,21 @@ void config_projekt_GUI(int argc, char *argv[])
                 break;
             case 3:
             	g_signal_connect(button, "clicked", G_CALLBACK(toggle_language), NULL);
+            	break;
+            case 4:
+            	g_signal_connect(button, "clicked", G_CALLBACK(toggle_theme), NULL);
+            	break;
+            case 5:
+            	g_signal_connect(button, "clicked", G_CALLBACK(clean_log), NULL);
+            	break;
+            case 6:
+            	g_signal_connect(button, "clicked", G_CALLBACK(new_setup_dir), NULL);
+            	break;
+            case 7:
+            	g_signal_connect(button, "clicked", G_CALLBACK(run_post_update), NULL);
+            	break;
+            case 8:
+            	g_signal_connect(button, "clicked", G_CALLBACK(run_setup), NULL);
             	break;
         }
     }
