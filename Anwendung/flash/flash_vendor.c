6b12fd7c1a5d2ac97617788b702871b964720bdc
Elias Mörz <hallo.schneller50kmh@gmail.com>
2025-03-27 09:48:57 +0100

    for #99
    convert to the new structure

--- Anwendung/flash/flash_vendor.c
+++ Anwendung/flash/flash_vendor.c
@@ -23,44 +23,44 @@
 #include "program_functions.h"
 #include "flash_function_header.h"
 
-// Button handler functions
+// button handler functions
 // function to flash vendor.img on only-a-devices
-void vendor_on_a(GtkWidget *widget, GtkWindow *window)
+void vendor_on_a(GtkWidget *widget, gpointer stack)
 {
     flash_image(widget, window, "vendor", NULL, "vendor.img", NULL);
 }
 
 // function to flash vendor.img (a/b-devices)
-void vendor_on_ab(GtkWidget *widget, GtkWindow *window)
+void vendor_on_ab(GtkWidget *widget, gpointer stack)
 {
     flash_image(widget, window, "vendor_a", "vendor_b", "vendor.img", NULL);
 }
 
 // function to flash vendor_boot.img (only-a-devices)
-void vendor_boot_on_a(GtkWidget *widget, GtkWindow *window)
+void vendor_boot_on_a(GtkWidget *widget, gpointer stack)
 {
     flash_image(widget, window, "vendor_boot", NULL, "vendor_boot.img", NULL);
 }
 
 // function to flash vendor_boot.img (a/b-devices)
-void vendor_boot_on_ab(GtkWidget *widget, GtkWindow *window)
+void vendor_boot_on_ab(GtkWidget *widget, gpointer stack)
 {
     flash_image(widget, window, "vendor_boot_a", "vendor_boot_b", "vendor_boot.img", NULL);
 }
 
 // function to flash boot.img (heimdall)
-void vendor_heimdall(GtkWidget *widget, GtkWindow *window)
+void vendor_heimdall(GtkWidget *widget, gpointer stack)
 {
     flash_heimdall(widget, window, "VENDOR", "vendor.img");
 }
 
 // function to flash init_boot.img (heimdall)
-void vendor_boot_heimdall(GtkWidget *widget, GtkWindow *window)
+void vendor_boot_heimdall(GtkWidget *widget, gpointer stack)
 {
     flash_heimdall(widget, window, "VENDOR_BOOT", "vendor_boot.img");
 }
 
-// Function to set up button labels based on the language
+// function to set up button labels based on the language
 void set_button_labels_flash_vendor(char labels[][30]) 
 {
     if (strcmp(language, "en") == 0) 
@@ -71,6 +71,7 @@ void set_button_labels_flash_vendor(char labels[][30])
         strcpy(labels[3], "vendor_boot (only-a)");
         strcpy(labels[4], "vendor_boot (a/b)");
         strcpy(labels[5], "vendor_boot (heimdall)");
+        strcpy(labels[6], "Back");
     } 
     
     else 
@@ -81,78 +82,57 @@ void set_button_labels_flash_vendor(char labels[][30])
         strcpy(labels[3], "vendor_boot (only-a)");
         strcpy(labels[4], "vendor_boot (a/b)");
         strcpy(labels[5], "vendor_boot (heimdall)");
+        strcpy(labels[6], "Zurück");
     }
 }
 
 // main function
-void flash_vendor(int argc, char *argv[])
+void flash_vendor(GtkWidget *widget, gpointer stack)
 {
 	LOG_INFO("flash_vendor");
-	GtkWidget *window, *grid, *button;
-    char button_labels[6][30];
+	apply_language();
     
-    gtk_init();
-    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
-    apply_theme();
-    apply_language();
-    set_button_labels_flash_vendor(button_labels);
+    char button_labels[7][30];  // labels for the button 
+    set_button_labels_flash_vendor(labels);  // for both languages
     
-    window = gtk_window_new();
-    gtk_window_set_title(GTK_WINDOW(window), "Flashen:");
-    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
-    g_signal_connect(window, "destroy", G_CALLBACK(on_window_destroy), main_loop);
-    
-    grid = gtk_grid_new();
-    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
-    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+    GtkWidget *flash_vendor = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_widget_set_halign(flash_vendor, GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(flash_vendor, GTK_ALIGN_CENTER);
+
+    GtkWidget *grid = gtk_grid_new();
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
-    gtk_window_set_child(GTK_WINDOW(window), grid);
-    
-    for (int i = 0; i < 6; i++) 
-    {
-        button = gtk_button_new_with_label(button_labels[i]);
-        gtk_grid_attach(GTK_GRID(grid), button, i % 3, i / 3, 1, 1);
-        
-        switch (i) {
-            case 0:
-                g_signal_connect(button, "clicked", G_CALLBACK(vendor_on_a), NULL);
-                break;
-            case 1:
-                g_signal_connect(button, "clicked", G_CALLBACK(vendor_on_ab), NULL);
-                break;
-            case 2:
-                g_signal_connect(button, "clicked", G_CALLBACK(vendor_heimdall), NULL);
-                break;
-            case 3:
-                g_signal_connect(button, "clicked", G_CALLBACK(vendor_boot_on_a), NULL);
-                break;
-            case 4:
-                g_signal_connect(button, "clicked", G_CALLBACK(vendor_boot_on_ab), NULL);
-                break;
-            case 5:
-                g_signal_connect(button, "clicked", G_CALLBACK(vendor_boot_heimdall), NULL);
-                break;
-        }
-    }
 	
-    gtk_window_present(GTK_WINDOW(window)); // gtk_window_present instead of gtk_widget_show
+	// create button
+    GtkWidget *btn1 = create_nav_button(labels[0], G_CALLBACK(vendor_on_a), stack);
+    GtkWidget *btn2 = create_nav_button(labels[1], G_CALLBACK(vendor_on_ab), stack);
+    GtkWidget *btn3 = create_nav_button(labels[2], G_CALLBACK(vendor_heimdall), stack);
+    GtkWidget *btn4 = create_nav_button(labels[3], G_CALLBACK(vendor_boot_on_a), stack);
+    GtkWidget *btn5 = create_nav_button(labels[4], G_CALLBACK(vendor_boot_on_ab), stack);
+    GtkWidget *btn6 = create_nav_button(labels[5], G_CALLBACK(vendor_boot_heimdall), stack);
+    GtkWidget *btn_back = create_nav_button(labels[6], G_CALLBACK(flash_GUI), stack);
 
-     // run GTK main loop
-    g_main_loop_run(main_loop); 
-    
-    // free the provider
-    if (provider != NULL) 
+    // add the button to the grid
+    // line 1
+    gtk_grid_attach(GTK_GRID(grid), btn1, 0, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), btn2, 1, 0, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), btn3, 2, 0, 1, 1);
+    // line 2 (1)
+    gtk_grid_attach(GTK_GRID(grid), btn4, 0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), btn5, 1, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), btn6, 2, 1, 1, 1);
+
+    // pack the grid to the box
+    gtk_box_append(GTK_BOX(flash_vendor), grid);
+    // add the back button under the grid
+    gtk_box_append(GTK_BOX(flash_vendor), btn_back); 
+
+	// is needed to prevent it from being stacked again when called again
+    if (!gtk_stack_get_child_by_name(GTK_STACK(stack), "flash_vendor")) 
     {
-    	g_object_unref(provider);
-    	provider = NULL;
-	}
-
-	if (main_loop != NULL) 
-	{
-    	g_main_loop_unref(main_loop);
-    	main_loop = NULL;
-	}
-    
+        gtk_stack_add_named(GTK_STACK(stack), flash_vendor, "flash_vendor");
+    }
+	gtk_stack_set_visible_child_name(GTK_STACK(stack), "flash_vendor");
+        
     LOG_INFO("end flash_vendor");
 }
