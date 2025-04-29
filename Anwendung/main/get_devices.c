8bc2acc4615e3bc022707410bfd322ee448db9b3
NachtsternBuild <hallo.schneller50kmh@gmail.com>
2025-04-29 18:18:45 +0200

    for #115
    Add some icons and add UI improvements

--- Anwendung/main/get_devices.c
+++ Anwendung/main/get_devices.c
@@ -115,13 +115,15 @@ void get_devices(GtkWidget *widget, gpointer stack)
     GtkWidget *grid = gtk_grid_new();
     gtk_widget_set_halign(grid, GTK_ALIGN_CENTER);
     gtk_widget_set_valign(grid, GTK_ALIGN_CENTER);
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
 	
 	// create the button for get_devices
-    GtkWidget *btn1 = create_nav_button(labels[0], G_CALLBACK(get_adb), stack);
-    GtkWidget *btn2 = create_nav_button(labels[1], G_CALLBACK(get_fastboot), stack);
-    GtkWidget *btn3 = create_nav_button(labels[2], G_CALLBACK(bootloader_status_adb), stack);
-    GtkWidget *btn4 = create_nav_button(labels[3], G_CALLBACK(bootloader_status_fastboot), stack);
-    GtkWidget *btn_back = create_nav_button(labels[4], G_CALLBACK(show_home_page), stack);
+    GtkWidget *btn1 = create_icon_nav_button_with_position("content-loading-symbolic", labels[0], G_CALLBACK(get_adb), stack, GTK_ALIGN_CENTER);
+    GtkWidget *btn2 = create_icon_nav_button_with_position("preferences-other-symbolic", labels[1], G_CALLBACK(get_fastboot), stack, GTK_ALIGN_CENTER);
+    GtkWidget *btn3 = create_icon_nav_button_with_position("content-loading-symbolic", labels[2], G_CALLBACK(bootloader_status_adb), stack, GTK_ALIGN_CENTER);
+    GtkWidget *btn4 = create_icon_nav_button_with_position("preferences-other-symbolic", labels[3], G_CALLBACK(bootloader_status_fastboot), stack, GTK_ALIGN_CENTER);
+    GtkWidget *btn_back = create_icon_nav_button_with_position("pan-start-symbolic", labels[4], G_CALLBACK(show_home_page), stack, GTK_ALIGN_CENTER);
 	
     // add the button to the grid
     gtk_grid_attach(GTK_GRID(grid), btn1, 0, 0, 1, 1);
@@ -140,7 +142,6 @@ void get_devices(GtkWidget *widget, gpointer stack)
         gtk_stack_add_named(GTK_STACK(stack), get_devices, "get_devices");
     }
     
-    LOG_INFO("get_devices");
 	gtk_stack_set_visible_child_name(GTK_STACK(stack), "get_devices");
 	LOG_INFO("end get_devices");
 }
